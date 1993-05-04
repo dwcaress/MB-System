1bb4eb8935b28e02a9ad2324d88890b0a69954ab
dale <dale@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
1993-05-04 22:22:52 +0000

    Initial version

---
+++ trunk/src/utilities/mbdefaults.c
@@ -0,0 +1,256 @@
+/*--------------------------------------------------------------------
+ *    The MB-system:	mbdefaults.c	3.00	1/23/93
+ *
+ *    Copyright (c) 1993 by 
+ *    D. W. Caress (caress@lamont.ldgo.columbia.edu)
+ *    and D. N. Chayes (dale@lamont.ldgo.columbia.edu)
+ *    Lamont-Doherty Earth Observatory
+ *    Palisades, NY  10964
+ *
+ *    See README file for copying and redistribution conditions.
+ *--------------------------------------------------------------------*/
+/*
+ * MBDEFAULTS sets and retrieves the default MBIO control parameters 
+ * stored in the file ~/.mbio_defaults.  Only the parameters specified
+ * by command line arguments will be changed; if no ~/.mbio_defaults
+ * file exists one will be created.
+ *
+ * Author:	D. W. Caress
+ * Date:	January 23, 1993
+ */
+
+/* standard include files */
+#include <stdio.h>
+#include <math.h>
+#include <strings.h>
+
+/* mbio include files */
+#include "../../include/mb_status.h"
+
+/*--------------------------------------------------------------------*/
+
+main (argc, argv)
+int argc;
+char **argv; 
+{
+	static char rcs_id[] = "3.00";
+	static char program_name[] = "MBDEFAULTS";
+	static char help_message[] = "MBDEFAULTS sets and retrieves the default MBIO control \nparameters stored in the file ~/.mbio_defaults. \nOnly the parameters specified by command line \narguments will be changed; if no ~/.mbio_defaults \nfile exists one will be created.";
+	static char usage_message[] = "mbdefaults [-Fformat -Rw/e/s/n -Ppings -Sspeed -Llonflip\n	-Byr/mo/da/hr/mn/sc -Eyr/mo/da/hr/mn/sc -V -H]";
+	extern char *optarg;
+	extern int optkind;
+	int	errflg = 0;
+	int	c;
+	int	status;
+	int	verbose = 0;
+	int	help = 0;
+	int	flag = 0;
+	FILE	*fp;
+	char	file[128];
+	char	home[128];
+	char	*HOME = "HOME";
+	char	*getenv();
+
+	/* MBIO control parameters */
+	int format;
+	int pings;
+	int lonflip;
+	double bounds[4];
+	int btime_i[6];
+	int etime_i[6];
+	double speedmin;
+	double timegap;
+
+	/* get current default values */
+	status = mb_defaults(verbose,&format,&pings,&lonflip,bounds,
+		btime_i,etime_i,&speedmin,&timegap);
+
+	/* process argument list */
+	while ((c = getopt(argc, argv, "VvHhFf:P:p:L:l:R:r:B:b:E:e:S:s:T:t:")) != -1)
+	  switch (c) 
+		{
+		case 'H':
+		case 'h':
+			help++;
+			break;
+		case 'V':
+		case 'v':
+			verbose++;
+			break;
+		case 'F':
+		case 'f':
+			sscanf (optarg,"%d", &format);
+			flag++;
+			break;
+		case 'P':
+		case 'p':
+			sscanf (optarg,"%d", &pings);
+			flag++;
+			break;
+		case 'L':
+		case 'l':
+			sscanf (optarg,"%d", &lonflip);
+			flag++;
+			break;
+		case 'R':
+		case 'r':
+			sscanf (optarg,"%lf/%lf/%lf/%lf", 
+				&bounds[0],&bounds[1],&bounds[2],&bounds[3]);
+			flag++;
+			break;
+		case 'B':
+		case 'b':
+			sscanf (optarg,"%d/%d/%d/%d/%d/%d",
+				&btime_i[0],&btime_i[1],&btime_i[2],
+				&btime_i[3],&btime_i[4],&btime_i[5]);
+			flag++;
+			break;
+		case 'E':
+		case 'e':
+			sscanf (optarg,"%d/%d/%d/%d/%d/%d",
+				&etime_i[0],&etime_i[1],&etime_i[2],
+				&etime_i[3],&etime_i[4],&etime_i[5]);
+			flag++;
+			break;
+		case 'S':
+		case 's':
+			sscanf (optarg,"%lf", &speedmin);
+			flag++;
+			break;
+		case 'T':
+		case 't':
+			sscanf (optarg,"%lf", &timegap);
+			flag++;
+			break;
+		case '?':
+			errflg++;
+		}
+
+	/* if error flagged then print it and exit */
+	if (errflg)
+		{
+		fprintf(stderr,"usage: %s\n", usage_message);
+		exit(MB_FAILURE);
+		}
+
+	/* print starting message */
+	if (verbose == 1)
+		{
+		fprintf(stderr,"\nProgram %s\n",program_name);
+		fprintf(stderr,"Version %s\n",rcs_id);
+		fprintf(stderr,"MB-system Version %s\n",MB_VERSION);
+		}
+
+	/* print starting debug statements */
+	if (verbose >= 2)
+		{
+		fprintf(stderr,"\ndbg2  Program <%s>\n",program_name);
+		fprintf(stderr,"dbg2  Version %s\n",rcs_id);
+		fprintf(stderr,"dbg2  MB-system Version %s\n",MB_VERSION);
+		fprintf(stderr,"dbg2  Control Parameters:\n");
+		fprintf(stderr,"dbg2       verbose:    %d\n",verbose);
+		fprintf(stderr,"dbg2       help:       %d\n",help);
+		fprintf(stderr,"dbg2       format:     %d\n",format);
+		fprintf(stderr,"dbg2       pings:      %d\n",pings);
+		fprintf(stderr,"dbg2       lonflip:    %d\n",lonflip);
+		fprintf(stderr,"dbg2       bounds[0]:  %f\n",bounds[0]);
+		fprintf(stderr,"dbg2       bounds[1]:  %f\n",bounds[1]);
+		fprintf(stderr,"dbg2       bounds[2]:  %f\n",bounds[2]);
+		fprintf(stderr,"dbg2       bounds[3]:  %f\n",bounds[3]);
+		fprintf(stderr,"dbg2       btime_i[0]: %d\n",btime_i[0]);
+		fprintf(stderr,"dbg2       btime_i[1]: %d\n",btime_i[1]);
+		fprintf(stderr,"dbg2       btime_i[2]: %d\n",btime_i[2]);
+		fprintf(stderr,"dbg2       btime_i[3]: %d\n",btime_i[3]);
+		fprintf(stderr,"dbg2       btime_i[4]: %d\n",btime_i[4]);
+		fprintf(stderr,"dbg2       btime_i[5]: %d\n",btime_i[5]);
+		fprintf(stderr,"dbg2       etime_i[0]: %d\n",etime_i[0]);
+		fprintf(stderr,"dbg2       etime_i[1]: %d\n",etime_i[1]);
+		fprintf(stderr,"dbg2       etime_i[2]: %d\n",etime_i[2]);
+		fprintf(stderr,"dbg2       etime_i[3]: %d\n",etime_i[3]);
+		fprintf(stderr,"dbg2       etime_i[4]: %d\n",etime_i[4]);
+		fprintf(stderr,"dbg2       etime_i[5]: %d\n",etime_i[5]);
+		fprintf(stderr,"dbg2       speedmin:   %f\n",speedmin);
+		fprintf(stderr,"dbg2       timegap:    %f\n",timegap);
+		}
+
+	/* if help desired then print it and exit */
+	if (help)
+		{
+		fprintf(stderr,"\n%s\n",help_message);
+		fprintf(stderr,"\nusage: %s\n", usage_message);
+		exit(MB_SUCCESS);
+		}
+
+	/* write out new ~/.mbio_defaults file if needed */
+	if (flag || status == MB_FAILURE)
+		{
+		strcpy(file,getenv(HOME));
+		strcat(file,"/.mbio_defaults");
+		if ((fp = fopen(file, "w")) == NULL)
+			{
+			fprintf (stderr, "Could not open file %s\n", file);
+			exit(1);
+			}
+		fprintf(fp,"MBIO Default Control Parameters\n");
+		fprintf(fp,"format:     %d\n",format);
+		fprintf(fp,"pings:      %d\n",pings);
+		fprintf(fp,"lonflip:    %d\n",lonflip);
+		fprintf(fp,"speed:      %f\n",speedmin);
+		fprintf(fp,"timegap:    %f\n",timegap);
+		fprintf(fp,"bounds:     %f %f %f %f\n",
+			bounds[0],bounds[1],bounds[2],bounds[3]);
+		fprintf(fp,"begin time: %d %d %d %d %d %d\n",
+			btime_i[0],btime_i[1],btime_i[2],
+			btime_i[3],btime_i[4],btime_i[5]);
+		fprintf(fp,"end time:   %d %d %d %d %d %d\n",
+			etime_i[0],etime_i[1],etime_i[2],
+			etime_i[3],etime_i[4],etime_i[5]);
+		fclose(fp);
+
+		printf("\nNew MBIO Default Control Parameters:\n");
+		printf("format:   %d\n",format);
+		printf("pings:    %d\n",pings);
+		printf("lonflip:  %d\n",lonflip);
+		printf("speed:    %f\n",speedmin);
+		printf("timegap:  %f\n",timegap);
+		printf("bounds: %f %f %f %f\n",
+			bounds[0],bounds[1],bounds[2],bounds[3]);
+		printf("begin time: %d %d %d %d %d %d\n",
+			btime_i[0],btime_i[1],btime_i[2],
+			btime_i[3],btime_i[4],btime_i[5]);
+		printf("end time:   %d %d %d %d %d %d\n\n",
+			etime_i[0],etime_i[1],etime_i[2],
+			etime_i[3],etime_i[4],etime_i[5]);
+		}
+
+	/* else just list the current defaults */
+	else
+		{
+		printf("\nCurrent MBIO Default Control Parameters:\n");
+		printf("format:   %d\n",format);
+		printf("pings:    %d\n",pings);
+		printf("lonflip:  %d\n",lonflip);
+		printf("speed:    %f\n",speedmin);
+		printf("timegap:  %f\n",timegap);
+		printf("bounds: %f %f %f %f\n",
+			bounds[0],bounds[1],bounds[2],bounds[3]);
+		printf("begin time: %d %d %d %d %d %d\n",
+			btime_i[0],btime_i[1],btime_i[2],
+			btime_i[3],btime_i[4],btime_i[5]);
+		printf("end time:   %d %d %d %d %d %d\n\n",
+			etime_i[0],etime_i[1],etime_i[2],
+			etime_i[3],etime_i[4],etime_i[5]);
+		}
+
+	/* print output debug statements */
+	if (verbose >= 2)
+		{
+		fprintf(stderr,"\ndbg2  Program <%s> completed\n",
+			program_name);
+		fprintf(stderr,"dbg2  Ending status:\n");
+		fprintf(stderr,"dbg2       status:  %d\n",status);
+		}
+
+	/* end it all */
+	exit(status);
+}
---
+++ trunk/src/utilities/mbunclean.c
@@ -0,0 +1,511 @@
+/*--------------------------------------------------------------------
+ *    The MB-system:	mbunclean.c	3.00	3/10/93
+ *    $Id: mbunclean.c,v 3.0 1993-05-04 22:17:18 dale Exp $
+ *
+ *    Copyright (c) 1993 by 
+ *    D. W. Caress (caress@lamont.ldgo.columbia.edu)
+ *    and D. N. Chayes (dale@lamont.ldgo.columbia.edu)
+ *    Lamont-Doherty Earth Observatory
+ *    Palisades, NY  10964
+ *
+ *    See README file for copying and redistribution conditions.
+ *--------------------------------------------------------------------*/
+/*
+ * MBUNCLEAN unflags multibeam bathymetry and backscatter data 
+ * which has been flagged as bad by being set negative.
+ * The default input and output streams are stdin and stdout.
+ *
+ * Author:	D. W. Caress
+ * Date:	March 10, 1993
+ * 
+ * $Id: mbunclean.c,v 3.0 1993-05-04 22:17:18 dale Exp $
+ *
+ */
+
+/* standard include files */
+#include <stdio.h>
+#include <math.h>
+#include <strings.h>
+
+/* mbio include files */
+#include "../../include/mb_status.h"
+#include "../../include/mb_format.h"
+
+/*--------------------------------------------------------------------*/
+
+main (argc, argv)
+int argc;
+char **argv; 
+{
+	/* id variables */
+	static char rcs_id[] = "$Id: mbunclean.c,v 3.0 1993-05-04 22:17:18 dale Exp $";
+	static char program_name[] = "MBUNCLEAN";
+	static char help_message[] =  "MBUNCLEAN unflags multibeam bathymetry and backscatter data \nwhich has been flagged as bad by being set negative. \nThe default input and output streams are stdin and stdout.";
+	static char usage_message[] = "mbunclean [-Fformat -Llonflip -V -H  -Iinfile -Ooutfile]";
+
+	/* parsing variables */
+	extern char *optarg;
+	extern int optkind;
+	int	errflg = 0;
+	int	c;
+	int	help = 0;
+	int	flag = 0;
+
+	/* MBIO status variables */
+	int	status;
+	int	verbose = 0;
+	int	error = MB_ERROR_NO_ERROR;
+	char	*message;
+
+	/* MBIO read control parameters */
+	int	format;
+	int	pings;
+	int	lonflip;
+	double	bounds[4];
+	int	btime_i[6];
+	int	etime_i[6];
+	double	btime_d;
+	double	etime_d;
+	double	speedmin;
+	double	timegap;
+	char	ifile[128];
+	int	beams_bath;
+	int	beams_back;
+	char	*imbio_ptr;
+
+	/* MBIO write control parameters */
+	char	ofile[128];
+	char	*ombio_ptr;
+
+	/* mbio read and write values */
+	char	*store_ptr;
+	int	kind;
+	int	time_i[6];
+	double	time_d;
+	double	navlon;
+	double	navlat;
+	double	speed;
+	double	heading;
+	double	distance;
+	int	*bath;
+	int	*bathdist;
+	int	*back;
+	int	*backdist;
+	int	idata = 0;
+	int	icomment = 0;
+	int	odata = 0;
+	int	ocomment = 0;
+	int	unflag = 0;
+	int	data_use;
+	char	comment[256];
+
+	/* time, user, host variables */
+	long int	right_now;
+	char	date[25], user[128], host[128];
+
+	int	i, j, k, l, m;
+
+	/* get current default values */
+	status = mb_defaults(verbose,&format,&pings,&lonflip,bounds,
+		btime_i,etime_i,&speedmin,&timegap);
+
+	/* reset all defaults but the format and lonflip */
+	pings = 1;
+	bounds[0] = -360.;
+	bounds[1] = 360.;
+	bounds[2] = -90.;
+	bounds[3] = 90.;
+	btime_i[0] = 1962;
+	btime_i[1] = 2;
+	btime_i[2] = 21;
+	btime_i[3] = 10;
+	btime_i[4] = 30;
+	btime_i[5] = 0;
+	etime_i[0] = 2062;
+	etime_i[1] = 2;
+	etime_i[2] = 21;
+	etime_i[3] = 10;
+	etime_i[4] = 30;
+	etime_i[5] = 0;
+	speedmin = 0.0;
+	timegap = 1000000000.0;
+
+	/* set default input and output */
+	strcpy (ifile, "stdin");
+	strcpy (ofile, "stdout");
+
+	/* process argument list */
+	while ((c = getopt(argc, argv, "VvHhF:f:L:l:I:i:O:o:")) != -1)
+	  switch (c) 
+		{
+		case 'H':
+		case 'h':
+			help++;
+			break;
+		case 'V':
+		case 'v':
+			verbose++;
+			break;
+		case 'F':
+		case 'f':
+			sscanf (optarg,"%d", &format);
+			flag++;
+			break;
+		case 'L':
+		case 'l':
+			sscanf (optarg,"%d", &lonflip);
+			flag++;
+			break;
+		case 'I':
+		case 'i':
+			sscanf (optarg,"%s", ifile);
+			flag++;
+			break;
+		case 'O':
+		case 'o':
+			sscanf (optarg,"%s", ofile);
+			flag++;
+			break;
+		case '?':
+			errflg++;
+		}
+
+	/* if error flagged then print it and exit */
+	if (errflg)
+		{
+		fprintf(stderr,"usage: %s\n", usage_message);
+		fprintf(stderr,"\nProgram <%s> Terminated\n",
+			program_name);
+		exit(MB_FAILURE);
+		}
+
+	/* print starting message */
+	if (verbose == 1)
+		{
+		fprintf(stderr,"\nProgram %s\n",program_name);
+		fprintf(stderr,"Version %s\n",rcs_id);
+		fprintf(stderr,"MB-system Version %s\n",MB_VERSION);
+		}
+
+	/* print starting debug statements */
+	if (verbose >= 2)
+		{
+		fprintf(stderr,"\ndbg2  Program <%s>\n",program_name);
+		fprintf(stderr,"dbg2  Version %s\n",rcs_id);
+		fprintf(stderr,"dbg2  MB-system Version %s\n",MB_VERSION);
+		fprintf(stderr,"dbg2  Control Parameters:\n");
+		fprintf(stderr,"dbg2       verbose:        %d\n",verbose);
+		fprintf(stderr,"dbg2       help:           %d\n",help);
+		fprintf(stderr,"dbg2       data format:    %d\n",format);
+		fprintf(stderr,"dbg2       pings:          %d\n",pings);
+		fprintf(stderr,"dbg2       lonflip:        %d\n",lonflip);
+		fprintf(stderr,"dbg2       bounds[0]:      %f\n",bounds[0]);
+		fprintf(stderr,"dbg2       bounds[1]:      %f\n",bounds[1]);
+		fprintf(stderr,"dbg2       bounds[2]:      %f\n",bounds[2]);
+		fprintf(stderr,"dbg2       bounds[3]:      %f\n",bounds[3]);
+		fprintf(stderr,"dbg2       btime_i[0]:     %d\n",btime_i[0]);
+		fprintf(stderr,"dbg2       btime_i[1]:     %d\n",btime_i[1]);
+		fprintf(stderr,"dbg2       btime_i[2]:     %d\n",btime_i[2]);
+		fprintf(stderr,"dbg2       btime_i[3]:     %d\n",btime_i[3]);
+		fprintf(stderr,"dbg2       btime_i[4]:     %d\n",btime_i[4]);
+		fprintf(stderr,"dbg2       btime_i[5]:     %d\n",btime_i[5]);
+		fprintf(stderr,"dbg2       etime_i[0]:     %d\n",etime_i[0]);
+		fprintf(stderr,"dbg2       etime_i[1]:     %d\n",etime_i[1]);
+		fprintf(stderr,"dbg2       etime_i[2]:     %d\n",etime_i[2]);
+		fprintf(stderr,"dbg2       etime_i[3]:     %d\n",etime_i[3]);
+		fprintf(stderr,"dbg2       etime_i[4]:     %d\n",etime_i[4]);
+		fprintf(stderr,"dbg2       etime_i[5]:     %d\n",etime_i[5]);
+		fprintf(stderr,"dbg2       speedmin:       %f\n",speedmin);
+		fprintf(stderr,"dbg2       timegap:        %f\n",timegap);
+		fprintf(stderr,"dbg2       input file:     %s\n",ifile);
+		fprintf(stderr,"dbg2       output file:    %s\n",ofile);
+		}
+
+	/* if help desired then print it and exit */
+	if (help)
+		{
+		fprintf(stderr,"\n%s\n",help_message);
+		fprintf(stderr,"\nusage: %s\n", usage_message);
+		exit(MB_ERROR_NO_ERROR);
+		}
+
+	/* initialize reading the input multibeam file */
+	if ((status = mb_read_init(
+		verbose,ifile,format,pings,lonflip,bounds,
+		btime_i,etime_i,speedmin,timegap,
+		&imbio_ptr,&btime_d,&etime_d,
+		&beams_bath,&beams_back,&error)) != MB_SUCCESS)
+		{
+		mb_error(verbose,error,&message);
+		fprintf(stderr,"\nMBIO Error returned from function <mb_read_init>:\n%s\n",message);
+		fprintf(stderr,"\nMultibeam File <%s> not initialized for reading\n",ifile);
+		fprintf(stderr,"\nProgram <%s> Terminated\n",
+			program_name);
+		exit(error);
+		}
+
+	/* initialize writing the output multibeam file */
+	if ((status = mb_write_init(
+		verbose,ofile,format,&ombio_ptr,
+		&beams_bath,&beams_back,&error)) != MB_SUCCESS)
+		{
+		mb_error(verbose,error,&message);
+		fprintf(stderr,"\nMBIO Error returned from function <mb_write_init>:\n%s\n",message);
+		fprintf(stderr,"\nMultibeam File <%s> not initialized for writing\n",ofile);
+		fprintf(stderr,"\nProgram <%s> Terminated\n",
+			program_name);
+		exit(error);
+		}
+
+	/* allocate memory for data arrays */
+	status = mb_malloc(verbose,beams_bath*sizeof(int),&bath,&error);
+	status = mb_malloc(verbose,beams_bath*sizeof(int),&bathdist,&error);
+	status = mb_malloc(verbose,beams_back*sizeof(int),&back,&error);
+	status = mb_malloc(verbose,beams_back*sizeof(int),&backdist,&error);
+
+	/* if error initializing memory then quit */
+	if (error != MB_ERROR_NO_ERROR)
+		{
+		mb_error(verbose,error,message);
+		fprintf(stderr,"\nMBIO Error allocating data arrays:\n%s\n",message);
+		fprintf(stderr,"\nProgram <%s> Terminated\n",
+			program_name);
+		exit(error);
+		}
+
+	/* write comments to beginning of output file */
+	kind = MB_DATA_COMMENT;
+	strncpy(comment,"\0",256);
+	sprintf(comment,"This data unflagged by program %s version %s",
+		program_name,rcs_id);
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+	strncpy(comment,"\0",256);
+	sprintf(comment,"MB-system Version %s",MB_VERSION);
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+	right_now = time((long *)0);
+	strncpy(date,"\0",25);
+	right_now = time((long *)0);
+	strncpy(date,ctime(&right_now),24);
+	strcpy(user,getenv("USER"));
+	gethostname(host,128);
+	strncpy(comment,"\0",256);
+	sprintf(comment,"Run by user <%s> on cpu <%s> at <%s>",
+		user,host,date);
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+	strncpy(comment,"\0",256);
+	sprintf(comment,"Control Parameters:");
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+	strncpy(comment,"\0",256);
+	sprintf(comment,"  MBIO data format:   %d",format);
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+	strncpy(comment,"\0",256);
+	sprintf(comment,"  Input file:         %s",ifile);
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+	strncpy(comment,"\0",256);
+	sprintf(comment,"  Output file:        %s",ofile);
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+	strncpy(comment,"\0",256);
+	sprintf(comment,"  Longitude flip:     %d",lonflip);
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+	strncpy(comment,"\0",256);
+	sprintf(comment," ");
+	status = mb_put(verbose,ombio_ptr,kind,
+			time_i,time_d,
+			navlon,navlat,speed,heading,
+			beams_bath,bath,bathdist,
+			beams_back,back,backdist,
+			comment,&error);
+	if (error == MB_ERROR_NO_ERROR) ocomment++;
+
+	/* read and write */
+	while (error <= MB_ERROR_NO_ERROR)
+		{
+		/* read some data */
+		error = MB_ERROR_NO_ERROR;
+		status = MB_SUCCESS;
+		status = mb_get_all(verbose,imbio_ptr,&store_ptr,&kind,
+			time_i,&time_d,&navlon,&navlat,&speed,
+			&heading,&distance,
+			&beams_bath,bath,bathdist,
+			&beams_back,back,backdist,
+			comment,&error);
+
+		/* increment counter */
+		if (error <= MB_ERROR_NO_ERROR 
+			&& kind == MB_DATA_DATA)
+			idata = idata + pings;
+		else if (error <= MB_ERROR_NO_ERROR 
+			&& kind == MB_DATA_COMMENT)
+			icomment++;
+
+		/* time gaps do not matter to mbunclean */
+		if (error == MB_ERROR_TIME_GAP)
+			{
+			status = MB_SUCCESS;
+			error = MB_ERROR_NO_ERROR;
+			}
+
+		/* output error messages */
+		if (verbose >= 1 && error == MB_ERROR_COMMENT)
+			{
+			if (icomment == 1)
+				fprintf(stderr,"\nComments:\n");
+			fprintf(stderr,"%s\n",comment);
+			}
+		else if (verbose >= 1 && error < MB_ERROR_NO_ERROR
+			&& error >= MB_ERROR_OTHER)
+			{
+			mb_error(verbose,error,&message);
+			fprintf(stderr,"\nNonfatal MBIO Error:\n%s\n",message);
+			fprintf(stderr,"Input Record: %d\n",idata);
+			fprintf(stderr,"Time: %d %d %d %d %d %d\n",
+				time_i[0],time_i[1],time_i[2],
+				time_i[3],time_i[4],time_i[5]);
+			}
+		else if (verbose >= 1 && error < MB_ERROR_NO_ERROR)
+			{
+			mb_error(verbose,error,&message);
+			fprintf(stderr,"\nNonfatal MBIO Error:\n%s\n",message);
+			fprintf(stderr,"Number of good records so far: %d\n",idata);
+			}
+		else if (verbose >= 1 && error != MB_ERROR_NO_ERROR 
+			&& error != MB_ERROR_EOF)
+			{
+			mb_error(verbose,error,&message);
+			fprintf(stderr,"\nFatal MBIO Error:\n%s\n",message);
+			fprintf(stderr,"Last Good Time: %d %d %d %d %d %d\n",
+				time_i[0],time_i[1],time_i[2],
+				time_i[3],time_i[4],time_i[5]);
+			}
+
+		/* process some data */
+		data_use = MB_NO;
+		if (kind == MB_DATA_DATA
+			&& error == MB_ERROR_NO_ERROR)
+			{
+			for (i=0;i<beams_bath;i++)
+				if (bath[i] < 0)
+					{
+					bath[i] = -bath[i];
+					data_use = MB_YES;
+					unflag++;
+					}
+			for (i=0;i<beams_back;i++)
+				if (back[i] < 0)
+					{
+					back[i] = -back[i];
+					data_use = MB_YES;
+					unflag++;
+					}
+			}
+
+		/* write some data */
+		if (error == MB_ERROR_NO_ERROR
+			|| kind == MB_DATA_COMMENT)
+			{
+			status = mb_put_all(verbose,ombio_ptr,
+					store_ptr,data_use,kind,
+					time_i,time_d,
+					navlon,navlat,speed,heading,
+					beams_bath,bath,bathdist,
+					beams_back,back,backdist,
+					comment,&error);
+			if (status == MB_SUCCESS)
+				{
+				if (kind == MB_DATA_DATA)
+					odata++;
+				else if (kind == MB_DATA_COMMENT)
+					ocomment++;
+				}
+			else
+				{
+				mb_error(verbose,error,&message);
+				fprintf(stderr,"\nMBIO Error returned from function <mb_put>:\n%s\n",message);
+				fprintf(stderr,"\nMultibeam Data Not Written To File <%s>\n",ofile);
+				fprintf(stderr,"Output Record: %d\n",odata+1);
+				fprintf(stderr,"Time: %d %d %d %d %d %d\n",
+					time_i[0],time_i[1],time_i[2],
+					time_i[3],time_i[4],time_i[5]);
+				fprintf(stderr,"\nProgram <%s> Terminated\n",
+					program_name);
+				exit(error);
+				}
+			}
+		}
+
+	/* close the files */
+	status = mb_close(verbose,imbio_ptr,&error);
+	status = mb_close(verbose,ombio_ptr,&error);
+
+	/* deallocate memory for data arrays */
+	mb_free(verbose,bath,&error); 
+	mb_free(verbose,bathdist,&error); 
+	mb_free(verbose,back,&error); 
+	mb_free(verbose,backdist,&error); 
+
+	/* check memory */
+	if (verbose >= 4)
+		status = mb_memory_list(verbose,&error);
+
+	/* give the statistics */
+	if (verbose >= 1)
+		{
+		fprintf(stderr,"\n%d input data records\n",idata);
+		fprintf(stderr,"%d input comment records\n",icomment);
+		fprintf(stderr,"%d output data records\n",odata);
+		fprintf(stderr,"%d output comment records\n",ocomment);
+		fprintf(stderr,"%d beams unflagged\n",unflag);
+		}
+
+	/* end it all */
+	exit(status);
+}
+/*--------------------------------------------------------------------*/
