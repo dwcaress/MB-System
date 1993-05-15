a45cc03e79604bc4377ff359b9eebeda9b29c11a
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
1993-05-15 14:40:30 +0000

    removed excess rcs_id messages

--- trunk/src/mbio/mb_time.c
+++ trunk/src/mbio/mb_time.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------
  *    The MB-system:	mb_time.c	3.00	1/21/93
- *    $Id: mb_time.c,v 3.1 1993-05-14 22:43:31 sohara Exp $
+ *    $Id: mb_time.c,v 3.2 1993-05-15 14:40:30 caress Exp $
  *
  *    Copyright (c) 1993 by 
  *    D. W. Caress (caress@lamont.ldgo.columbia.edu)
@@ -18,6 +18,9 @@
  * Date:	January 21, 1993
  *
  * $Log: not supported by cvs2svn $
+ * Revision 3.1  1993/05/14  22:43:31  sohara
+ * fixed rcs_id message
+ *
  * Revision 3.0  1993/04/23  19:00:00  dale
  * Initial version
  *
@@ -40,6 +43,7 @@
 #define MININSECOND 0.0166667
 #define SECONDINMIN 60.0
 int	jday[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
+static char rcs_id[]="$Id: mb_time.c,v 3.2 1993-05-15 14:40:30 caress Exp $";
 
 /*--------------------------------------------------------------------*/
 /* 	function mb_get_time returns the number of minutes from
@@ -49,7 +53,6 @@ int verbose;
 int time_i[6];
 double *time_d;
 {
-  static char rcs_id[]="$Id: mb_time.c,v 3.1 1993-05-14 22:43:31 sohara Exp $";
   char	*function_name = "mb_get_time";
 	int	status;
 	int	julday;
@@ -101,7 +104,6 @@ int mb_get_date(verbose,time_d,time_i)
 double time_d;
 int time_i[6];
 {
-  char rcs_id[]="$Id";
 
 	char	*function_name = "mb_get_date";
 	int	status;
@@ -164,7 +166,6 @@ int verbose;
 int time_i[6];
 int time_j[4];
 {
-  char rcs_id[]="$Id";
 	char	*function_name = "mb_get_jtime";
 	int	status;
 	int	leapday;
@@ -221,7 +222,6 @@ int verbose;
 int time_j[4];
 int time_i[6];
 {
-  char rcs_id[]="$Id";
 	char	*function_name = "mb_get_itime";
 	int	status;
 	int	leapday;
