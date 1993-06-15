a133d710d20710dabaa8b16c1f8a9dac9cbec1aa
caress <caress@mbari.org>
1993-06-15 16:00:29 +0000

    Fixed bug which caused mbmerge to change non-survey data records into survey data records.  Code change is on line 142 which used to read:      mb_io_ptr->new_kind = MB_DATA_DATA; but now reads:      mb_io_ptr->new_kind = kind;

--- src/mbio/mb_put_all.c
+++ src/mbio/mb_put_all.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------
  *    The MB-system:	mb_put_all.c	3.00	2/4/93
- *    $Id: mb_put_all.c,v 3.1 1993-05-14 22:37:56 sohara Exp $
+ *    $Id: mb_put_all.c,v 3.2 1993-06-15 16:00:29 caress Exp $
  *
  *    Copyright (c) 1993 by 
  *    D. W. Caress (caress@lamont.ldgo.columbia.edu)
@@ -22,6 +22,9 @@
  * Date:	February 4, 1993
  *
  * $Log: not supported by cvs2svn $
+ * Revision 3.1  1993/05/14  22:37:56  sohara
+ * fixed rcs_id message
+ *
  * Revision 3.0  1993/04/23  18:21:17  dale
  * Initial version
  *
@@ -63,7 +66,7 @@ int	*backdist;
 char	*comment;
 int	*error;
 {
-  static char rcs_id[]="$Id: mb_put_all.c,v 3.1 1993-05-14 22:37:56 sohara Exp $";
+  static char rcs_id[]="$Id: mb_put_all.c,v 3.2 1993-06-15 16:00:29 caress Exp $";
 	char	*function_name = "mb_put_all";
 	int	status = MB_SUCCESS;
 	struct mb_io_struct *mb_io_ptr;
@@ -136,7 +139,7 @@ int	*error;
 	else if (usevalues == MB_YES)
 		{
 		mb_io_ptr->new_error = MB_ERROR_NO_ERROR;
-		mb_io_ptr->new_kind = MB_DATA_DATA;
+		mb_io_ptr->new_kind = kind;
 		if (time_i[0] == 0)
 			mb_get_time(verbose,time_d,mb_io_ptr->new_time_i);
 		else
