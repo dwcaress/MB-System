e5493b8f2a3e2a9d33e64ac2e04a54a8f589305c
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
1993-06-30 02:52:06 +0000

    Changed location of levitus database to /home/hs/packages/Levitus/levitus.annual This is another temporary fix.

--- trunk/src/utilities/mblevitus.c
+++ trunk/src/utilities/mblevitus.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------
  *    The MB-system:	mblevitus.c	3.00	4/15/93
- *    $Id: mblevitus.c,v 3.1 1993-05-16 18:18:10 caress Exp $
+ *    $Id: mblevitus.c,v 3.2 1993-06-30 02:52:06 caress Exp $
  *
  *    Copyright (c) 1993 by 
  *    D. W. Caress (caress@lamont.ldgo.columbia.edu)
@@ -19,6 +19,10 @@
  * Date:	April 15, 1993
  *
  * $Log: not supported by cvs2svn $
+ * Revision 3.1  1993/05/16  18:18:10  caress
+ * Changed location of Levitus annual file to
+ * /usr/local/lib - this is a temporary fix.
+ *
  * Revision 3.0  1993/05/04  22:38:44  dale
  * Inital version.
  *
@@ -43,7 +47,7 @@ main (argc, argv)
 int argc;
 char **argv; 
 {
-	static char rcs_id[] = "$Id: mblevitus.c,v 3.1 1993-05-16 18:18:10 caress Exp $";
+	static char rcs_id[] = "$Id: mblevitus.c,v 3.2 1993-06-30 02:52:06 caress Exp $";
 	static char program_name[] = "MBLEVITUS";
 	static char help_message[] = "MBLEVITUS generates an average water velocity profile for a \nspecified location from the Levitus temperature and salinity database.";
 	static char usage_message[] = "mblevitus [-Rlon/lat -Ooutfile -V -H]";
@@ -58,7 +62,7 @@ char **argv;
 	int	error = MB_ERROR_NO_ERROR;
 
 	/* control parameters */
-	char	*ifile = "/usr/local/lib/levitus.annual";
+	char	*ifile = "/home/hs/packages/Levitus/levitus.annual";
 	char	ofile[128];
 	FILE	*ifp, *ofp, *outfp;
 	int	record_size;
