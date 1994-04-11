8a39e1f693d3ca56eb1b7f9e1a1a7c627e17d210
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
1994-04-11 23:34:42 +0000

    Initial revision

---
+++ trunk/oddsnends/mb_ttimes.c
@@ -0,0 +1,120 @@
+/*--------------------------------------------------------------------
+ *    The MB-system:	mb_ttimes.c	4/9/94
+ *    $Id: mb_ttimes.c,v 1.1 1994-04-11 23:34:41 caress Exp $
+
+ *    Copyright (c) 1994 by 
+ *    D. W. Caress (caress@lamont.ldgo.columbia.edu)
+ *    and D. N. Chayes (dale@lamont.ldgo.columbia.edu)
+ *    Lamont-Doherty Earth Observatory
+ *    Palisades, NY  10964
+ *
+ *    See README file for copying and redistribution conditions.
+ *--------------------------------------------------------------------*/
+/*
+ * mb_ttimes.c calls the appropriate mbsys_ routine for 
+ * extracting travel times, beam angles, and bad data flags from
+ * a stored survey data ping.
+ *
+ * Author:	D. W. Caress
+ * Date:	April 9, 1994
+ *
+ * $Log: not supported by cvs2svn $
+ *
+ *
+ */
+
+/* standard include files */
+#include <stdio.h>
+#include <math.h>
+#include <strings.h>
+
+/* mbio include files */
+#include "../../include/mb_status.h"
+#include "../../include/mb_format.h"
+#include "../../include/mb_io.h"
+
+/*--------------------------------------------------------------------*/
+int mb_ttimes(verbose,mbio_ptr,store_ptr,kind,
+			nbeams,ttimes,angles,flags,error)
+int	verbose;
+char	*mbio_ptr;
+char	*store_ptr;
+int	*kind;
+int	*nbeams;
+double	*ttimes;
+double	*angles;
+int	*flags;
+int	*error;
+{
+  static char rcs_id[]="$Id: mb_ttimes.c,v 1.1 1994-04-11 23:34:41 caress Exp $";
+	char	*function_name = "mb_ttimes";
+	int	status;
+	int	system;
+	struct mb_io_struct *mb_io_ptr;
+
+	/* print input debug statements */
+	if (verbose >= 2)
+		{
+		fprintf(stderr,"\ndbg2  MBIO function <%s> called\n",
+			function_name);
+		fprintf(stderr,"dbg2  Input arguments:\n");
+		fprintf(stderr,"dbg2       verbose:    %d\n",verbose);
+		fprintf(stderr,"dbg2       mb_ptr:     %d\n",mbio_ptr);
+		fprintf(stderr,"dbg2       store_ptr:  %d\n",store_ptr);
+		}
+
+	/* get mbio descriptor */
+	mb_io_ptr = (struct mb_io_struct *) mbio_ptr;
+
+	/* get multibeam system id */
+	system = mb_system_table[mb_io_ptr->format_num];
+
+	/* call the appropriate mbsys_ extraction routine */
+	if (system == MB_SYS_SB)
+		{
+		status = mbsys_sb_ttimes(verbose,mbio_ptr,store_ptr,
+				kind,nbeams,ttimes,angles,flags,error);
+		}
+	else if (system == MB_SYS_HSDS)
+		{
+		status = mbsys_hsds_ttimes(verbose,mbio_ptr,store_ptr,
+				kind,nbeams,ttimes,angles,flags,error);
+		}
+	else if (system == MB_SYS_SB2100)
+		{
+		status = mbsys_sb2100_ttimes(verbose,mbio_ptr,store_ptr,
+				kind,nbeams,ttimes,angles,flags,error);
+		}
+	else if (system == MB_SYS_EM12)
+		{
+		status = mbsys_em12_ttimes(verbose,mbio_ptr,store_ptr,
+				kind,nbeams,ttimes,angles,flags,error);
+		}
+	else if (system == MB_SYS_LDEOIH)
+		{
+		status = mbsys_ldeoih_ttimes(verbose,mbio_ptr,store_ptr,
+				kind,nbeams,ttimes,angles,flags,error);
+		}
+	else
+		{
+		status = MB_FAILURE;
+		*error = MB_ERROR_BAD_FORMAT;
+		}
+
+	/* print output debug statements */
+	if (verbose >= 2)
+		{
+		fprintf(stderr,"\ndbg2  MBIO function <%s> completed\n",
+			function_name);
+		fprintf(stderr,"dbg2  Return values:\n");
+		fprintf(stderr,"dbg2       kind:       %d\n",*kind);
+		fprintf(stderr,"dbg2       nbeams:     %d\n",*nbeams);
+		fprintf(stderr,"dbg2       error:      %d\n",*error);
+		fprintf(stderr,"dbg2  Return status:\n");
+		fprintf(stderr,"dbg2       status:  %d\n",status);
+		}
+
+	/* return status */
+	return(status);
+}
+/*--------------------------------------------------------------------*/
