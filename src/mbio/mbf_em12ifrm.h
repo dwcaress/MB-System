b7b6ec67a08c113c9012bfaa2dcdb03d2af244b6
caress <caress@mbari.org>
2000-12-10 20:24:25 +0000

    Initial revision.

---
+++ src/mbio/mbf_em12ifrm.h
@@ -0,0 +1,239 @@
+/*--------------------------------------------------------------------
+ *    The MB-system:	mbf_em12ifrm.h	12/5/00
+ *	$Id: mbf_em12ifrm.h,v 5.0 2000-12-10 20:24:25 caress Exp $
+ *
+ *    Copyright (c) 2000 by
+ *    David W. Caress (caress@mbari.org)
+ *      Monterey Bay Aquarium Research Institute
+ *      Moss Landing, CA 95039
+ *    and Dale N. Chayes (dale@ldeo.columbia.edu)
+ *      Lamont-Doherty Earth Observatory
+ *      Palisades, NY 10964
+ *
+ *    See README file for copying and redistribution conditions.
+ *--------------------------------------------------------------------*/
+/*
+ * mbf_em12ifrm.h defines the data structures used by MBIO functions
+ * to store multibeam data read from the MBF_EM12IFRM format (MBIO id 58).  
+ *
+ * Author:	D. W. Caress
+ * Date:	December 5, 2000
+ * $Log: not supported by cvs2svn $
+ *
+ *
+ */
+/*
+ * Notes on the MBF_EM12IFRM data format:
+ *   1. IFREMER uses their own software called TRISMUS
+ *      to process multibeam data. This package recasts
+ *      the data as parallel files for bathymetry (.SO suffix), 
+ *      imagery (.IM suffix), and asynchronous navigation 
+ *      (.NA suffix). This format supports IFREMER EM12S and EM12D
+ *      data in the TRISMUS format. We recommend that users
+ *      translate the TRISMUS data to the Simrad vendor format
+ *      before processing.
+ *   2. The systems of interest:
+ *         EM-12S:   Deep water 12kHz multibeam sonar with up to 
+ *                   81 beams of bathymetry and up to 523 sidescan 
+ *                   samples per bathymetry beam.
+ *         EM-12D:   Deep water 12kHz multibeam sonar with up to 
+ *                   81 beams of bathymetry and up to 523 sidescan 
+ *                   samples per bathymetry beam. This is a dual
+ *                   system which produces alternating port and
+ *                   starboard pings to provide a 150 degree swath.
+ *   3. The navigation data contain NMEA type ascii strings
+ *      beginning with "$CASTM". The sources of these strings
+ *      include "NACOU", "NAGP1",  and "NAGP2".
+ *      MB-System uses only the "NACOU" strings.
+ *   4. Each bathymetry record is 1032 bytes long and begins 
+ *      with a 35 character string containing the identifier 
+ *      and time stamp. The first six characters are one of
+ *      the following:
+ *           "$12SOC": EM12S ping
+ *           "$12SOB": EM12D port ping
+ *           "$12SOT": EM12S starboard ping
+ *           "$COMM:": Comment (MB-System only)
+ *   5. The imagery records are of variable length and begin 
+ *      with a 35 character string containing the identifier 
+ *      and time stamp. The first six characters are one of
+ *      the following:
+ *           "$12IMC": EM12S ping
+ *           "$12IMB": EM12D port ping
+ *           "$12IMT": EM12S starboard ping
+ *      As with the Simrad vendor formats, multiple sidescan 
+ *      datagrams are recorded for each ping.
+ *
+ */
+
+/* maximum number of beams and pixels */
+#define	MBF_EM12IFRM_MAXBEAMS	81
+#define	MBF_EM12IFRM_MAXPIXELS	50*MBF_EM12IFRM_MAXBEAMS
+
+struct mbf_em12ifrm_struct
+	{
+	/* type of data record */
+	int	kind;			/* Data vs Comment */
+
+	/* type of sonar */
+	int	sonar;			/* Type of Simrad sonar */
+
+	/* parameter info (start, stop and parameter datagrams) */
+	int	par_year;
+	int	par_month;
+	int	par_day;
+	int	par_hour;
+	int	par_minute;
+	int	par_second;
+	int	par_centisecond;
+	int	pos_type;	/* positioning system type */
+	double	pos_delay;	/* positioning system delay (sec) */
+	double	roll_offset;	/* roll offset (degrees) */
+	double	pitch_offset;	/* pitch offset (degrees) */
+	double	heading_offset;	/* heading offset (degrees) */
+	double	em100_td;	/* EM-100 tranducer depth (meters) */
+	double	em100_tx;	/* EM-100 tranducer fore-aft 
+					offset (meters) */
+	double	em100_ty;	/* EM-100 tranducer athwartships 
+					offset (meters) */
+	double	em12_td;	/* EM-12 tranducer depth (meters) */
+	double	em12_tx;	/* EM-12 tranducer fore-aft 
+					offset (meters) */
+	double	em12_ty;	/* EM-12 tranducer athwartships 
+					offset (meters) */
+	double	em1000_td;	/* EM-1000 tranducer depth (meters) */
+	double	em1000_tx;	/* EM-1000 tranducer fore-aft 
+					offset (meters) */
+	double	em1000_ty;	/* EM-1000 tranducer athwartships 
+					offset (meters) */
+	char	spare_parameter[128];
+	int	survey_line;
+	char	comment[80];
+
+	/* position (position datagrams) */
+	int	pos_year;
+	int	pos_month;
+	int	pos_day;
+	int	pos_hour;
+	int	pos_minute;
+	int	pos_second;
+	int	pos_centisecond;
+	double	latitude;
+	double	longitude;
+	double	utm_northing;
+	double	utm_easting;
+	int	utm_zone;
+	double	utm_zone_lon;
+	int	utm_system;
+	int	pos_quality;
+	double	speed;			/* meters/second */
+	double	line_heading;		/* degrees */
+
+	/* sound velocity profile */
+	int	svp_year;
+	int	svp_month;
+	int	svp_day;
+	int	svp_hour;
+	int	svp_minute;
+	int	svp_second;
+	int	svp_centisecond;
+	int	svp_num;
+	int	svp_depth[100]; /* meters */
+	int	svp_vel[100];	/* 0.1 meters/sec */
+
+	/* time stamp */
+	int	year;
+	int	month;
+	int	day;
+	int	hour;
+	int	minute;
+	int	second;
+	int	centisecond;
+
+	/* swath id */
+	int	swath_id;	/* EM_SWATH_CENTER:	0
+				   EM_SWATH_PORT:	-1  (EM12D only)
+				   EM_SWATH_STARBOARD:	1   (EM12D only) */
+
+	/* bathymetry */
+	int	ping_number;
+	int	beams_bath;	/* EM-1000:  60
+				   EM12S:    81
+				   EM12D:    81 */
+	int	bath_mode;	/* EM-1000: 1=deep; 2=medium; 3=shallow 
+				   EM-12S:  1=shallow equiangle spacing
+				            2=deep equiangle spacing
+				            3=shallow equidistant spacing
+				            4=deep 120 degree equidistant
+				            5=deep 105 degree equidistant
+				            6=deep 90 degree equidistant
+				   EM-12D:  1=shallow equiangle spacing
+				            2=deep equiangle spacing
+				            3=shallow equidistant spacing
+				            4=deep 150 degree equidistant
+				            5=deep 140 degree equidistant
+				            6=deep 128 degree equidistant
+				            7=deep 114 degree equidistant
+				            8=deep 98 degree equidistant */
+	int	bath_res;	/* EM-12 only: 1=high res; 2=low res */
+	int	bath_quality;	/* number of good beams, 
+					negative if ping rejected */
+	int	keel_depth;	/* depth of most vertical beam:
+					EM-1000:        0.02 meters 
+					EM-12 high res: 0.10 meters 
+					EM-12 low res:  0.20 meters */
+	int	heading;	/* 0.1 degrees */
+	int	roll;		/* 0.01 degrees */
+	int	pitch;		/* 0.01 degrees */
+	int	xducer_pitch;	/* 0.01 degrees */
+	int	ping_heave;	/* 0.01 meters */
+	int	sound_vel;	/* 0.1 meters/sec */
+	short int bath[MBF_EM12IFRM_MAXBEAMS];	
+				/* depths:
+					EM-1000:        0.02 meters 
+					EM-12 high res: 0.10 meters 
+					EM-12 low res:  0.20 meters */
+	short int bath_acrosstrack[MBF_EM12IFRM_MAXBEAMS];
+				/* acrosstrack distances:
+					EM-1000:         0.1 meters 
+					EM-12 high res:  0.2 meters 
+					EM-12 low res:   0.5 meters */
+	short int bath_alongtrack[MBF_EM12IFRM_MAXBEAMS];
+				/* alongtrack distances:
+					EM-1000:         0.1 meters 
+					EM-12 high res:  0.2 meters 
+					EM-12 low res:   0.5 meters */
+	short int tt[MBF_EM12IFRM_MAXBEAMS];	/* meters */
+				/* travel times:
+					EM-1000:         0.05 msec 
+					EM-12 high res:  0.20 msec 
+					EM-12 low res:   0.80 msec */
+	mb_s_char	amp[MBF_EM12IFRM_MAXBEAMS];	    /* 0.5 dB */
+	mb_u_char	quality[MBF_EM12IFRM_MAXBEAMS];	    /* meters */
+	mb_s_char	heave[MBF_EM12IFRM_MAXBEAMS];	    /* 0.1 meters */
+	
+	/* sidescan */
+	int	pixels_ss;	/* total number of samples for this ping */
+	int	ss_mode;	/* 1 = EM-12 shallow:   0.6 m/sample
+				   2 = EM-12 deep:      2.4 m/sample
+				   3 = EM-1000 deep:    0.3 m/sample
+				   4 = EM-1000 medium:  0.3 m/sample
+				   5 = EM-1000 shallow: 0.15 m/sample */
+	short int beam_frequency[MBF_EM12IFRM_MAXBEAMS]; 
+				/*	0 = 12.67 kHz
+					1 = 13.00 kHz
+					2 = 13.33 kHz
+					3 = 95.00 kHz */
+	short int beam_samples[MBF_EM12IFRM_MAXBEAMS];	
+				/* number of sidescan samples derived from
+					each beam */
+	short int beam_center_sample[MBF_EM12IFRM_MAXBEAMS];
+				/* center beam sample number among samples
+					from one beam */	
+	short int beam_start_sample[MBF_EM12IFRM_MAXBEAMS];
+				/* start beam sample number among samples
+					from entire ping */
+	mb_s_char ss[MBF_EM12IFRM_MAXPIXELS];
+	short int ssp[MBF_EM12IFRM_MAXPIXELS];
+
+};
+
---
+++ src/mbio/mbf_hs10jams.h
@@ -0,0 +1,141 @@
+/*--------------------------------------------------------------------
+ *    The MB-system:	mbf_hs10jams.h	12/4/00
+ *	$Id: mbf_hs10jams.h,v 5.0 2000-12-10 20:24:25 caress Exp $
+ *
+ *    Copyright (c) 2000 by
+ *    David W. Caress (caress@mbari.org)
+ *      Monterey Bay Aquarium Research Institute
+ *      Moss Landing, CA 95039
+ *    and Dale N. Chayes (dale@ldeo.columbia.edu)
+ *      Lamont-Doherty Earth Observatory
+ *      Palisades, NY 10964
+ *
+ *    See README file for copying and redistribution conditions.
+ *--------------------------------------------------------------------*/
+/*
+ * mbf_hs10jams.h defines the data structures used by MBIO functions
+ * to store Furuno HS-10 multibeam sonar data read from the 
+ * MBF_HS10JAMS format.  
+ *
+ * Author:	D. W. Caress
+ * Date:	December 4, 2000
+ *
+ * $Log: not supported by cvs2svn $
+ *
+ */
+/*
+ * Notes on the MBF_HS10JAMS data format:
+ *   1. The Furuno HS-10 multibeam sonar generated 45 beams
+ *      of bathymetry and amplitude.
+ *   2. To my knowledge, only one Furuno HS-10 multibeam sonar
+ *      has been operated. It was installed on S/V Yokosuka,
+ *      a JAMSTEC research vessel. The Furuno HS-10 has since been
+ *      replaced by a SeaBeam 2112 multibeam sonar.
+ *   3. A specification for the raw HS-10 data format was
+ *      provided by JAMSTEC, and is included below. The raw format
+ *      consists of 800 byte binary records in which only the
+ *      lower 4 bits of each byte are used.
+ *   4. The actual data files provided to WHOI seem to be simple
+ *      716 byte ASCII records with time, lat, lon, heading, 
+ *      center beam depth, 45 depths, 45 acrosstrack distances, 
+ *      45 beam amplitudes. Format 171 supports the actual data
+ *      received.
+ *   5. The data received use 5 characters each for depth, 
+ *      acrosstrack, and amplitude values. Null beams have
+ *      depth values of 29999 and acrosstrack values of 99999.
+ *      MB-System supports beam flagging by setting flagged
+ *      beams negative.
+ *   6. The internal data structure supports the data included
+ *      in the format 171 files, and does not yet include values
+ *      listed in the raw format spec but not seen in the data
+ *      provided.
+ *   7. Comment records are supported as an MB-System extension
+ *      where the first two bytes of the 716 byte ASCII records
+ *      are "##"
+ *   8. The raw data format specification is as follows:
+ * 
+ *      ----------------------------------------------------------
+ *      HS-10 MNBES Data Format - JAMSTEC
+ *      
+ *      800 bytes/record, 10 records/block
+ *      
+ *      Note: 4 bits from LSB is effective in each byte.
+ *           zB. 30 30 35 39 ---> 0 0 5 9 (HEX) = 89 (DEC)
+ *               30 30 32 3D ---> 0 0 2 D (HEX) = 45 (DEC)
+ *      The HS-10 processor calculates the water depth by use of
+ *      average sound velocity and by correcting the difference 
+ *      between the true angle of the sound path (obtained by the 
+ *      true sound velocity profile) and the nominal angle of each 
+ *      beam (every 2 degrees). The horizontal distance of the n-th 
+ *      beam is
+ *              Distance(n) = Depth(n) * tan[T(n)],
+ *      where T(n) is the nominal angle of the n-th beam: 
+ *              ( T(n) = 2 * (n-23) degrees, n=1,45 ).
+ *      
+ *      No.  Bytes  Data
+ *       1.    4    Year
+ *       2.    4    Month
+ *       3.    4    Day
+ *       4.    4    Hour
+ *       5.    4    Minute
+ *       6.    4    Second
+ *       7.    8    Latitude in 1/10000 minute
+ *       8.    8    Longitude in 1/10000 minute
+ *       9.    8    X in 1/10 metre
+ *      10.    8    Y in 1/10 metre
+ *      11.    4    Ship's speed in 1/10 knot
+ *      12.    4    Ship's heading in 1/10 degree
+ *      13. 4x45    45 Water depths in metre
+ *      14. 4x45    45 Intensity of reflection in dB
+ *      15.    4    Selection of navigation
+ *                    0:HYB, 1:ANS, 2:MANU(L/L) 3:MANU(X/Y)
+ *      16.    4    Surface sound velocity in 1/10 m/sec
+ *      17.    8    Initial latitude in 1/10000 minute
+ *      18.    8    Initial longitude in 1/10000 minute
+ *      19.    8    Initial X in 1/10 metre
+ *      20.    8    Initial Y in 1/10 metre
+ *      21.    4    Manual bearing in 1/10 degree
+ *      22.    4    Manual ship's speed in 1/10 knot
+ *      23.    4    Ship's draft in 1/10 metre
+ *      24.    4    Offset X in 1/10 metre
+ *      25.    4    Offset Y in 1/10 metre
+ *      26.    4    Selection of sound velocity
+ *                    0:no correction, 1:manual input, 2:calculation correction
+ *      27.    4    Average sound velocity in 1/10 m/sec
+ *      28.    4    Input selection of water temperature
+ *                    0:AUTO, 1:MANUAL
+ *      29.    4    Water temperature in 1/10 degree
+ *      30.    4    Tide level in 1/10 metre
+ *      31. 4x10    10 Depth of layer in metre
+ *      32. 4x10    10 Temperature of layer in 1/10 degree
+ *      33. 4x10    10 Salinity in 1/10 per mille
+ *      34. 4x10    10 Sound velocity in 1/10 m/sec
+ *      35.    4    Transmitted pulse width
+ *                    0:1m, 1:2m, 2:4m, 3:8m
+ *      36.    4    Level of transmission [1-16]
+ *                    1:Off, 16:Max, -2dB in each step
+ *      37.    4    Selection of period of tranmission
+ *                    0:Auto, 1:Manual
+ *      38:    4    Period of tranmission in second
+ *      39:    4    Pre-amp ATT
+ *                    0:OFF, 1:ON
+ *      40:    4    Receiving gain [1-16]
+ *                    1:Off, 16:Max, -2dB in each step
+ *      41.    4    TVG [1-4]
+ *      42.    4    AVG [1-4]
+ *      43.    4    Threshold [1-16]
+ *      44.    4    Gate width (R/L) [1-4]
+ *      45.    4    Gate width (F/B) [1-4]
+ *      46.    4    Selection of beam pattern [1-3]
+ *      47.    4    Interferance removal
+ *                    0:OFF, 1:ON
+ *      48.    4    KP shift [1-32]
+ *      49.    4    Sonar mode [0]
+ *      50.         not used
+ *      ----------------------------------------------------------
+ * 
+ */
+ *   
+ */
+ 
+#define	MBF_HS10JAMS_MAXLINE	716
