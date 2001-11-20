81e12eb73e7d779f753db7ef9c436914e14f9778
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2001-11-20 20:41:55 +0000

    Reset output of comments to not use ##.

--- trunk/src/utilities/mbinfo.c
+++ trunk/src/utilities/mbinfo.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------
  *    The MB-system:	mbinfo.c	2/1/93
- *    $Id: mbinfo.c,v 5.7 2001-11-20 02:00:19 caress Exp $
+ *    $Id: mbinfo.c,v 5.8 2001-11-20 20:41:55 caress Exp $
  *
  *    Copyright (c) 1993, 1994, 2000 by
  *    David W. Caress (caress@mbari.org)
@@ -26,6 +26,9 @@
  * Date:	February 1, 1993
  *
  * $Log: not supported by cvs2svn $
+ * Revision 5.7  2001/11/20  02:00:19  caress
+ * Now prints ## before comments when -C option used.
+ *
  * Revision 5.6  2001/10/25  16:02:55  caress
  * Fixed bug in parsing DRAFT metadata tag.
  *
@@ -175,7 +178,7 @@ struct ping
 
 main (int argc, char **argv)
 {
-	static char rcs_id[] = "$Id: mbinfo.c,v 5.7 2001-11-20 02:00:19 caress Exp $";
+	static char rcs_id[] = "$Id: mbinfo.c,v 5.8 2001-11-20 20:41:55 caress Exp $";
 	static char program_name[] = "MBINFO";
 	static char help_message[] =  "MBINFO reads a swath sonar data file and outputs \nsome basic statistics.  If pings are averaged (pings > 2) \nMBINFO estimates the variance for each of the swath \nbeams by reading a set number of pings (>2) and then finding \nthe variance of the detrended values for each beam. \nThe results are dumped to stdout.";
 	static char usage_message[] = "mbinfo [-Byr/mo/da/hr/mn/sc -C -Eyr/mo/da/hr/mn/sc -Fformat -Ifile -Llonflip -Ppings -Rw/e/s/n -Sspeed -V -H]";
@@ -817,7 +820,7 @@ main (int argc, char **argv)
 						fprintf(output,"\nComments in file %s:\n",file);
 						icomment++;
 						}
-					fprintf(output,"##  %s\n",comment);
+					fprintf(output,"  %s\n",comment);
 					}
 				}
 			
