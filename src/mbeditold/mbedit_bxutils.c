b0ccfe50146a7ee224aa135af4ff63e4f578379e
caress <caress@mbari.org>
2002-01-24 02:11:48 +0000

    Added preprocessor call for DARWIN.

--- src/mbeditold/mbedit_bxutils.c
+++ src/mbeditold/mbedit_bxutils.c
@@ -47,7 +47,7 @@
  */
 #if defined(__cplusplus) || defined(__STDC__) || defined(__EXTENSIONS__)
 #include <stdlib.h>
-#ifndef VMS
+#if !defined(VMS) && !defined(DARWIN)
 #include <malloc.h>
 #endif
 #endif
