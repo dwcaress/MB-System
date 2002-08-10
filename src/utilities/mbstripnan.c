0fcea03bb87c7b1d0868b5af494c79d615150653
caress <caress@mbari.org>
2002-08-10 07:00:21 +0000

    First version from Mike McCann.

---
+++ src/utilities/mbstripnan.c
@@ -0,0 +1,44 @@
+/*--------------------------------------------------------------------
+ *    The MB-system:    mbstripNaN.c        8/8/02
+ *    $Id: mbstripnan.c,v 5.0 2002-08-10 07:00:21 caress Exp $
+ *
+ *    Copyright (c) 2002 by
+ *    Mike McCann (mccann@mbari.org)
+ *    David W. Caress (caress@mbari.org)
+ *      Monterey Bay Aquarium Research Institute
+ *      Moss Landing, CA 95039
+ *    and Dale N. Chayes (dale@ldeo.columbia.edu)
+ *      Lamont-Doherty Earth Observatory
+ *      Palisades, NY 10964
+ *
+ *    See README file for copying and redistribution conditions.
+ *--------------------------------------------------------------------*/
+
+/* mbstripnan.c
+ * Little program to filter output from GMT's grd2xyz removing any nodes 
+ * that have NaN elevations.  Output meant to feed into GMT's surface
+ * in support of the mbm_grd2geovrml macro.
+ *
+ * Mike McCann   8 August 2002
+ * MBARI
+ *
+ * $Log: not supported by cvs2svn $
+ */
+
+#include <stdio.h>
+#include <math.h>
+/*--------------------------------------------------------------------*/
+/*
+ * Read double x,y,z on stdin and send to stdout all but NaNs
+ */
+main () {
+	struct node { double lon, lat, height; };
+	struct node n;
+	while ( ( fread(&n, 24, 1, stdin) > 0 ) ) {
+		if ( ! isnand(n.height) ) {
+			fwrite(&n, 24, 1, stdout);
+		}
+	}
+}
+/*--------------------------------------------------------------------*/
+
