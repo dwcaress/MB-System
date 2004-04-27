e570dfcceef4fda1c8bfc96e0ffc8e171553408b
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2004-04-27 02:59:48 +0000

    Added support for subbottom data.

--- trunk/src/utilities/mbinfo.c
+++ trunk/src/utilities/mbinfo.c
@@ -1,8 +1,8 @@
 /*--------------------------------------------------------------------
  *    The MB-system:	mbinfo.c	2/1/93
- *    $Id: mbinfo.c,v 5.14 2003-04-17 21:18:57 caress Exp $
+ *    $Id: mbinfo.c,v 5.15 2004-04-27 02:59:48 caress Exp $
  *
- *    Copyright (c) 1993, 1994, 2000, 2002, 2003 by
+ *    Copyright (c) 1993, 1994, 2000, 2002, 2004 by
  *    David W. Caress (caress@mbari.org)
  *      Monterey Bay Aquarium Research Institute
  *      Moss Landing, CA 95039
@@ -26,6 +26,9 @@
  * Date:	February 1, 1993
  *
  * $Log: not supported by cvs2svn $
+ * Revision 5.14  2003/04/17 21:18:57  caress
+ * Release 5.0.beta30
+ *
  * Revision 5.13  2002/10/02 23:56:06  caress
  * Release 5.0.beta24
  *
@@ -197,7 +200,7 @@ struct ping
 
 main (int argc, char **argv)
 {
-	static char rcs_id[] = "$Id: mbinfo.c,v 5.14 2003-04-17 21:18:57 caress Exp $";
+	static char rcs_id[] = "$Id: mbinfo.c,v 5.15 2004-04-27 02:59:48 caress Exp $";
 	static char program_name[] = "MBINFO";
 	static char help_message[] =  "MBINFO reads a swath sonar data file and outputs \nsome basic statistics.  If pings are averaged (pings > 2) \nMBINFO estimates the variance for each of the swath \nbeams by reading a set number of pings (>2) and then finding \nthe variance of the detrended values for each beam. \nThe results are dumped to stdout.";
 	static char usage_message[] = "mbinfo [-Byr/mo/da/hr/mn/sc -C -Eyr/mo/da/hr/mn/sc -Fformat -Ifile -Llonflip -Mnx/ny -N -Ppings -Rw/e/s/n -Sspeed -V -H]";
@@ -340,12 +343,14 @@ main (int argc, char **argv)
 	double	timtot = 0.0;
 	double	spdavg = 0.0;
 	int	irec = 0;
+	int	isbtmrec = 0;
 	double	timbegfile = 0.0;
 	double	timendfile = 0.0;
 	double	distotfile = 0.0;
 	double	timtotfile = 0.0;
 	double	spdavgfile = 0.0;
 	int	irecfile = 0;
+	int	isbtmrecfile = 0;
 	int	ntdbeams = 0;
 	int	ngdbeams = 0;
 	int	nzdbeams = 0;
@@ -825,6 +830,7 @@ main (int argc, char **argv)
 
 	/* initialize data arrays */
 	irecfile = 0;
+	isbtmrecfile = 0;
 	distotfile = 0.0;
 	timtotfile = 0.0;
 	spdavgfile = 0.0;
@@ -924,6 +930,14 @@ main (int argc, char **argv)
 				irec++;
 				irecfile++;
 				nread++;
+				}
+
+			/* increment counters */
+			if (pass == 0 
+				&& (error == MB_ERROR_SUBBOTTOM))
+				{
+				isbtmrec++;
+				isbtmrecfile++;
 				}
 
 			/* print comment records */
@@ -1747,7 +1761,9 @@ main (int argc, char **argv)
 	mb_get_jtime(verbose,timbeg_i,timbeg_j);
 	mb_get_jtime(verbose,timend_i,timend_j);
 	fprintf(output,"\nData Totals:\n");
-	fprintf(output,"Number of Records:         %8d\n",irec);
+	fprintf(output,"Number of Records:           %8d\n",irec);
+	if (isbtmrec > 0)
+		fprintf(output,"Number of Subbottom Records: %8d\n",isbtmrec);
 	fprintf(output,"Bathymetry Data (%d beams):\n",beams_bath_max);
 	fprintf(output,"  Number of Beams:         %8d\n",
 		ntdbeams);
