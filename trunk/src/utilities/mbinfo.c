b3ec94cb47f8e222051c2a03ca01c29bc39c8fb3
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2004-09-16 00:59:15 +0000

    Fixed good-nav-only mode.

--- trunk/src/utilities/mbinfo.c
+++ trunk/src/utilities/mbinfo.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------
  *    The MB-system:	mbinfo.c	2/1/93
- *    $Id: mbinfo.c,v 5.16 2004-07-15 19:33:57 caress Exp $
+ *    $Id: mbinfo.c,v 5.17 2004-09-16 00:59:15 caress Exp $
  *
  *    Copyright (c) 1993, 1994, 2000, 2002, 2004 by
  *    David W. Caress (caress@mbari.org)
@@ -26,6 +26,9 @@
  * Date:	February 1, 1993
  *
  * $Log: not supported by cvs2svn $
+ * Revision 5.16  2004/07/15 19:33:57  caress
+ * Improvements to support for Reson 7k data.
+ *
  * Revision 5.15  2004/04/27 02:59:48  caress
  * Added support for subbottom data.
  *
@@ -203,7 +206,7 @@ struct ping
 
 main (int argc, char **argv)
 {
-	static char rcs_id[] = "$Id: mbinfo.c,v 5.16 2004-07-15 19:33:57 caress Exp $";
+	static char rcs_id[] = "$Id: mbinfo.c,v 5.17 2004-09-16 00:59:15 caress Exp $";
 	static char program_name[] = "MBINFO";
 	static char help_message[] =  "MBINFO reads a swath sonar data file and outputs \nsome basic statistics.  If pings are averaged (pings > 2) \nMBINFO estimates the variance for each of the swath \nbeams by reading a set number of pings (>2) and then finding \nthe variance of the detrended values for each beam. \nThe results are dumped to stdout.";
 	static char usage_message[] = "mbinfo [-Byr/mo/da/hr/mn/sc -C -Eyr/mo/da/hr/mn/sc -Fformat -Ifile -Llonflip -Mnx/ny -N -Ppings -Rw/e/s/n -Sspeed -V -H]";
@@ -1265,8 +1268,23 @@ main (int argc, char **argv)
 					sdpbeg = sonardepth;
 					altbeg = altitude;
 					}
-				if (irec == 2 && spdbeg <= 0.0)
-					spdbeg = speed;
+				else if (good_nav_only == MB_YES)
+					{
+					if (lonbeg == 0.0 && latbeg == 0.0
+						&& navlon != 0.0 && navlat != 0.0)
+						{
+						lonbeg = navlon;
+						latbeg = navlat;
+					if (spdbeg == 0.0 && speed != 0.0)
+						spdbeg = speed;
+					if (hdgbeg == 0.0 && heading != 0.0)
+						hdgbeg = heading;
+					if (sdpbeg == 0.0 && sonardepth != 0.0)
+						sdpbeg = sonardepth;
+					if (altbeg == 0.0 && altitude != 0.0)
+						altbeg = altitude;
+						}
+					}
 
 				/* reset ending values each time */
 				if (beams_bath > 0)
