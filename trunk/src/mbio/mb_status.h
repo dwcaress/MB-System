b54abbb2c6cfd0879b3322457f4ace874426d6a9
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2005-02-19 07:09:22 +0000

    Release 5.0.6

--- trunk/src/mbio/mb_status.h
+++ trunk/src/mbio/mb_status.h
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------
  *    The MB-system:	mbio_status.h	2/1/93
- *    $Id: mb_status.h,v 5.52 2005-02-08 23:00:14 caress Exp $
+ *    $Id: mb_status.h,v 5.53 2005-02-19 07:03:14 caress Exp $
  *
  *    Copyright (c) 1993, 1994, 2000, 2002, 2003 by
  *    David W. Caress (caress@mbari.org)
@@ -20,6 +20,9 @@
  * Date:	January 19, 1993
  *
  * $Log: not supported by cvs2svn $
+ * Revision 5.52  2005/02/08 23:00:14  caress
+ * Heading towards 5.0.6 release.
+ *
  * Revision 5.51  2004/11/06 03:55:17  caress
  * Working to support the Reson 7k format.
  *
@@ -268,7 +271,7 @@
 
 /* MB-system version id */
 #define	MB_VERSION	"5.0.6"
-#define	MB_BUILD_DATE	"February 9, 2005"
+#define	MB_BUILD_DATE	"February 19, 2005"
 
 /* MBIO function boolean convention */
 #define	MB_YES	1
