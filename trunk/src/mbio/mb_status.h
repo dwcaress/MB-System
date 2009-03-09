c068592a0a3da50757c29908620339c696f18302
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2009-03-09 15:11:31 +0000

    Release 5.1.2beta01

--- trunk/src/mbio/mb_status.h
+++ trunk/src/mbio/mb_status.h
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------
  *    The MB-system:	mbio_status.h	2/1/93
- *    $Id: mb_status.h,v 5.97 2009-03-06 21:46:47 caress Exp $
+ *    $Id: mb_status.h,v 5.98 2009-03-09 15:11:31 caress Exp $
  *
  *    Copyright (c) 1993-2008 by
  *    David W. Caress (caress@mbari.org)
@@ -20,6 +20,9 @@
  * Date:	January 19, 1993
  *
  * $Log: not supported by cvs2svn $
+ * Revision 5.97  2009/03/06 21:46:47  caress
+ * Release 5.1.2beta01 March 6 2009
+ *
  * Revision 5.96  2008/12/31 08:53:32  caress
  * Updates towards release 5.1.1
  *
@@ -404,7 +407,7 @@
 
 /* MB-system version id */
 #define	MB_VERSION	"5.1.2beta01"
-#define	MB_BUILD_DATE	"6 March 2009"
+#define	MB_BUILD_DATE	"9 March 2009"
 
 /* MBIO function boolean convention */
 #define	MB_YES	1
