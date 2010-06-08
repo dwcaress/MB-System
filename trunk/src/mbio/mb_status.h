23982ee4608c9bd8bbd1b0a566c71bb8c30ccae1
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2010-06-08 00:44:13 +0000

    For release 5.1.3beta1862 on 7 June 2010
    Fixed a number of man page issues.

--- trunk/src/mbio/mb_status.h
+++ trunk/src/mbio/mb_status.h
@@ -412,8 +412,8 @@
 #define MB_STATUS_DEF
 
 /* MB-system version id */
-#define	MB_VERSION	"5.1.3beta1860"
-#define	MB_BUILD_DATE	"28 May 2010"
+#define	MB_VERSION	"5.1.3beta1862"
+#define	MB_BUILD_DATE	"7 June 2010"
 
 /* MBIO function boolean convention */
 #define	MB_YES	1
