6ea2bd6037fa4d81c41e67f9bc3313950782245c
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2011-01-14 20:09:57 +0000

    Making an incremental release 5.2.1882 for internal use at MBARI.

--- trunk/src/mbio/mb_status.h
+++ trunk/src/mbio/mb_status.h
@@ -412,8 +412,8 @@
 #define MB_STATUS_DEF
 
 /* MB-system version id */
-#define	MB_VERSION	"5.2.1880"
-#define	MB_BUILD_DATE	"30 December 2010"
+#define	MB_VERSION	"5.2.1882"
+#define	MB_BUILD_DATE	"14 January 2011"
 
 /* MBIO function boolean convention */
 #define	MB_YES	1
