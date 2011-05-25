b09b814316ce5d0e0fd9c9337e0ea5f149327edc
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2011-05-25 06:09:35 +0000

    Update 5.3.1896
    Fixed mbinfo handling of swath files with no good data
    (Thanks Suzanne O'Hara)
    
    Changed mb_get_format() function in mb_format.c so that formats
    are inferred from parameter files if they exist.
    
    Supplemented informational output to stdout from MBnavadjust.
    
    Started development of new version of format 71 (mbf_ldeoih).

--- trunk/src/mbio/mb_status.h
+++ trunk/src/mbio/mb_status.h
@@ -412,8 +412,8 @@
 #define MB_STATUS_DEF
 
 /* MB-system version id */
-#define	MB_VERSION	"5.3.1895"
-#define	MB_BUILD_DATE	"16 May 2011"
+#define	MB_VERSION	"5.3.1896"
+#define	MB_BUILD_DATE	"25 May 2011"
 
 /* MBIO function boolean convention */
 #define	MB_YES	1
