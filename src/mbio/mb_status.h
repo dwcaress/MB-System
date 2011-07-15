fd90d040b49bf5ec387e24c5e65a21ea4a81395e
caress <caress@mbari.org>
2011-07-15 08:35:36 +0000

    Release 5.3.1902

--- src/mbio/mb_status.h
+++ src/mbio/mb_status.h
@@ -412,8 +412,8 @@
 #define MB_STATUS_DEF
 
 /* MB-system version id */
-#define	MB_VERSION	"5.3.1900"
-#define	MB_BUILD_DATE	"27 June 2011"
+#define	MB_VERSION	"5.3.1902"
+#define	MB_BUILD_DATE	"15 July 2011"
 #define	MB_SVN		"$Id$"
 
 /* MBIO function boolean convention */
