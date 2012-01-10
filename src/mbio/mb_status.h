6f0e1c585e62bc9c551c231a482421060a4935d6
caress <caress@mbari.org>
2012-01-10 08:08:14 +0000

    Preparing for release 5.3.1916

--- src/mbio/mb_status.h
+++ src/mbio/mb_status.h
@@ -412,8 +412,8 @@
 #define MB_STATUS_DEF
 
 /* MB-system version id */
-#define	MB_VERSION	"5.3.1912"
-#define	MB_BUILD_DATE	"19 November 2011"
+#define	MB_VERSION	"5.3.1916"
+#define	MB_BUILD_DATE	"10 January 2012"
 #define	MB_SVN		"$Id$"
 
 /* MBIO function boolean convention */
