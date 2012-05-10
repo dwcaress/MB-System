f0f2e1f3718a4e67924620cd1223b1439b9ec9e8
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2012-05-10 16:51:51 +0000

    Updating proj4 to 4.8.0 release.

--- trunk/src/proj/PJ_sterea.c
+++ trunk/src/proj/PJ_sterea.c
@@ -42,7 +42,7 @@ PROJ_HEAD(sterea, "Oblique Stereographic Alternative")
 FORWARD(e_forward); /* ellipsoid */
 	double cosc, sinc, cosl, k;
 
-	lp = pj_gauss(lp, P->en);
+	lp = pj_gauss(P->ctx, lp, P->en);
 	sinc = sin(lp.phi);
 	cosc = cos(lp.phi);
 	cosl = cos(lp.lam);
@@ -67,10 +67,13 @@ INVERSE(e_inverse); /* ellipsoid */
 		lp.phi = P->phic0;
 		lp.lam = 0.;
 	}
-	return(pj_inv_gauss(lp, P->en));
+	return(pj_inv_gauss(P->ctx, lp, P->en));
 }
 FREEUP; if (P) { if (P->en) free(P->en); free(P); } }
-ENTRY0(sterea)
+ENTRYA(sterea)
+
+        P->en=0; 
+ENTRYX
 	double R;
 
 	if (!(P->en = pj_gauss_ini(P->e, P->phi0, &(P->phic0), &R))) E_ERROR_0;
