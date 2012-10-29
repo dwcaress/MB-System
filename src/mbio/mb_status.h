377f8dc5a2262fb920531d5336a3efbb76896f51
caress <caress@mbari.org>
2012-10-29 19:17:41 +0000

    Updated subversion Id tags on a number of files. Potential release 5.3.1997

--- src/mbio/mb_status.h
+++ src/mbio/mb_status.h
@@ -29,7 +29,7 @@
 #define MB_STATUS_DEF
 
 /* MB-system version id */
-#define	MB_VERSION	"5.3.1996"
+#define	MB_VERSION	"5.3.1997"
 #define	MB_BUILD_DATE	"29 October 2012"
 #define	MB_SVN		"$Id$"
 
