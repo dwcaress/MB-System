d848a66783769409452a6ca9588e1c29acb5cc02
caress <caress@3bb7f1f9-8866-0410-989a-a36d4a1b0156>
2013-01-06 07:29:37 +0000

    Release 5.3.2008

--- trunk/src/mbio/mb_status.h
+++ trunk/src/mbio/mb_status.h
@@ -29,8 +29,8 @@
 #define MB_STATUS_DEF
 
 /* MB-system version id */
-#define	MB_VERSION	"5.3.2007"
-#define	MB_BUILD_DATE	"5 January 2013"
+#define	MB_VERSION	"5.3.2008"
+#define	MB_BUILD_DATE	"6 January 2013"
 #define	MB_SVN		"$Id$"
 
 /* MBIO function boolean convention */
