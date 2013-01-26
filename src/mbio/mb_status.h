c10d55b8c87ff553ed7fc07cf87abf2017e6d4bf
caress <caress@mbari.org>
2013-01-26 00:25:44 +0000

    Release 5.3.2012 Fixed bug in mbkongsbergpreprocess causing seg faults on Linux.

--- src/mbio/mb_status.h
+++ src/mbio/mb_status.h
@@ -29,8 +29,8 @@
 #define MB_STATUS_DEF
 
 /* MB-system version id */
-#define	MB_VERSION	"5.3.2011"
-#define	MB_BUILD_DATE	"17 January 2013"
+#define	MB_VERSION	"5.3.2012"
+#define	MB_BUILD_DATE	"25 January 2013"
 #define	MB_SVN		"$Id$"
 
 /* MBIO function boolean convention */
