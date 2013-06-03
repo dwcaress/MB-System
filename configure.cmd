363a944f682b6863b5f5b50c2bb0769413490cd4
caress <caress@mbari.org>
2013-06-03 21:42:42 +0000

    One of several commits implementing fixes to the autotools build system.

--- configure.cmd
+++ configure.cmd
@@ -21,6 +21,37 @@ autoupdate
 autoreconf --force --install --symlink --warnings=all
 
 #------------------------------------------------------------------------------
+# More notes from Bob Covill
+# May 28, 2013
+#------------------------------------------------------------------------------
+#
+# 1. The actual ./configure script is controlled by a template file
+# configure.in . Any changes to configure should be added there.
+#
+# 2. A number of configure options are saved when you run ./configure to a
+# header file ./src/mbsystem_config.h . Again this has a
+# template ./src/mbsystem_config.h.in .
+#
+# 3. Each folder has a Makefile template Makefile.am.
+#
+# 4. Any changes to the configure system should be done in the above
+# templates. After any of the files are changed the configure system has
+# to be rebuilt with the autoreconf command ...
+autoreconf --force --install --warnings=all
+# This will generate a new configure script and update the Makefile.in
+# files.
+#
+# 5. Once you run a successful ./configure the final Makefiles are
+# generated.
+#
+# 6. After configure you can run the usual ...
+make
+make install
+make clean (to clean up compiled code)
+make distclean (to clean up compiled code and configure output)
+make uninstall (to remove a previously installed version)
+
+#------------------------------------------------------------------------------
 # Configure script command line options:
 #------------------------------------------------------------------------------
 # --prefix=install location for mbsystem (/usr/local/mbsystem)
@@ -67,7 +98,7 @@ sudo ./configure \
 #------------------------------------------------------------------------------
 
 # Build in ~/buildtest
-CFLAGS="-I/usr/X11R6/include -L/usr/X11R6/lib -DDARWIN -DBYTESWAPPED" \
+CFLAGS="-I/usr/X11R6/include -L/usr/X11R6/lib -DBYTESWAPPED" \
 ./configure \
     --prefix=/Users/caress/buildtest \
     --with-netcdf-include=/sw/include \
--- src/mbsystem_config.h
+++ src/mbsystem_config.h
@@ -84,7 +84,7 @@
 #define PACKAGE_NAME "mbsystem"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "mbsystem 3.5.2017"
+#define PACKAGE_STRING "mbsystem 3.5.2084"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "mbsystem"
@@ -93,13 +93,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.5.2017"
+#define PACKAGE_VERSION "3.5.2084"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "3.5.2017"
+#define VERSION "3.5.2084"
 
 /* Turned on WIN32 define in config */
 /* #undef WIN32 */
