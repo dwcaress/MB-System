14e3259ebcd7b3505a9c663e6db5cd0624f5d88e
caress <caress@mbari.org>
2013-06-07 20:11:12 +0000

    Yet another go at getting the build system just right….

--- src/mbio/mb_config.h
+++ src/mbio/mb_config.h
@@ -84,7 +84,7 @@
 #define PACKAGE_NAME "mbsystem"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "mbsystem 5.4.2117"
+#define PACKAGE_STRING "mbsystem 5.4.2120"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "mbsystem"
@@ -93,13 +93,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.4.2117"
+#define PACKAGE_VERSION "5.4.2120"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "5.4.2117"
+#define VERSION "5.4.2120"
 
 /* Set VERSION_DATE define in mb_config.h */
 #define VERSION_DATE "7 June 2013"
