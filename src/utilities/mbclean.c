2d1d164be7e1f79d52b49a4c36d7ac4ca1759363
Kurt Schwehr <schwehr@google.com>
2019-05-27 10:43:37 -0700

    Introduce test/utilities/ mbbackangle_test.py mbclean_test.py mbctdlist_test.py mbdatalist_test.py.
    Also remove extra spaces in usage of mbclean.c.

--- src/utilities/mbclean.c
+++ src/utilities/mbclean.c
@@ -114,12 +114,13 @@ int mbclean_save_edit(int verbose, FILE *sofp, double time_d, int beam, int acti
 
 int main(int argc, char **argv) {
 	char program_name[] = "mbclean";
-	char help_message[] = "Mbclean  identifies  and  flags  artifacts in swath sonar bathymetry data.\n"
-                            "Several algorithms are available for identifying  artifacts;  \n"
-                            "multiple  algorithms  can  be applied in a single pass.\n";
-	char usage_message[] = "mbclean [-Amax -Blow/high -Cslope/unit -Dmin/max \n"
-	                       "\t-Fformat -Gfraction_low/fraction_high -Iinfile -Krange_min \n"
-	                       "\t-Llonflip -Mmode Ntolerance -Ooutfile -Pmin_speed/max_speed -Q -Rmaxheadingrate \n"
+	char help_message[] =
+            "Mbclean identifies and flags artifacts in swath sonar bathymetry data.\n"
+            "Several algorithms are available for identifying artifacts;\n"
+            "multiple algorithms can be applied in a single pass.\n";
+	char usage_message[] = "mbclean [-Amax -Blow/high -Cslope/unit -Dmin/max\n"
+	                       "\t-Fformat -Gfraction_low/fraction_high -Iinfile -Krange_min\n"
+	                       "\t-Llonflip -Mmode Ntolerance -Ooutfile -Pmin_speed/max_speed -Q -Rmaxheadingrate\n"
 	                       "\t-Sspike_slope/mode/format -Ttolerance -Wwest/east/south/north \n"
 	                       "\t-Xbeamsleft/beamsright -Ydistanceleft/distanceright[/mode] -Z\n\t-V -H]\n\n";
 	extern char *optarg;
---
+++ test/utilities/mbcopy_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# Copyright 2019 Google Inc. All Rights Reserved.
+#
+# See README file for copying and redistribution conditions.
+
+"""Tests for mbcopy command line app."""
+
+import os
+import subprocess
+import unittest
+
+
+class MbcopyTest(unittest.TestCase):
+
+  def setUp(self):
+    self.cmd = '../../src/utilities/mbcopy'
+
+  def testNoArgs(self):
+    cmd = [self.cmd]
+    raised = False
+    try:
+      subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+      raised = True
+      self.assertEqual(3, e.returncode)
+      self.assertIn(b'MBIO Error returned from function <mb_format>', e.output)
+      self.assertIn(b'initialization failed', e.output)
+
+    self.assertTrue(raised)
+
+  def testHelp(self):
+    cmd = [self.cmd, '-h']
+    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode()
+    self.assertIn('Version', output)
+    self.assertIn('copies an input swath sonar data file to an output', output)
+    self.assertIn('-Qsleep_factor', output)
+
+  def testHelpVerbose2(self):
+    cmd = [self.cmd, '-h', '-V', '-V']
+    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode()
+    self.assertIn('Version', output)
+    self.assertIn('copies an input swath sonar data file to an output', output)
+    self.assertIn('-Qsleep_factor', output)
+    self.assertIn('dbg2', output)
+    self.assertIn('lonflip', output)
+    self.assertIn('fbtversion:', output)
+
+  # TODO(schwehr): Add tests of actual usage.
+
+
+if __name__ == '__main__':
+  unittest.main()
