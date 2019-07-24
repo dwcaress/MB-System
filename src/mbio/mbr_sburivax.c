e844c964e12ce5b908204063ae078d806f6a25a9
Kurt Schwehr <schwehr@google.com>
2019-07-23 22:28:45 -0700

    mbr_sburivax: Localize variables

--- src/mbio/mbr_sburivax.c
+++ src/mbio/mbr_sburivax.c
@@ -47,8 +47,6 @@ int mbr_info_sburivax(int verbose, int *system, int *beams_bath_max, int *beams_
                       int *traveltime, int *beam_flagging, int *platform_source, int *nav_source, int *sensordepth_source,
                       int *heading_source, int *attitude_source, int *svp_source, double *beamwidth_xtrack,
                       double *beamwidth_ltrack, int *error) {
-	int status = MB_SUCCESS;
-
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  MBIO function <%s> called\n", __func__);
 		fprintf(stderr, "dbg2  Input arguments:\n");
@@ -56,7 +54,6 @@ int mbr_info_sburivax(int verbose, int *system, int *beams_bath_max, int *beams_
 	}
 
 	/* set format info parameters */
-	status = MB_SUCCESS;
 	*error = MB_ERROR_NO_ERROR;
 	*system = MB_SYS_SB;
 	*beams_bath_max = 19;
@@ -81,6 +78,8 @@ int mbr_info_sburivax(int verbose, int *system, int *beams_bath_max, int *beams_
 	*svp_source = MB_DATA_NONE;
 	*beamwidth_xtrack = 2.67;
 	*beamwidth_ltrack = 2.67;
+
+	const int status = MB_SUCCESS;
 
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  MBIO function <%s> completed\n", __func__);
@@ -114,8 +113,6 @@ int mbr_info_sburivax(int verbose, int *system, int *beams_bath_max, int *beams_
 }
 /*--------------------------------------------------------------------*/
 int mbr_alm_sburivax(int verbose, void *mbio_ptr, int *error) {
-	int status = MB_SUCCESS;
-
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  MBIO function <%s> called\n", __func__);
 		fprintf(stderr, "dbg2  Input arguments:\n");
@@ -126,14 +123,11 @@ int mbr_alm_sburivax(int verbose, void *mbio_ptr, int *error) {
 	/* get pointer to mbio descriptor */
 	struct mb_io_struct *mb_io_ptr = (struct mb_io_struct *)mbio_ptr;
 
-	/* set initial status */
-	status = MB_SUCCESS;
-
 	/* allocate memory for data structure */
 	mb_io_ptr->structure_size = sizeof(struct mbf_sburicen_struct);
 	mb_io_ptr->data_structure_size = sizeof(struct mbf_sburicen_data_struct);
-	status = mb_mallocd(verbose, __FILE__, __LINE__, mb_io_ptr->structure_size, &mb_io_ptr->raw_data, error);
-	status = mb_mallocd(verbose, __FILE__, __LINE__, sizeof(struct mbsys_sb_struct), &mb_io_ptr->store_data, error);
+	int status = mb_mallocd(verbose, __FILE__, __LINE__, mb_io_ptr->structure_size, &mb_io_ptr->raw_data, error);
+	status &= mb_mallocd(verbose, __FILE__, __LINE__, sizeof(struct mbsys_sb_struct), &mb_io_ptr->store_data, error);
 
 	/* set record counters to zero */
 	mb_io_ptr->irecord_count = 0;
@@ -151,8 +145,6 @@ int mbr_alm_sburivax(int verbose, void *mbio_ptr, int *error) {
 }
 /*--------------------------------------------------------------------*/
 int mbr_dem_sburivax(int verbose, void *mbio_ptr, int *error) {
-	int status = MB_SUCCESS;
-
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  MBIO function <%s> called\n", __func__);
 		fprintf(stderr, "dbg2  Input arguments:\n");
@@ -164,8 +156,8 @@ int mbr_dem_sburivax(int verbose, void *mbio_ptr, int *error) {
 	struct mb_io_struct *mb_io_ptr = (struct mb_io_struct *)mbio_ptr;
 
 	/* deallocate memory for data descriptor */
-	status = mb_freed(verbose, __FILE__, __LINE__, (void **)&mb_io_ptr->raw_data, error);
-	status = mb_freed(verbose, __FILE__, __LINE__, (void **)&mb_io_ptr->store_data, error);
+	int status = mb_freed(verbose, __FILE__, __LINE__, (void **)&mb_io_ptr->raw_data, error);
+	status &= mb_freed(verbose, __FILE__, __LINE__, (void **)&mb_io_ptr->store_data, error);
 
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  MBIO function <%s> completed\n", __func__);
@@ -179,15 +171,6 @@ int mbr_dem_sburivax(int verbose, void *mbio_ptr, int *error) {
 }
 /*--------------------------------------------------------------------*/
 int mbr_rt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
-	int status = MB_SUCCESS;
-	struct mbf_sburicen_struct *dataplus;
-	struct mbf_sburicen_data_struct *data;
-	struct mbsys_sb_struct *store;
-	char *datacomment;
-	short dummy;
-	double tmplon;
-	int id;
-
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  MBIO function <%s> called\n", __func__);
 		fprintf(stderr, "dbg2  Input arguments:\n");
@@ -200,14 +183,16 @@ int mbr_rt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 	struct mb_io_struct *mb_io_ptr = (struct mb_io_struct *)mbio_ptr;
 
 	/* get pointer to raw data structure */
-	dataplus = (struct mbf_sburicen_struct *)mb_io_ptr->raw_data;
-	data = &(dataplus->data);
-	datacomment = (char *)data;
+	struct mbf_sburicen_struct *dataplus = (struct mbf_sburicen_struct *)mb_io_ptr->raw_data;
+	struct mbf_sburicen_data_struct *data = &(dataplus->data);
+	char *datacomment = (char *)data;
 	dataplus->kind = MB_DATA_DATA;
-	store = (struct mbsys_sb_struct *)store_ptr;
+	struct mbsys_sb_struct *store = (struct mbsys_sb_struct *)store_ptr;
 
 	/* set file position */
 	mb_io_ptr->file_pos = mb_io_ptr->file_bytes;
+
+	int status = MB_SUCCESS;
 
 	/* read next record from file */
 	if ((status = fread(data, 1, mb_io_ptr->data_structure_size, mb_io_ptr->mbfp)) == mb_io_ptr->data_structure_size) {
@@ -226,6 +211,7 @@ int mbr_rt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 	    every five records */
 	if (status == MB_SUCCESS && mb_io_ptr->irecord_count >= 5) {
 		mb_io_ptr->irecord_count = 0;
+		short dummy;
 		fread(&dummy, 1, sizeof(short), mb_io_ptr->mbfp);
 		mb_io_ptr->file_bytes += sizeof(short);
 	}
@@ -279,7 +265,7 @@ int mbr_rt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 		/* position */
         if (data->lon2u < 0) {
             /* if longitude stored with negative values wrap into positive 0-360 domain */
-            tmplon = 360.0 + data->lon2u / 60. + data->lon2b / 600000.;
+            const double tmplon = 360.0 + data->lon2u / 60. + data->lon2b / 600000.;
             data->lon2u = (short)(60.0 * tmplon);
             data->lon2b = (short)(600000.0 * (tmplon - data->lon2u / 60.0));
         }
@@ -296,7 +282,7 @@ int mbr_rt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 
 		/* depths and distances */
 		/* switch order of data as it is read into the global arrays */
-		id = MBSYS_SB_BEAMS - 1;
+		const int id = MBSYS_SB_BEAMS - 1;
 		for (int i = 0; i < MBSYS_SB_BEAMS; i++) {
 			store->dist[id - i] = data->dist[i];
 			store->deph[id - i] = data->deph[i];
@@ -325,14 +311,6 @@ int mbr_rt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 }
 /*--------------------------------------------------------------------*/
 int mbr_wt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
-	int status = MB_SUCCESS;
-	struct mbf_sburicen_struct *dataplus;
-	struct mbf_sburicen_data_struct *data;
-	struct mbsys_sb_struct *store;
-	char *datacomment;
-	short dummy = 0;
-	int id;
-
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  MBIO function <%s> called\n", __func__);
 		fprintf(stderr, "dbg2  Input arguments:\n");
@@ -345,10 +323,10 @@ int mbr_wt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 	struct mb_io_struct *mb_io_ptr = (struct mb_io_struct *)mbio_ptr;
 
 	/* get pointer to raw data structure */
-	dataplus = (struct mbf_sburicen_struct *)mb_io_ptr->raw_data;
-	data = &(dataplus->data);
-	datacomment = (char *)data;
-	store = (struct mbsys_sb_struct *)store_ptr;
+	struct mbf_sburicen_struct *dataplus = (struct mbf_sburicen_struct *)mb_io_ptr->raw_data;
+	struct mbf_sburicen_data_struct *data = &(dataplus->data);
+	char *datacomment = (char *)data;
+	struct mbsys_sb_struct *store = (struct mbsys_sb_struct *)store_ptr;
 
 	/* first set some plausible amounts for some of the
 	    variables in the MBURICEN record */
@@ -376,7 +354,7 @@ int mbr_wt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 			/* depths and distances */
 			/* switch order of data as it is read
 			    into the output arrays */
-			id = MBSYS_SB_BEAMS - 1;
+			const int id = MBSYS_SB_BEAMS - 1;
 			for (int i = 0; i < MBSYS_SB_BEAMS; i++) {
 				data->dist[i] = store->dist[id - i];
 				data->deph[i] = store->deph[id - i];
@@ -396,6 +374,8 @@ int mbr_wt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 			strncat(datacomment, store->comment, MBSYS_SB_MAXLINE - 1);
 		}
 	}
+
+	int status = MB_SUCCESS;
 
 	/* print debug statements */
 	if (verbose >= 5) {
@@ -444,6 +424,7 @@ int mbr_wt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 		    every five records */
 		if (status == MB_SUCCESS && mb_io_ptr->orecord_count >= 5) {
 			mb_io_ptr->orecord_count = 0;
+			short dummy = 0;
 			fwrite(&dummy, 1, sizeof(short), mb_io_ptr->mbfp);
 		}
 	}
@@ -467,8 +448,6 @@ int mbr_wt_sburivax(int verbose, void *mbio_ptr, void *store_ptr, int *error) {
 
 /*--------------------------------------------------------------------*/
 int mbr_register_sburivax(int verbose, void *mbio_ptr, int *error) {
-	int status = MB_SUCCESS;
-
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  MBIO function <%s> called\n", __func__);
 		fprintf(stderr, "dbg2  Input arguments:\n");
@@ -479,7 +458,7 @@ int mbr_register_sburivax(int verbose, void *mbio_ptr, int *error) {
 	struct mb_io_struct *mb_io_ptr = (struct mb_io_struct *)mbio_ptr;
 
 	/* set format info parameters */
-	status = mbr_info_sburivax(
+	const int status = mbr_info_sburivax(
 	    verbose, &mb_io_ptr->system, &mb_io_ptr->beams_bath_max, &mb_io_ptr->beams_amp_max, &mb_io_ptr->pixels_ss_max,
 	    mb_io_ptr->format_name, mb_io_ptr->system_name, mb_io_ptr->format_description, &mb_io_ptr->numfile, &mb_io_ptr->filetype,
 	    &mb_io_ptr->variable_beams, &mb_io_ptr->traveltime, &mb_io_ptr->beam_flagging, &mb_io_ptr->platform_source,
