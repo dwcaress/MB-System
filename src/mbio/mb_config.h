909540c99d027cd29eb1fccddc5dba637733b921
David Caress <caress@haxby.shore.mbari.org>
2019-08-02 16:18:10 -0700

    For release 5.7.6beta4
    Release 5.7.6.beta3 broke one of the Travis CI tests. This update fixes that and will result in a beta release 5.7.6beta4.

--- src/mbio/mb_config.h
+++ src/mbio/mb_config.h
@@ -83,7 +83,7 @@
 #define PACKAGE_NAME "mbsystem"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "mbsystem 5.7.6beta3"
+#define PACKAGE_STRING "mbsystem 5.7.6beta4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "mbsystem"
@@ -92,7 +92,7 @@
 #define PACKAGE_URL "http://www.mbari.org/data/mbsystem/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.7.6beta3"
+#define PACKAGE_VERSION "5.7.6beta4"
 
 /* Build tools using PCL */
 /* #undef PCLTOOLS_ENABLED */
@@ -104,7 +104,7 @@
 /* #undef TEST_ENABLED */
 
 /* Version number of package */
-#define VERSION "5.7.6beta3"
+#define VERSION "5.7.6beta4"
 
 /* Set VERSION_DATE define in mb_config.h */
 #define VERSION_DATE "2 August 2019"
