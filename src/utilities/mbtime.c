2113cfca2c83ebc6d5546695e2a4c02a2e705610
Kurt Schwehr <schwehr@google.com>
2019-09-23 21:36:23 -0700

    mbtime.c - Cleanup
    - errflag and flag to stdbool.h types
    - Remove unused flag variable
    - Localize variables
    - Convert defines to an enum

--- src/utilities/mbtime.c
+++ src/utilities/mbtime.c
@@ -24,6 +24,7 @@
  */
 
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,8 +33,10 @@
 #include "mb_define.h"
 #include "mb_status.h"
 
-#define MBTIME_INPUT_EPOCH 0
-#define MBTIME_INPUT_CALENDAR 1
+typedef enum {
+    MBTIME_INPUT_EPOCH = 0,
+    MBTIME_INPUT_CALENDAR  = 1
+} time_mode_t;
 
 static const char program_name[] = "MBTIME";
 static const char help_message[] =
@@ -47,108 +50,97 @@ static const char usage_message[] =
 /*--------------------------------------------------------------------*/
 
 int main(int argc, char **argv) {
-	int errflg = 0;
-	int c;
-	int help = 0;
-	int flag = 0;
-
-	/* MBIO status variables */
 	int verbose = 0;
-	int error = MB_ERROR_NO_ERROR;
-
-	/* time conversion variables */
-	int mode = MBTIME_INPUT_EPOCH;
+	time_mode_t mode = MBTIME_INPUT_EPOCH;
 	int time_i[7];
 	double time_d = 0.0;
 	double seconds;
 
 	/* process argument list */
-	while ((c = getopt(argc, argv, "VvHhM:m:T:t:")) != -1)
-		switch (c) {
-		case 'H':
-		case 'h':
-			help++;
-			break;
-		case 'M':
-		case 'm':
-			sscanf(optarg, "%lf", &time_d);
-			mode = MBTIME_INPUT_EPOCH;
-			flag++;
-			break;
-		case 'T':
-		case 't':
-			sscanf(optarg, "%d/%d/%d/%d/%d/%lf", &time_i[0], &time_i[1], &time_i[2], &time_i[3], &time_i[4], &seconds);
-			time_i[5] = (int)seconds;
-			time_i[6] = (int)(1000000 * (seconds - time_i[5]));
-			mode = MBTIME_INPUT_CALENDAR;
-			flag++;
-			break;
-		case 'V':
-		case 'v':
-			verbose++;
-			break;
-		case '?':
-			errflg++;
+	{
+		bool errflg = false;
+		int c;
+		bool help = false;
+		while ((c = getopt(argc, argv, "VvHhM:m:T:t:")) != -1)
+			switch (c) {
+			case 'H':
+			case 'h':
+				help = true;
+				break;
+			case 'M':
+			case 'm':
+				sscanf(optarg, "%lf", &time_d);
+				mode = MBTIME_INPUT_EPOCH;
+				break;
+			case 'T':
+			case 't':
+				sscanf(optarg, "%d/%d/%d/%d/%d/%lf", &time_i[0], &time_i[1], &time_i[2], &time_i[3], &time_i[4], &seconds);
+				time_i[5] = (int)seconds;
+				time_i[6] = (int)(1000000 * (seconds - time_i[5]));
+				mode = MBTIME_INPUT_CALENDAR;
+				break;
+			case 'V':
+			case 'v':
+				verbose++;
+				break;
+			case '?':
+				errflg = true;
+			}
+
+		if (errflg) {
+			fprintf(stderr, "usage: %s\n", usage_message);
+			fprintf(stderr, "\nProgram <%s> Terminated\n", program_name);
+			exit(MB_ERROR_BAD_USAGE);
 		}
 
-	/* if error flagged then print it and exit */
-	if (errflg) {
-		fprintf(stderr, "usage: %s\n", usage_message);
-		fprintf(stderr, "\nProgram <%s> Terminated\n", program_name);
-		error = MB_ERROR_BAD_USAGE;
-		exit(error);
-	}
+		if (verbose == 1 || help) {
+			fprintf(stdout, "\nProgram %s\n", program_name);
+			fprintf(stdout, "MB-system Version %s\n", MB_VERSION);
+		}
 
-	if (verbose == 1 || help) {
-		fprintf(stdout, "\nProgram %s\n", program_name);
-		fprintf(stdout, "MB-system Version %s\n", MB_VERSION);
-	}
+		if (verbose >= 2) {
+			fprintf(stderr, "\ndbg2  Program <%s>\n", program_name);
+			fprintf(stderr, "dbg2  MB-system Version %s\n", MB_VERSION);
+			fprintf(stderr, "dbg2  Control Parameters:\n");
+			fprintf(stderr, "dbg2       verbose:    %d\n", verbose);
+			fprintf(stderr, "dbg2       help:       %d\n", help);
+			fprintf(stderr, "dbg2       mode:       %d\n", mode);
+			fprintf(stderr, "dbg2       time_i[0]:  %d\n", time_i[0]);
+			fprintf(stderr, "dbg2       time_i[1]:  %d\n", time_i[1]);
+			fprintf(stderr, "dbg2       time_i[2]:  %d\n", time_i[2]);
+			fprintf(stderr, "dbg2       time_i[3]:  %d\n", time_i[3]);
+			fprintf(stderr, "dbg2       time_i[4]:  %d\n", time_i[4]);
+			fprintf(stderr, "dbg2       time_i[5]:  %d\n", time_i[5]);
+			fprintf(stderr, "dbg2       time_i[6]:  %d\n", time_i[6]);
+			fprintf(stderr, "dbg2       time_d:     %f\n", time_d);
+		}
 
-	if (verbose >= 2) {
-		fprintf(stderr, "\ndbg2  Program <%s>\n", program_name);
-		fprintf(stderr, "dbg2  MB-system Version %s\n", MB_VERSION);
-		fprintf(stderr, "dbg2  Control Parameters:\n");
-		fprintf(stderr, "dbg2       verbose:    %d\n", verbose);
-		fprintf(stderr, "dbg2       help:       %d\n", help);
-		fprintf(stderr, "dbg2       mode:       %d\n", mode);
-		fprintf(stderr, "dbg2       time_i[0]:  %d\n", time_i[0]);
-		fprintf(stderr, "dbg2       time_i[1]:  %d\n", time_i[1]);
-		fprintf(stderr, "dbg2       time_i[2]:  %d\n", time_i[2]);
-		fprintf(stderr, "dbg2       time_i[3]:  %d\n", time_i[3]);
-		fprintf(stderr, "dbg2       time_i[4]:  %d\n", time_i[4]);
-		fprintf(stderr, "dbg2       time_i[5]:  %d\n", time_i[5]);
-		fprintf(stderr, "dbg2       time_i[6]:  %d\n", time_i[6]);
-		fprintf(stderr, "dbg2       time_d:     %f\n", time_d);
-	}
+		if (help) {
+			fprintf(stderr, "\n%s\n", help_message);
+			fprintf(stderr, "\nusage: %s\n", usage_message);
+			exit(MB_ERROR_NO_ERROR);
+		}
 
-	/* if help desired then print it and exit */
-	if (help) {
-		fprintf(stderr, "\n%s\n", help_message);
-		fprintf(stderr, "\nusage: %s\n", usage_message);
-		exit(error);
 	}
 
 	/* convert to calendar time and output */
 	if (mode == MBTIME_INPUT_EPOCH) {
 		mb_get_date(verbose, time_d, time_i);
-		fprintf(stdout, "%4.4d/%2.2d/%2.2d/%2.2d/%2.2d/%2.2d.%6.6d\n", time_i[0], time_i[1], time_i[2], time_i[3], time_i[4],
+		fprintf(stdout, "%4.4d/%2.2d/%2.2d/%2.2d/%2.2d/%2.2d.%6.6d\n",
+			time_i[0], time_i[1], time_i[2], time_i[3], time_i[4],
 		        time_i[5], time_i[6]);
-	}
-
-	/* else convert to epoch time and output */
-	else {
+	} else {
+		/* convert to epoch time and output */
 		mb_get_time(verbose, time_i, &time_d);
 		fprintf(stdout, "%f\n", time_d);
 	}
 
-	const int status = MB_SUCCESS;
-
 	if (verbose >= 2) {
 		fprintf(stderr, "\ndbg2  Program <%s> completed\n", program_name);
 		fprintf(stderr, "dbg2  Ending status:\n");
-		fprintf(stderr, "dbg2       status:  %d\n", status);
+		fprintf(stderr, "dbg2       status:  %d\n", MB_SUCCESS);
 	}
 
-	exit(error);
+	exit(MB_ERROR_NO_ERROR);
 }
 /*--------------------------------------------------------------------*/
