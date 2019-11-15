1b541d2bd15869596a846827533d839317cad32b
David Caress <caress@Haxby.local>
2019-11-14 22:14:44 -0800

    Fixing Travis test builds

--- travis/script.sh
+++ travis/script.sh
@@ -4,6 +4,6 @@ CFLAGS="-O2"
 # TODO(schwehr): Enable more checks.
 # CFLAGS="$CFLAGS -Wall"
 # CFLAGS="$CFLAGS -Wextra"
-CFLAGS="$CFLAGS" ./configure --enable-mbtrn --enable-test
+CFLAGS="$CFLAGS" ./configure --enable-mbtrn --enable-mbtnav --enable-test
 make -j 3
 make check
