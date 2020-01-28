a2946a86ce7bdbfd9c4aed84e5fffe31dbf6c12e
Kurt Schwehr <schwehr@google.com>
2020-01-28 12:20:51 -0800

    pb_math.c: Cleanup
    - Localize variables
    - Add const
    - Flip sense of if for if foo a = b; else return; -> if !foo: return;  a = b;
    - Fix formatting

--- src/surf/pb_math.c
+++ src/surf/pb_math.c
@@ -32,239 +32,201 @@ double setToPlusMinusPI(double angle) {
   if(angle < -PI) {
     angle = setToPlusMinusPI(angle + (2.0*PI));
   }
- return(angle);
+ return angle;
 }
 
 void rotateCoordinates(
     double rotAngle, XY_Coords* origCoords, XY_Coords* targetCoords) {
-  double angle = setToPlusMinusPI(rotAngle);
+  const double angle = setToPlusMinusPI(rotAngle);
   targetCoords->x = origCoords->x * cos(angle) + origCoords->y * sin(angle);
   targetCoords->y = -(origCoords->x * sin(angle)) + origCoords->y * cos(angle);
 }
 
-
-void xyToRhoPhi(double x0,double y0,double pointX,double pointY,
-                                             double* rho,double* phi)
-{
- double x,y,angle;
-
- x = pointX - x0;
- y = pointY - y0;
- *rho = sqrt((x*x) + (y*y));
- angle = pbAtan2(x,y);
- if(angle < 0.0)
-  *phi = angle + (2.0 * PI);
- else
-  *phi = angle;
-}
-
-
-void lambdaPhiToRhoPhi(double x0,double y0,double pointX,double pointY,
-                                             double* rho,double* phi)
-{
- double x,y,angle;
-
- x = pointX - x0;
- y = pointY - y0;
- x = RAD_TO_METER_X(x,y0);
- y = RAD_TO_METER_Y(y);
- *rho = sqrt((x*x) + (y*y));
- angle = pbAtan2(x,y);
- if(angle < 0.0)
-  *phi = angle + (2.0 * PI);
- else
-  *phi = angle;
-}
-
-
-Boolean signf(double value)
-{
- if(value < 0.0) return(False);
- return(True);
-}
-
-
-
-Boolean signsh(short value)
-{
- if(value < 0) return(False);
- return(True);
-}
-
-
-
-
-/* Berechnung von Tiefe und Ablagen aus Traveltime */
-
-Boolean depthFromTT(FanParam* fanParam,Boolean isPitchcompensated)
-{
- double alpha,arg,travelWay,depth,tanAl,tan2Al,tanP,tan2P;
-
- /* aktuellen Winkel Aufgrund cmean rechnen */
-
-  if((fanParam->cmean == 0.0) || (fanParam->ckeel == 0.0)
-     || (fanParam->travelTime == 0.0 )) return (False);
-  arg = (fanParam->cmean / fanParam->ckeel) * sin(fanParam->angle);
-  if((arg < 1.0) && (arg > -1.0))
-     alpha = asin(arg);
+void xyToRhoPhi(
+    double x0, double y0, double pointX, double pointY,
+    double *rho, double *phi) {
+  const double x = pointX - x0;
+  const double y = pointY - y0;
+  *rho = sqrt((x*x) + (y*y));
+  const double angle = pbAtan2(x,y);
+  if(angle < 0.0)
+    *phi = angle + (2.0 * PI);
   else
-     return(False);
+    *phi = angle;
+}
 
- /* Winkelparameter */
-
-  tanAl  = tan(alpha);
-  tan2Al = tanAl * tanAl;
-  if(isPitchcompensated == False)
-  {
-   tanP  = tan(fanParam->pitchTx);
-   tan2P = tanP * tanP;
-  }
+void lambdaPhiToRhoPhi(
+    double x0, double y0, double pointX, double pointY,
+    double *rho,double *phi) {
+  double x = pointX - x0;
+  double y = pointY - y0;
+  x = RAD_TO_METER_X(x,y0);
+  y = RAD_TO_METER_Y(y);
+  *rho = sqrt((x*x) + (y*y));
+  const double angle = pbAtan2(x,y);
+  if(angle < 0.0)
+    *phi = angle + (2.0 * PI);
   else
-  {
-   tanP  = 0.0;
-   tan2P = 0.0;
+    *phi = angle;
+}
+
+Boolean signf(double value) {
+  return value >= 0.0;
+}
+
+Boolean signsh(short value) {
+  return value >= 0;
+}
+
+// Berechnung von Tiefe und Ablagen aus Traveltime
+Boolean depthFromTT(FanParam* fanParam, Boolean isPitchcompensated) {
+  // aktuellen Winkel Aufgrund cmean rechnen
+  if(fanParam->cmean == 0.0 || fanParam->ckeel == 0.0 ||
+     fanParam->travelTime == 0.0)
+    return False;
+
+  const double arg = (fanParam->cmean / fanParam->ckeel) * sin(fanParam->angle);
+
+  if (arg >= 1.0 || arg <= -1.0)
+     return False;
+
+  const double alpha = asin(arg);
+
+  // Winkelparameter
+
+  double tanAl  = tan(alpha);
+  double tan2Al = tanAl * tanAl;
+
+  // TODO(schwehr): Simplify
+  double tanP;
+  double tan2P;
+  if (!isPitchcompensated) {
+    tanP  = tan(fanParam->pitchTx);
+    tan2P = tanP * tanP;
+  } else {
+    tanP  = 0.0;
+    tan2P = 0.0;
   }
 
- /* mittleren rueckgestreuten Weg auf Bezugsniveau HubRx rechnen */
-
-  travelWay = fanParam->travelTime * fanParam->cmean;
-
+  // mittleren rueckgestreuten Weg auf Bezugsniveau HubRx rechnen
+  double travelWay = fanParam->travelTime * fanParam->cmean;
   travelWay = travelWay
             - (((fanParam->heaveTx - fanParam->heaveRx)/2.0) * cos(alpha));
 
- /* Pitchkompensierte Tiefe ohne Huboffset (3D-Pythagoras) */
+  // Pitchkompensierte Tiefe ohne Huboffset (3D-Pythagoras)
 
-  depth = travelWay / sqrt(tan2Al + tan2P + 1);
+  const double depth = travelWay / sqrt(tan2Al + tan2P + 1);
 
 
- /* Positionen */
-
+  // Positionen
   fanParam->posAhead = (depth * tanP)  + fanParam->transducerOffsetAhead;
-
   fanParam->posStar  = (depth * tanAl) + fanParam->transducerOffsetStar;
 
 
- /* Tiefe rechnen */
+  // Tiefe rechnen
+  fanParam->depth = depth              // Pitch-kompensierte Messtiefe
+                  - fanParam->heaveRx  // absolute Hubkompensation
+                  + fanParam->draught; // Tiefgang
 
-  fanParam->depth = depth                                  /* Pitch-kompensierte Messtiefe */
-                  - fanParam->heaveRx                      /* absolute Hubkompensation     */
-                  + fanParam->draught;                     /* Tiefgang                     */
-
-  return(True);
+  return True;
 }
 
+// Berechnung von Traveltime und Ablagen aus Depth
+Boolean TTfromDepth(FanParam *fanParam, Boolean isPitchcompensated) {
+  /* aktuellen Winkel Aufgrund cmean rechnen */
 
+  if(fanParam->cmean == 0.0 || fanParam->ckeel == 0.0 ||
+     fanParam->travelTime == 0.0)
+    return False;
 
+  const double arg = (fanParam->cmean / fanParam->ckeel) * sin(fanParam->angle);
+  if (arg >= 1.0 || arg <= -1.0)
+     return False;
 
-/* Berechnung von Traveltime und Ablagen aus Depth */
-
-Boolean TTfromDepth(FanParam* fanParam,Boolean isPitchcompensated)
-{
- double alpha,arg,travelWay,depth,tanAl,tan2Al,tanP,tan2P;
-
- /* aktuellen Winkel Aufgrund cmean rechnen */
-
-  if((fanParam->cmean == 0.0) || (fanParam->ckeel == 0.0)
-     || (fanParam->travelTime == 0.0 )) return (False);
-  arg = (fanParam->cmean / fanParam->ckeel) * sin(fanParam->angle);
-  if((arg < 1.0) && (arg > -1.0))
-     alpha = asin(arg);
-  else
-     return(False);
+  const double alpha = asin(arg);
 
  /* Winkelparameter */
 
-  tanAl  = tan(alpha);
-  tan2Al = tanAl * tanAl;
-  if(isPitchcompensated == False)
-  {
-   tanP  = tan(fanParam->pitchTx);
-   tan2P = tanP * tanP;
+  const double tanAl = tan(alpha);
+  const double tan2Al = tanAl * tanAl;
+
+  // TODO(schwehr): Simplify
+  double tanP;
+  double tan2P;
+  if (!isPitchcompensated) {
+    tanP  = tan(fanParam->pitchTx);
+    tan2P = tanP * tanP;
+  } else {
+    tanP  = 0.0;
+    tan2P = 0.0;
   }
-  else
-  {
-   tanP  = 0.0;
-   tan2P = 0.0;
-  }
 
- /* Tiefe auf HubRx-Niveau rechnen */
+  // Tiefe auf HubRx-Niveau rechnen
 
-  depth =           fanParam->depth                        /* Pitch-kompensierte Messtiefe */
-                  + fanParam->heaveRx                      /* absolute Hubkompensation     */
-                  - fanParam->draught;                     /* Tiefgang                     */
+  double depth = fanParam->depth       // Pitch-kompensierte Messtiefe
+                 + fanParam->heaveRx   // absolute Hubkompensation
+                 - fanParam->draught;  // Tiefgang
 
- /* Travelway auf HubRx-Niveau (3D-Pythagoras) */
+  // Travelway auf HubRx-Niveau (3D-Pythagoras)
 
-  travelWay = depth * sqrt(tan2Al + tan2P + 1);
+  double travelWay = depth * sqrt(tan2Al + tan2P + 1);
 
- /* mittleren rueckgestreuten Weg auf Bezugsniveau HubRx rechnen */
+  // mittleren rueckgestreuten Weg auf Bezugsniveau HubRx rechnen */
 
   travelWay = travelWay
             + (((fanParam->heaveTx - fanParam->heaveRx)/2.0) * cos(alpha/2.0));
   fanParam->travelTime = travelWay / fanParam->cmean;
 
  /* Positionen */
-
   fanParam->posAhead = (depth * tanP)  + fanParam->transducerOffsetAhead;
-
   fanParam->posStar  = (depth * tanAl) + fanParam->transducerOffsetStar;
 
-
- return(True);
+  return True;
 }
 
 
+// Berechnung von Draught aus Ablage und Depth
 
+Boolean draughtFromDepth(FanParam *fanParam) {
+  // aktuellen Winkel aufgrund cmean rechnen */
+  if(fanParam->cmean == 0.0 || fanParam->ckeel == 0.0) return False;
 
-
-/* Berechnung von Draught aus Ablage und Depth */
-
-Boolean draughtFromDepth(FanParam* fanParam)
-{
- double alpha,arg,fanDepth;
-
- /* aktuellen Winkel aufgrund cmean rechnen */
-
- if((fanParam->cmean == 0.0) || (fanParam->ckeel == 0.0)) return (False);
- arg = (fanParam->cmean / fanParam->ckeel) * sin(fanParam->angle);
- if((arg < 1.0) && (arg > -1.0))
-    alpha = asin(arg);
- else
-    return(False);
-
- /* Faechertiefe */
-
- fanDepth =
-   fabs(fanParam->posStar - fanParam->transducerOffsetStar)/tan(fabs(alpha));
-
- fanParam->draught = fanParam->depth - fanDepth;
- return(True);
-}
-
-/* Berechnung von Heave aus Ablage,Tiefgang und Depth */
-Boolean heaveFromDepth(FanParam* fanParam) {
-  /* aktuellen Winkel aufgrund cmean rechnen */
-
-  if((fanParam->cmean == 0.0) || (fanParam->ckeel == 0.0)) return (False);
   const double arg = (fanParam->cmean / fanParam->ckeel) * sin(fanParam->angle);
-  double alpha;
-  if((arg < 1.0) && (arg > -1.0))
-    alpha = asin(arg);
-  else
+
+  if(arg >= 1.0 || arg <= -1.0)
     return False;
 
+  const double alpha = asin(arg);
+
+  // Faechertiefe
+  double fanDepth =
+   fabs(fanParam->posStar - fanParam->transducerOffsetStar)/tan(fabs(alpha));
+
+  fanParam->draught = fanParam->depth - fanDepth;
+  return True;
+}
+
+// Berechnung von Heave aus Ablage,Tiefgang und Depth
+Boolean heaveFromDepth(FanParam *fanParam) {
+  // aktuellen Winkel aufgrund cmean rechnen
+  if(fanParam->cmean == 0.0 || fanParam->ckeel == 0.0) return False;
+
+  const double arg = (fanParam->cmean / fanParam->ckeel) * sin(fanParam->angle);
+  if(arg >= 1.0 || arg <= -1.0)
+    return False;
+
+  const double alpha = asin(arg);
+
   /* Faechertiefe */
-
   const double fanDepth = (fanParam->posStar - fanParam->transducerOffsetStar)/tan(alpha);
-
   fanParam->heaveTx = - (fanParam->depth - fanDepth - fanParam->draught);
   fanParam->heaveRx = fanParam->heaveTx;
 
   return True;
 }
-/* nach Del Grosso : ....     */
-double cMeanToTemperature(double salinity,double cMean)
-{
+
+// nach Del Grosso: ...
+double cMeanToTemperature(double salinity, double cMean) {
   const double k0 = -(1448.6 + (1.15*(salinity-35.0)) - cMean);
   const double k1 = -4.618;
   const double k2 = 0.0523;
@@ -282,8 +244,7 @@ double cMeanToTemperature(double salinity,double cMean)
   return 0.0;
 }
 
-double temperatureToCMean(double salinity, double temperature)
-{
+double temperatureToCMean(double salinity, double temperature) {
   return
       1448.6
       + (temperature * 4.618)
@@ -291,8 +252,7 @@ double temperatureToCMean(double salinity, double temperature)
       + (1.15 * (salinity - 35.0));
 }
 
-double temperatureToCMeanDelGrosso(double salinity, double temperature)
-{
+double temperatureToCMeanDelGrosso(double salinity, double temperature) {
   return
       1448.6
       + (temperature * 4.618)
@@ -300,8 +260,7 @@ double temperatureToCMeanDelGrosso(double salinity, double temperature)
       + (1.15 * (salinity - 35.0));
 }
 
-double temperatureToCMeanMedwin(double salinity, double temperature)
-{
+double temperatureToCMeanMedwin(double salinity, double temperature) {
   return
       1449.2
       + (temperature * 4.6)
@@ -315,60 +274,56 @@ double temperatureToCMeanMedwin(double salinity, double temperature)
 SurfTime surfTimeOfDayFromAbsTime(SurfTime absTime) {
   const double HOURS_PER_DAY = 3600.0 * 24.0;
 
- while (absTime > (1000.0 * HOURS_PER_DAY))
-   absTime = absTime - (1000.0 * HOURS_PER_DAY);
- while (absTime > (100.0 * HOURS_PER_DAY))
-   absTime = absTime - (100.0 * HOURS_PER_DAY);
- while (absTime > (10.0 * HOURS_PER_DAY))
-   absTime = absTime - (10.0 * HOURS_PER_DAY);
- while (absTime > (HOURS_PER_DAY))
-   absTime = absTime - (HOURS_PER_DAY);
- return(absTime);
+  while (absTime > (1000.0 * HOURS_PER_DAY))
+    absTime = absTime - (1000.0 * HOURS_PER_DAY);
+  while (absTime > (100.0 * HOURS_PER_DAY))
+    absTime = absTime - (100.0 * HOURS_PER_DAY);
+  while (absTime > (10.0 * HOURS_PER_DAY))
+    absTime = absTime - (10.0 * HOURS_PER_DAY);
+  while (absTime > (HOURS_PER_DAY))
+    absTime = absTime - (HOURS_PER_DAY);
+  return absTime;
 }
 
 void timeFromRelTime(SurfTime relTime, char *buffer) {
   short day = 0;
+  while (relTime >= (24.0*3600.0)) {
+    relTime = relTime - (24.0*3600.0);
+    day++;
+  }
+
   short hour = 0;
+  while (relTime >= 3600.0) {
+    relTime = relTime - 3600.0;
+    hour++;
+  }
+
   short min = 0;
+  while (relTime >= 60.0) {
+    relTime = relTime - 60.0;
+    min++;
+  }
+
   short sec = 0;
+  while (relTime > 0.0) {
+    relTime = relTime - 1.0;
+    sec++;
+  }
 
- while (relTime >= (24.0*3600.0))
- {
-  relTime = relTime - (24.0*3600.0);
-  day++;
- }
- while (relTime >= 3600.0)
- {
-  relTime = relTime - 3600.0;
-  hour++;
- }
- while (relTime >= 60.0)
- {
-  relTime = relTime - 60.0;
-  min++;
- }
- while (relTime > 0.0)
- {
-  relTime = relTime - 1.0;
-  sec++;
- }
- if(sec >= 60)
- {
-  min++;
-  sec = 0;
- }
- if(min >= 60)
- {
-  hour++;
-  min = 0;
- }
- if(hour >= 24)
- {
-  day++;
-  hour = 0;
- }
+  if(sec >= 60) {
+    min++;
+    sec = 0;
+  }
+  if(min >= 60) {
+    hour++;
+    min = 0;
+  }
+  if(hour >= 24) {
+    day++;
+    hour = 0;
+  }
 
- sprintf(buffer,"%02d:%02d:%02d",hour,min,sec);
+  sprintf(buffer, "%02d:%02d:%02d", hour, min, sec);
 }
 
 
@@ -380,7 +335,7 @@ Boolean relTimeFromTime(char *buffer, SurfTime *relTime) {
   int hour;
   int min;
   int sec;
-  const int nr = sscanf(buffer,"%2d:%2d:%2d",&hour,&min,&sec);
+  const int nr = sscanf(buffer, "%2d:%2d:%2d", &hour, &min, &sec);
   if (nr != 3) return False;
 
   *relTime = hour * 3600.0 + min * 60.0 + sec;
