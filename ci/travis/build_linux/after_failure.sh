ebdb00be9987f829d8c0feefdac82a8315c80b64
David Caress <caress@Haxby.local>
2020-05-08 01:48:10 -0700

    Undo changes to after_failure.sh scripts

--- ci/travis/build_linux/after_failure.sh
+++ ci/travis/build_linux/after_failure.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-echo ------------------- >> test/utilities/test-suite.log
-echo Test logs: >> test/utilities/test-suite.log
-echo ------------------- >> test/utilities/test-suite.log
-for i in test/utilities/*_test.log
-do
-  echo ------------------- >> test/utilities/test-suite.log
-  cat $i >> test/utilities/test-suite.log
-done
-echo ------------------- >> test/utilities/test-suite.log
-
-
 if [ -e test/utilities/test-suite.log ]; then
     cat test/utilities/test-suite.log
 fi
