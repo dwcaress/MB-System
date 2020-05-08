667c53ce341e2785e0b939d848093bc58ff6e91b
David Caress <caress@Haxby.local>
2020-05-08 01:12:59 -0700

    Tweaking output logs available from Travis-CI tests
    Added individual test logs to the overall log that should be available through the Travis-CI web interface. Specifically augmented files that are cat'd in the ci/travis/build_$/after_failure.sh scripts.

--- ci/travis/build_osx/after_failure.sh
+++ ci/travis/build_osx/after_failure.sh
@@ -4,3 +4,12 @@ set -ex
 if [ -e test/utilities/test-suite.log ]; then
     cat test/utilities/test-suite.log
 fi
+echo -------------------
+echo Test logs:
+echo -------------------
+for i in test/utilities/*_test.log
+do
+  echo -------------------
+  cat $i
+done
+echo -------------------
