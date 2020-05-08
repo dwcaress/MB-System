6d5d905ae1d24ea9d827a326963317fef3cb624f
David Caress <caress@Haxby.local>
2020-05-08 01:36:46 -0700

    Try again to get log info
    Trying again to get the ci/travis/build_$/after_failure.sh scripts to add the needed information to the logs output by Travis-CI

--- ci/travis/build_linux/after_failure.sh
+++ ci/travis/build_linux/after_failure.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
+echo ------------------- >> test/utilities/test-suite.log
+echo Test logs: >> test/utilities/test-suite.log
+echo ------------------- >> test/utilities/test-suite.log
+for i in test/utilities/*_test.log
+do
+  echo ------------------- >> test/utilities/test-suite.log
+  cat $i >> test/utilities/test-suite.log
+done
+echo ------------------- >> test/utilities/test-suite.log
+
+
 if [ -e test/utilities/test-suite.log ]; then
     cat test/utilities/test-suite.log
 fi
-echo -------------------
-echo Test logs:
-echo -------------------
-for i in test/utilities/*_test.log
-do
-  echo -------------------
-  cat $i
-done
-echo -------------------
