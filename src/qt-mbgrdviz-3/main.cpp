3d2a268713f3db40546dc19e87f24d459387e19a
Tom O'Reilly <oreilly@mbari.org>
2020-07-29 16:31:43 -0700

    specify namespace mb_system

--- src/qt-mbgrdviz-3/main.cpp
+++ src/qt-mbgrdviz-3/main.cpp
@@ -5,41 +5,34 @@
 #include <QObjectList>
 #include "MBQuickItem.h"
 
-QQuickWindow *g_rootWindow;
-QQmlApplicationEngine *g_appEngine;
-
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
-    //    qmlRegisterSingletonType<BackEnd>("MbSystem.BackEnd", 1, 0, "BackEnd", BackEnd::qmlInstance);
-    qmlRegisterType<MBQuickItem>("mbsystem.MBQuickItem", 1, 0, "MBQuickItem");
+
+    qmlRegisterType<mb_system::MBQuickItem>("mbsystem.MBQuickItem",
+					    1, 0, "MBQuickItem");
 
     // QQmlApplicationEngine engine;
-    g_appEngine = new QQmlApplicationEngine();
+    mb_system::g_appEngine = new QQmlApplicationEngine();
 
-    g_appEngine->load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (g_appEngine->rootObjects().isEmpty())
+    mb_system::g_appEngine->load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (mb_system::g_appEngine->rootObjects().isEmpty())
         return -1;
 
-    g_rootWindow = qobject_cast<QQuickWindow*>(g_appEngine->rootObjects().value(0));
+    mb_system::g_rootWindow =
+      qobject_cast<QQuickWindow*>(mb_system::g_appEngine->rootObjects().value(0));
 
-
-    
-    /* ***    
-    // Create singleton and register singleton, process command line args
-    if (!MBQuickItem::registerSingleton(argc, argv, g_appEngine)) {
-      return -1;
-    }
-    *** */
     
     // Can we find MBQuickItem objects yet?
-    QObject *object = g_rootWindow->findChild<QObject *>("mbQuickItem");
+    QObject *object =
+      mb_system::g_rootWindow->findChild<QObject *>("mbQuickItem");
     if (!object) {
       qDebug() << "can't find mbQuickItem";
       return -1;
     }
-    MBQuickItem *mbQuickItem = (MBQuickItem *)object;
+    mb_system::MBQuickItem *mbQuickItem =
+      (mb_system::MBQuickItem *)object;
     
     // Stupid-simple command line processing for now.
     // If arguments, last one specifies grid file to load
@@ -53,7 +46,6 @@ int main(int argc, char *argv[])
       QUrl qUrl(QString("file://" + QString(fullPath)));
       mbQuickItem->setGridSurface(qUrl);
     }
-
 
     qDebug("call app.exec");
     return app.exec();
