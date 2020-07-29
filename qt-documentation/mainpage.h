bd5bc20e338d509cde7fdb75d5b2bd2530d93eb4
Tom O'Reilly <oreilly@mbari.org>
2020-07-29 13:57:45 -0700

    initial commit

---
+++ qt-documentation/mainpage.h
@@ -0,0 +1,17 @@
+/**************************************************************************//**
+ * \mainpage MB-System+
+ *
+ * \section intro_sec Introduction
+ *
+ * This prototypes test approaches for the  next major version of MB-System aka "MB-System+"
+ *
+ * \section build_sec Building
+ *
+ * \subsection build_deps Build Dependencies
+ * - g++ package
+ * - build-essential package
+ * - VTK 8.2+
+ * - QT 5.14+
+ *
+ *
+ */
