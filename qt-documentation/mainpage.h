50a832ea727c2bfa486a63d209a16d0386a28c0c
Tom O'Reilly <oreilly@mbari.org>
2020-07-29 17:27:52 -0700

    added content

--- qt-documentation/mainpage.h
+++ qt-documentation/mainpage.h
@@ -3,13 +3,12 @@
  *
  * \section intro_sec Introduction
  *
- * This prototypes test approaches for the  next major version of MB-System aka "MB-System+"
+ * These prototypes test approaches for the next major version of MB-System aka "MB-System+"
  *
  * \section build_sec Building
  *
  * \subsection build_deps Build Dependencies
  * - g++ package
- * - build-essential package
  * - VTK 8.2+
  * - QT 5.14+
  *
