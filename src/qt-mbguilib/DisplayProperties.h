f8520d4f8f44ea8dd8c3e5aa11af93c79a2fdf39
Tom O'Reilly <oreilly@mbari.org>
2020-08-03 08:06:04 -0700

    first commit

---
+++ src/qt-mbguilib/DisplayProperties.h
@@ -0,0 +1,17 @@
+#ifndef DISPLAYPROPERTIES_H
+#define DISPLAYPROPERTIES_H
+
+namespace mb_system {
+  /**
+     DisplayProperties describe surface display options, and are copied between
+     QVtkItem and QVtkRenderer
+  */
+  struct DisplayProperties {
+    /// Draw axes
+    bool drawAxes;
+  };
+
+}
+
+
+#endif
