4e82a87b3d1a582b342e6f5dd08eaa16b8f0631c
Tom O'Reilly <oreilly@mbari.org>
2020-12-01 14:24:01 -0800

    if var is already set, check for its existence

--- build-utils/findCIncludeDir.cmake
+++ build-utils/findCIncludeDir.cmake
@@ -1,16 +1,36 @@
-# Find C include directory with specified name
+# Find specified C include subdirectory directory and write full path name
+# to var. If var already exists, then check for that directory's existence.
+# Otherwise try to find directory 
+# If not found, exit with error
 function(findCIncludeDir directory var)
+  if (${var})
+    message("${var} is already set to ${${var}}; ${directory} better be there!")
+    if (NOT(EXISTS ${${var}}))
+      message("${${var}} doesn't exist")
+      message(FATAL_ERROR
+              "Specify ${directory} C header files directory with -D ${var}=<directory> on command line")        
+    endif()
+    return()
+    
+  endif()
+  message("${var} not yet set")  
   message("***findCIncludeDir(): look for ${directory} in ${CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES} ${CMAKE_SYSTEM_PREFIX_PATH}")
 
-  # Find specified file; REQUIRED parameter means exit if not found
+  # Find specified file
   find_file(${var} ${directory}
             PATHS ${CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES}
             ${CMAKE_SYSTEM_PREFIX_PATH}
-            NO_DEFAULT_PATH
-            REQUIRED)
+              NO_DEFAULT_PATH)
 
-message("***Found ${directory}: ${var}")
-# Set value in parent scope
-set(${var} ${${var}} PARENT_SCOPE)
+  message("***result: ${directory}: ${${var}}")
+  if (${var} MATCHES "-NOTFOUND$")
+    # Couldn't find directory
+    message("Could not find include directory for ${directory}")
+    message(FATAL_ERROR
+            "Specify ${directory} location with -D ${var}=<directory> on command line")  
+  endif()
+
+  # Set value in parent scope
+  set(${var} ${${var}} PARENT_SCOPE)
   
 endfunction()
--- build-utils/findCXXIncludeDir.cmake
+++ build-utils/findCXXIncludeDir.cmake
@@ -1,14 +1,33 @@
-
-# Find C++ include directory with specified name
+# Find specified C++ include subdirectory directory and write full path name
+# to var. If var already exists, then check for that directory's existence.
+# Otherwise try to find directory 
+# If not found, exit with error
 function(findCXXIncludeDir directory var)
   message("***findCXXIncludeDir(): look for ${directory}")
+  if (${var})
+    message("${var} is already set; ${directory} better be there!")
+    if (NOT(EXISTS ${${var}}))
+      message("${${var}} doesn't exist")
+      message(FATAL_ERROR
+              "Specify ${directory} C++ header files directory with -D ${var}=<directory> on command line")        
+    endif()
+    return()
+  endif()
+  
   find_file(${var} ${directory}
             PATHS ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES}
             ${CMAKE_SYSTEM_PREFIX_PATH}
-            NO_DEFAULT_PATH
-            REQUIRED)
+            NO_DEFAULT_PATH)
 
-  message("***Found ${directory}: ${var}")  
+  message("***result: ${directory}: ${${var}}")
+
+  if (${var} MATCHES "-NOTFOUND$")
+    # Couldn't find directory
+    message("Could not find include directory for ${directory}")
+    message(FATAL_ERROR
+            "Specify ${directory} location with -D ${var}=<directory> on command line")  
+  endif()
+  
   set(${var} ${${var}} PARENT_SCOPE)
   
 endfunction()
