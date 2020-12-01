6a3729378b85e287579b0889225af2b9387131e6
Tom O'Reilly <oreilly@mbari.org>
2020-11-30 18:45:38 -0800

    added $CMAKE_SYSTEM_PREFIX_PATH to searched PATHs

--- build-utils/findCIncludeDir.cmake
+++ build-utils/findCIncludeDir.cmake
@@ -1,8 +1,11 @@
 # Find C include directory with specified name
 function(findCIncludeDir directory var)
-  message("***findCIncludeDir(): look for ${directory} in ${CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES}")
+  message("***findCIncludeDir(): look for ${directory} in ${CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES} ${CMAKE_SYSTEM_PREFIX_PATH}")
+
+  # Find specified file; REQUIRED parameter means exit if not found
   find_file(${var} ${directory}
             PATHS ${CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES}
+            ${CMAKE_SYSTEM_PREFIX_PATH}
             NO_DEFAULT_PATH
             REQUIRED)
 
