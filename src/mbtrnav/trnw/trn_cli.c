62918fb510c3d43e26b54ae90c9d56e68eb1821f
Kent Headley <headley@mbari.org>
2021-03-18 20:19:49 -0700

    add full trnsvr API, add NULL checks where missing, centralize core message build/send/recv, refactor composite high level API functions

--- src/mbtrnav/trnw/trn_cli.c
+++ src/mbtrnav/trnw/trn_cli.c
@@ -110,7 +110,7 @@ static int32_t s_trncli_send_recv(trncli_t *self, byte *msg, int32_t len, bool b
     int32_t retval=-1;
     int32_t sret=-1;
     int32_t rret=-1;
-    
+
     msock_set_blocking(self->trn->sock,false);
     if((sret=msock_send(self->trn->sock,msg,len))>0){
         msock_set_blocking(self->trn->sock,block);
@@ -125,12 +125,11 @@ static int32_t s_trncli_send_recv(trncli_t *self, byte *msg, int32_t len, bool b
     return retval;
 }// end function s_trncli_send_recv
 
-trncli_t *trncli_new(trncli_type_id type, long int utm_zone)
+trncli_t *trncli_new(long int utm_zone)
 {
     trncli_t *instance=(trncli_t *)malloc(sizeof(trncli_t));
     if(NULL!=instance){
         memset(instance,0,sizeof(trncli_t));
-        instance->type = type;
         instance->measurement = NULL;
         instance->utm_zone=utm_zone;
         instance->trn = msock_connection_new();
@@ -182,186 +181,17 @@ int trncli_disconnect(trncli_t *self)
    return retval;
 }// end function trncli_disconnect
 
-
-int32_t trncli_init_server(trncli_t *self, trn_config_t *cfg)
-{
-    int32_t retval=-1;
-    
-    byte *msg=NULL;
-    int32_t mlen=0;
-    
-    if( (mlen=trnw_init_msg((char **)&msg, cfg))>0){
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"TX - INIT:\n");
-        trnw_msg_show((char *)msg,true,5);
-#endif
-
-        retval=s_trncli_send_recv(self, msg, mlen, true);
-
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"RX - INIT ret[%"PRId32"]\n",retval);
-        trnw_msg_show((char *)msg,true,5);
-#endif
-        free(msg);
-    }
-    return retval;
-}// end function trncli_init_server
-
-int32_t trncli_update_measurement(trncli_t *self, wmeast_t *meas)
-{
-    int32_t retval=-1;
-  
-    byte *msg=NULL;
-    int32_t mlen=0;
-    if( (mlen=trnw_meas_msg((char **)&msg,meas,TRN_MSG_MEAS,TRN_SENSOR_MB))>0){
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"TX MEAS UPDATE MEAST:\n");
-        wmeast_show(meas, true, 5);
-        fprintf(stderr,"TX MEAS UPDATE MSG:\n");
-        trnw_msg_show((char *)msg,true,5);
-#endif
-        retval=s_trncli_send_recv(self, msg, mlen, true);
-        
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"RX MEAS UPDATE ret[%"PRId32"]\n",retval);
-        trnw_msg_show((char *)msg,true,5);
-#endif
-        free(msg);
-    }
-    return retval;
-}// end function trncli_update_measurement
-
-int32_t trncli_update_motion(trncli_t *self, wposet_t *pose)
-{
-    int32_t retval=-1;
-    
-    byte *msg=NULL;
-    int32_t mlen=0;
-    if( (mlen=trnw_pose_msg((char **)&msg,pose,TRN_MSG_MOTN))>0){
-
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"TX MOTN UPDATE:\n");
-        wposet_show(pose, true, 5);
-        fprintf(stderr,"TX MOTN MSG:\n");
-        trnw_msg_show((char *)msg, true, 5);
-#endif
-        retval=s_trncli_send_recv(self, msg, mlen, true);
-
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"RX MOTN UPDATE ret[%"PRId32"]\n",retval);
-        trnw_msg_show((char *)msg,true,5);
-#endif
-        free(msg);
-    }
-    return retval;
-}// end function trncli_update_motion
-
-int32_t trncli_estimate_pose(trncli_t *self, wposet_t **pose, char msg_type)
-{
-    int32_t retval=-1;
-    
-    byte *msg=NULL;
-    int32_t mlen=0;
-    if( (mlen=trnw_type_msg((char **)&msg,msg_type))>0){
-        
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"TX EST_POSE MSG [%c]:\n",msg_type);
-        trnw_msg_show((char *)msg, true, 5);
-#endif
-        retval=s_trncli_send_recv(self, msg, mlen, true);
-        wposet_msg_to_pose(pose, (char *)msg);
-
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"RX EST_POSE MSG [%c] ret[%"PRId32"]\n",msg_type,retval);
-        trnw_msg_show((char *)msg,true,5);
-        fprintf(stderr,"RX EST_POSE [%c]:\n",msg_type);
-        wposet_show(*pose, true, 5);
-#endif
-        free(msg);
-    }
-    return retval;
-}// end function trncli_update_motion
-
-bool trncli_last_meas_valid(trncli_t *self)
-{
-    bool retval=false;
-    
-    byte *msg=NULL;
-    int32_t mlen=0;
-    ct_cdata_t *lmv_dat=NULL;
-
-    if( (mlen=trnw_type_msg((char **)&msg,TRN_MSG_LAST_MEAS))>0){
-        
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"TX LMV MSG:\n");
-        trnw_msg_show((char *)msg, true, 5);
-#endif
-
-        retval=s_trncli_send_recv(self, msg, mlen, true);
-        if( wcommst_cdata_unserialize(&lmv_dat,(char *)msg)==0 && NULL!=lmv_dat){
-            retval = (lmv_dat->parameter==0?false:true);
-        }
-        
-        wcommst_cdata_destroy(&lmv_dat);
-        
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"RX LMV MSG ret[%"PRId32"]\n",retval);
-        trnw_msg_show((char *)msg,true,5);
-#endif
-        
-        free(msg);
-    }
-    return retval;
-}
-
-int32_t trncli_reinit_filter(trncli_t *self)
-{
-    int32_t retval=-1;
-    
-    byte *msg=NULL;
-    int32_t mlen=0;
-    if( (mlen=trnw_type_msg((char **)&msg,TRN_MSG_SET_FR))>0){
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"TX REINIT FILT:\n");
-        trnw_msg_show((char *)msg,true,5);
-#endif
-        retval=s_trncli_send_recv(self, msg, mlen, true);
-
-#if defined(WITH_PDEBUG)
-        fprintf(stderr,"RX REINIT FILT ret[%"PRId32"]\n",retval);
-        trnw_msg_show((char *)msg,true,5);
-#endif
-        free(msg);
-    }
-    return retval;
-}// end function trncli_reinit_filter
-
-int32_t trncli_ack_server(trncli_t *self)
-{
-    int32_t retval=-1;
-    
-    byte *msg=NULL;
-    int32_t mlen=0;
-    if( (mlen=trnw_ack_msg((char **)&msg))>0){
-        retval=s_trncli_send_recv(self, msg, mlen, true);
-        fprintf(stderr,"%s - ACK ret[%"PRId32"]\n",__FUNCTION__,retval);
-        
-        free(msg);
-    }
-    return retval;
-}// end function trncli_ack_server
-
 int trncli_send_update(trncli_t *self, mb1_t *src, wposet_t **pt_out, wmeast_t **mt_out)
 {
     int retval=-1;
-    
+
     if(NULL!=self && NULL!=src && NULL!=pt_out && NULL!=mt_out){
         int test=-1;
         if( (test=wmeast_mb1_to_meas(mt_out, src, self->utm_zone)) == 0){
-            
+
             if( (test=wposet_mb1_to_pose(pt_out, src, self->utm_zone)) == 0){
                 // must do motion update first if pt time <= mt time
-   
+
                 if(trncli_update_motion(self,*pt_out)>0){
                     if(trncli_update_measurement(self, *mt_out)>0){
                         retval=0;
@@ -374,7 +204,7 @@ int trncli_send_update(trncli_t *self, mb1_t *src, wposet_t **pt_out, wmeast_t *
                     fprintf(stderr,"trncli_update_motion failed [%d]\n",test);
                     retval=errno;
                 }
-                
+
             }else{
                 fprintf(stderr,"wposet_mb1_to_pose failed [%d]\n",test);
             }
@@ -382,7 +212,7 @@ int trncli_send_update(trncli_t *self, mb1_t *src, wposet_t **pt_out, wmeast_t *
             fprintf(stderr,"wmeast_mb1_to_meas failed [%d]\n",test);
         }
     }
-    
+
     return retval;
 }
 
@@ -405,21 +235,21 @@ int trncli_get_bias_estimates(trncli_t *self, wposet_t *pt, pt_cdata_t **pt_out,
 
         }else{
             fprintf(stderr,"trncli_estimate_pose failed [%u]\n",uret);
-           retval=errno;
+            retval=errno;
         }
-     
-        if( trncli_last_meas_valid(self)==true){
-            
+
+        if( trncli_last_meas_succesful(self)==true){
+
             wposet_pose_to_cdata(pt_out, pt);
             wposet_pose_to_cdata(mle_out, mle);
             wposet_pose_to_cdata(mse_out, mse);
-            
-//            fprintf(stderr,"Bias Estimates:\n");
-//            fprintf(stderr,"MLE: [%.2lf,%.4lf,%.4lf,%.4lf]\n",mle_dat->time,
-//                    (mle_dat->x-pt_dat->x),(mle_dat->y-pt_dat->y),(mle_dat->z-pt_dat->z));
-//            fprintf(stderr,"MSE: [%.2lf,%.4lf,%.4lf,%.4lf]\n",mse_dat->time,
-//                    (mse_dat->x-pt_dat->x),(mse_dat->y-pt_dat->y),(mse_dat->z-pt_dat->z));
-//            fprintf(stderr,"COV: [%.2lf,%.2lf,%.2lf]\n",sqrt(mse_dat->covariance[0]),sqrt(mse_dat->covariance[2]),sqrt(mse_dat->covariance[5]));
+
+            //            fprintf(stderr,"Bias Estimates:\n");
+            //            fprintf(stderr,"MLE: [%.2lf,%.4lf,%.4lf,%.4lf]\n",mle_dat->time,
+            //                    (mle_dat->x-pt_dat->x),(mle_dat->y-pt_dat->y),(mle_dat->z-pt_dat->z));
+            //            fprintf(stderr,"MSE: [%.2lf,%.4lf,%.4lf,%.4lf]\n",mse_dat->time,
+            //                    (mse_dat->x-pt_dat->x),(mse_dat->y-pt_dat->y),(mse_dat->z-pt_dat->z));
+            //            fprintf(stderr,"COV: [%.2lf,%.2lf,%.2lf]\n",sqrt(mse_dat->covariance[0]),sqrt(mse_dat->covariance[2]),sqrt(mse_dat->covariance[5]));
 
 
         }else{
@@ -429,23 +259,305 @@ int trncli_get_bias_estimates(trncli_t *self, wposet_t *pt, pt_cdata_t **pt_out,
         wposet_destroy(mse);
 
     }
-    
+
     return retval;
 }
 
+int trncli_ptype_get(trncli_t *self, int msg_type)
+{
+    int retval=-1;
+
+     if(NULL!=self){
+         byte *msg=NULL;
+         int32_t mlen=0;
+         // create a message
+         if( (mlen=trnw_type_msg((char **)&msg,msg_type))>0){
+#if defined(WITH_PDEBUG)
+             fprintf(stderr,"TX MSG:\n");
+             trnw_msg_show((char *)msg, true, 5);
+#endif
+             // send message, get response
+             if(s_trncli_send_recv(self, msg, mlen, true)>0){
+
+                 // deserialize and get return value
+                 ct_cdata_t *ct_dat=NULL;
+                 if( wcommst_cdata_unserialize(&ct_dat,(char *)msg)==0 && NULL!=ct_dat){
+                     if(ct_dat->msg_type==TRN_MSG_ACK){
+	                     retval = ct_dat->parameter;
+                     }else{
+                         fprintf(stderr,"%s: no ACK[%c] \n",__func__,ct_dat->msg_type);
+                     }
+                 }
+                 // release cdata resources
+                 if(NULL!=ct_dat){
+                     wcommst_cdata_destroy(&ct_dat);
+                 }
+             }
+
+             // release message resources
+             if(NULL!=msg){
+                 free(msg);
+             }
+         }
+     }
+    return retval;
+}// end function trncli_ptype_get
+
+int trncli_ptype_set(trncli_t *self, int msg_type, int param)
+{
+    int retval=-1;
+
+    if(NULL!=self){
+        byte *msg=NULL;
+        int32_t mlen=0;
+        // create a message
+        if( (mlen=trnw_ptype_msg((char **)&msg,msg_type,param))>0){
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"TX MSG:\n");
+            trnw_msg_show((char *)msg, true, 5);
+#endif
+            // send message, get response
+            if(s_trncli_send_recv(self, msg, mlen, true)>0){
+
+                // deserialize and get return value
+                ct_cdata_t *ct_dat=NULL;
+                if( wcommst_cdata_unserialize(&ct_dat,(char *)msg)==0 && NULL!=ct_dat){
+                    retval = ( (ct_dat->msg_type==TRN_MSG_ACK) ? 0 : -1);
+                }
+                // release cdata resources
+                if(NULL!=ct_dat){
+                    wcommst_cdata_destroy(&ct_dat);
+                }
+            }
+
+            // release message resources
+            if(NULL!=msg){
+                free(msg);
+            }
+        }
+    }
+    return retval;
+}// end function trncli_ptype_set
+
 int trncli_mb1_to_meas(wmeast_t **dest, mb1_t *src, long int utmZone)
 {
-   return wmeast_mb1_to_meas(dest, src,utmZone);
+    return wmeast_mb1_to_meas(dest, src,utmZone);
 }// end function trncli_mb1_to_meas
 
 int trncli_cdata_to_pose(wposet_t **dest, pt_cdata_t *src)
 {
     return wposet_cdata_to_pose(dest, src);
-}// end function trncli_motion_to_pose
+}// end function trncli_cdata_to_pose
 
 int trncli_cdata_to_meas(wmeast_t **dest, mt_cdata_t *src)
 {
     return wmeast_cdata_to_meas(dest, src);
 }// end function trncli_cdata_to_meas
+
+int32_t trncli_update_measurement(trncli_t *self, wmeast_t *meas)
+{
+    int32_t retval=-1;
+
+    if(NULL!=self){
+        byte *msg=NULL;
+        int32_t mlen=0;
+        if( (mlen=trnw_meas_msg((char **)&msg,meas,TRN_MSG_MEAS,TRN_SENSOR_MB))>0){
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"TX MEAS UPDATE MEAST:\n");
+            wmeast_show(meas, true, 5);
+            fprintf(stderr,"TX MEAS UPDATE MSG:\n");
+            trnw_msg_show((char *)msg,true,5);
+#endif
+            retval=s_trncli_send_recv(self, msg, mlen, true);
+
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"RX MEAS UPDATE ret[%"PRId32"]\n",retval);
+            trnw_msg_show((char *)msg,true,5);
+#endif
+            free(msg);
+        }
+    }
+    return retval;
+}// end function trncli_update_measurement
+
+int32_t trncli_update_motion(trncli_t *self, wposet_t *pose)
+{
+    int32_t retval=-1;
+
+    if(NULL!=self){
+        byte *msg=NULL;
+        int32_t mlen=0;
+        if( (mlen=trnw_pose_msg((char **)&msg,pose,TRN_MSG_MOTN))>0){
+
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"TX MOTN UPDATE:\n");
+            wposet_show(pose, true, 5);
+            fprintf(stderr,"TX MOTN MSG:\n");
+            trnw_msg_show((char *)msg, true, 5);
+#endif
+            retval=s_trncli_send_recv(self, msg, mlen, true);
+
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"RX MOTN UPDATE ret[%"PRId32"]\n",retval);
+            trnw_msg_show((char *)msg,true,5);
+#endif
+            free(msg);
+        }
+    }
+    return retval;
+}// end function trncli_update_motion
+
+int32_t trncli_estimate_pose(trncli_t *self, wposet_t **pose, char msg_type)
+{
+    int32_t retval=-1;
+
+    if(NULL!=self){
+        byte *msg=NULL;
+        int32_t mlen=0;
+        if( (mlen=trnw_type_msg((char **)&msg,msg_type))>0){
+
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"TX EST_POSE MSG [%c]:\n",msg_type);
+            trnw_msg_show((char *)msg, true, 5);
+#endif
+            retval=s_trncli_send_recv(self, msg, mlen, true);
+            wposet_msg_to_pose(pose, (char *)msg);
+
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"RX EST_POSE MSG [%c] ret[%"PRId32"]\n",msg_type,retval);
+            trnw_msg_show((char *)msg,true,5);
+            fprintf(stderr,"RX EST_POSE [%c]:\n",msg_type);
+            wposet_show(*pose, true, 5);
+#endif
+            free(msg);
+        }
+    }
+    return retval;
+}// end function trncli_estimate_pose
+
+int32_t trncli_init_trn(trncli_t *self, trn_config_t *cfg)
+{
+    int32_t retval=-1;
+    if(NULL!=self){
+        byte *msg=NULL;
+        int32_t mlen=0;
+
+        if( (mlen=trnw_init_msg((char **)&msg, cfg))>0){
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"TX - INIT:\n");
+            trnw_msg_show((char *)msg,true,5);
+#endif
+            retval=s_trncli_send_recv(self, msg, mlen, true);
+
+#if defined(WITH_PDEBUG)
+            fprintf(stderr,"RX - INIT ret[%"PRId32"]\n",retval);
+            trnw_msg_show((char *)msg,true,5);
+#endif
+            free(msg);
+        }
+    }
+    return retval;
+}// end function trncli_init_trn
+
+int trncli_reinit_filter(trncli_t *self)
+{
+    return trncli_ptype_set(self,TRN_MSG_FILT_REINIT,0);
+} // end function trncli_reinit_filter
+
+int trncli_get_filter_type(trncli_t *self)
+{
+    return trncli_ptype_get(self,TRN_MSG_FILT_TYPE);
+} // end function trncli_get_filter_type
+
+int trncli_get_filter_state(trncli_t *self)
+{
+    return trncli_ptype_get(self,TRN_MSG_FILT_STATE);
+} // end function trncli_get_filter_state
+
+int trncli_set_filter_reinit(trncli_t *self, int value)
+{
+    return trncli_ptype_set(self,TRN_MSG_SET_FR, value);
+} // end function trncli_set_filter_reinit
+
+int trncli_set_modified_weighting(trncli_t *self, int value)
+{
+    return trncli_ptype_set(self,TRN_MSG_SET_MW, value);
+} // end function trncli_set_modified_weighting
+
+int trncli_set_ima(trncli_t *self, int value)
+{
+    return trncli_ptype_set(self,TRN_MSG_SET_IMA, value);
+} // end function
+
+int trncli_set_mim(trncli_t *self, int value)
+{
+    return trncli_ptype_set(self,TRN_MSG_SET_MIM, value);
+} // end function
+
+int trncli_set_vdr(trncli_t *self, int value)
+{
+    return trncli_ptype_set(self,TRN_MSG_SET_VDR, value);
+} // end function
+
+int trncli_set_filter_gradient(trncli_t *self, int value)
+{
+    return trncli_ptype_set(self,TRN_MSG_FILT_GRD, value);
+} // end function
+
+int trncli_set_utm(trncli_t *self, long int utm_zone)
+{
+    int retval=-1;
+    if(NULL!=self){
+        self->utm_zone=utm_zone;
+        retval=0;
+    }
+    return retval;
+} // end function
+
+bool trncli_last_meas_succesful(trncli_t *self)
+{
+    return ( (trncli_ptype_get(self,TRN_MSG_LAST_MEAS)<=0) ? false : true);
+} // end function trncli_last_meas_succesful
+
+int trncli_reinit_count(trncli_t *self)
+{
+    return trncli_ptype_get(self,TRN_MSG_N_REINITS);
+} // end function trncli_reinit_count
+
+bool trncli_outstanding_meas(trncli_t *self)
+{
+    return ( (trncli_ptype_get(self,TRN_MSG_OUT_MEAS)<=0) ? false : true);
+} // end function trncli_outstanding_meas
+
+bool trncli_is_converged(trncli_t *self)
+{
+    return ( (trncli_ptype_get(self,TRN_MSG_IS_CONV)<=0) ? false : true);
+} // end function trncli_is_converged
+
+bool trncli_is_intialized(trncli_t *self)
+{
+    return ( (trncli_ptype_get(self,TRN_MSG_IS_INIT)<=0) ? false : true);
+} // end function trncli_is_intialized
+
+bool trncli_ping(trncli_t *self)
+{
+    return ( (trncli_ptype_get(self,TRN_MSG_PING)<=0) ? false : true);
+} // end function trncli_ping
+
+int32_t trncli_ack_server(trncli_t *self)
+{
+    int32_t retval=-1;
+    if(NULL!=self){
+        byte *msg=NULL;
+        int32_t mlen=0;
+        if( (mlen=trnw_ack_msg((char **)&msg))>0){
+            retval=s_trncli_send_recv(self, msg, mlen, true);
+            fprintf(stderr,"%s - ACK ret[%"PRId32"]\n",__FUNCTION__,retval);
+            free(msg);
+        }
+    }
+    return retval;
+}// end function trncli_ack_server
+
 
 
