b6fb996f1ac7f1be240aa3e243193f55e0d8f95d
Kent Headley <headley@mbari.org>
2021-09-22 12:57:23 -0700

    update to reflect libtrnav directory structure changes

---
+++ src/mbtrnav/opt/lrauv/config_defs.h
@@ -0,0 +1,15 @@
+#define TRN_MAP_GRID      1
+#define TRN_MAP_OCTREE    2
+
+#define TRN_INST_DVL      1
+
+#define TRN_FILTER_PM     1
+#define TRN_FILTER_PF     2
+#define TRN_FILTER_PMB    3
+
+#define TRN_WEIGHT_NONE   0
+#define TRN_WEIGHT_ALPHA  1
+#define TRN_WEIGHT_CROSS  2
+#define TRN_WEIGHT_SB     3
+#define TRN_WEIGHT_SBNIS  4
+
