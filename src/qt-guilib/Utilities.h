4e77a87db6d30565bd626f3bc1c8e0a9205c756a
Tom O'Reilly <oreilly@mbari.org>
2021-11-23 13:02:25 -0800

    added Haxby colors

--- src/qt-guilib/Utilities.h
+++ src/qt-guilib/Utilities.h
@@ -6,8 +6,26 @@
 
 namespace mb_system {
 
+  const int NMapColors = 11;
+
+  const float haxbyRed[NMapColors] =
+    {0.950, 1.000, 1.000, 1.000, 0.941, 0.804, 0.541, 0.416, 0.196,
+     0.157, 0.145};
+
+  const float haxbyGreen[NMapColors] =
+   {0.950, 0.729, 0.631, 0.741, 0.925, 1.000, 0.925, 0.922, 0.745,
+    0.498, 0.224};
+  
+  const float haxbyBlue[NMapColors] =
+   {0.950, 0.522, 0.267, 0.341, 0.475, 0.635, 0.682, 1.000, 1.000,
+    0.984, 0.686};
+  
+  enum ColorMapScheme { BrewerDivergingSpectral, WhiteToBlue,
+                        Hawaii, RedToBlue, Haxby };
+
+  
   /// Make lookup table
-  void makeLookupTable(int colorScheme, vtkLookupTable *lut);
+  void makeLookupTable(ColorMapScheme colorMap, vtkLookupTable *lut);
 
 }
 
