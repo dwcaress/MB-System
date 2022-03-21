e68c3b579d7fc4b5a56ec2e54c7b7a4b2ffc5b85
Kent Headley <headley@mbari.org>
2022-03-20 21:24:49 -0700

    sync w/ libtrnav - formatting changes and Makefile paths (not used for MB-System build)

--- src/mbtrnav/opt/lrauv/LcmTrn.h
+++ src/mbtrnav/opt/lrauv/LcmTrn.h
@@ -58,24 +58,24 @@
  * software required.
  *****************************************************************************/
 
-#ifndef  LCMTRN_H
-#define  LCMTRN_H
+#ifndef LCMTRN_H
+#define LCMTRN_H
 
 /***********************************************************************
  * Headers
  ***********************************************************************/
 
-#include <stdio.h>
 #include <libconfig.h++>
+#include <stdio.h>
 
-#include <lcm/lcm-cpp.hpp>
-#include "TethysLcmTypes/LrauvLcmMessage.hpp"
 #include "LcmMessageReader.h"
 #include "LcmMessageWriter.h"
+#include "TethysLcmTypes/LrauvLcmMessage.hpp"
+#include <lcm/lcm-cpp.hpp>
 
-#include "structDefs.h"
-#include "TerrainNav.h"
 #include "TNavConfig.h"
+#include "TerrainNav.h"
+#include "structDefs.h"
 
 using namespace std;
 using namespace libconfig;
@@ -87,36 +87,36 @@ using namespace lrauv_lcm_tools;
  ***********************************************************************/
 
 // Definitions used in conjunction with configuration file (e.g., lcm-trn.cfg)
-#define TRN_MAP_GRID      1
-#define TRN_MAP_OCTREE    2
+#define TRN_MAP_GRID 1
+#define TRN_MAP_OCTREE 2
 
-#define TRN_INST_DVL      1
+#define TRN_INST_DVL 1
 
-#define TRN_FILTER_PM     1
-#define TRN_FILTER_PF     2
-#define TRN_FILTER_PMB    3
+#define TRN_FILTER_PM 1
+#define TRN_FILTER_PF 2
+#define TRN_FILTER_PMB 3
 
-#define TRN_WEIGHT_NONE   0
-#define TRN_WEIGHT_ALPHA  1
-#define TRN_WEIGHT_CROSS  2
-#define TRN_WEIGHT_SB     3
-#define TRN_WEIGHT_SBNIS  4
+#define TRN_WEIGHT_NONE 0
+#define TRN_WEIGHT_ALPHA 1
+#define TRN_WEIGHT_CROSS 2
+#define TRN_WEIGHT_SB 3
+#define TRN_WEIGHT_SBNIS 4
 
 // Other definitions
-#define  LCMTRN_CONFIG_ENV         "TRN_DATAFILES"
-#define  LCMTRN_DEFAULT_CONFIG     "lcm-trn.cfg"
-#define  LCMTRN_DEFAULT_ZONE       10     // default utm zone Monterey Bay
-#define  LCMTRN_DEFAULT_PERIOD     5      // min seconds between TRN updates
-#define  LCMTRN_DEFAULT_COHERENCE  0.25   // max seconds between AHRS and DVL
-#define  LCMTRN_DEFAULT_TIMEOUT    1000   // 1 second LCM timeout for normal
-#define  LCMTRN_DEFAULT_INITIAL    50     // 50 msec initial timeout for bursty
-#define  LCMTRN_DEFAULT_MAXIMUM    250    // 250 msec max timeout for bursty
-#define  LCMTRN_DEFAULT_INSTRUMENT 1      // DVL instrument
-#define  LCMTRN_DEFAULT_NUMBEAMS   4      // DVL instrument
-#define  LCMTRN_DEFAULT_FILTER     1
-#define  LCMTRN_DEFAULT_WEIGHTING  1
-#define  LCMTRN_DEFAULT_LOWGRADE   false
-#define  LCMTRN_DEFAULT_ALLOW      true
+#define LCMTRN_CONFIG_ENV "TRN_DATAFILES"
+#define LCMTRN_DEFAULT_CONFIG "lcm-trn.cfg"
+#define LCMTRN_DEFAULT_ZONE 10        // default utm zone Monterey Bay
+#define LCMTRN_DEFAULT_PERIOD 5       // min seconds between TRN updates
+#define LCMTRN_DEFAULT_COHERENCE 0.25 // max seconds between AHRS and DVL
+#define LCMTRN_DEFAULT_TIMEOUT 1000   // 1 second LCM timeout for normal
+#define LCMTRN_DEFAULT_INITIAL 50     // 50 msec initial timeout for bursty
+#define LCMTRN_DEFAULT_MAXIMUM 250    // 250 msec max timeout for bursty
+#define LCMTRN_DEFAULT_INSTRUMENT 1   // DVL instrument
+#define LCMTRN_DEFAULT_NUMBEAMS 4     // DVL instrument
+#define LCMTRN_DEFAULT_FILTER 1
+#define LCMTRN_DEFAULT_WEIGHTING 1
+#define LCMTRN_DEFAULT_LOWGRADE false
+#define LCMTRN_DEFAULT_ALLOW true
 
 // Define a useful way to delete and reset a pointer to an object.
 // If the ptr is non-NULL, delete the object referenced by the ptr
@@ -125,55 +125,56 @@ using namespace lrauv_lcm_tools;
 // SomeObj *obj = new SomeObject();
 // DELOBJ(obj);
 //
-#define DELOBJ(ptr) { if (NULL != ptr) {delete ptr; ptr = NULL;} }
+#define DELOBJ(ptr)                                                            \
+    {                                                                          \
+        if (NULL != ptr) {                                                     \
+            delete ptr;                                                        \
+            ptr = NULL;                                                        \
+        }                                                                      \
+    }
 
 namespace lcmTrn
 {
 
-typedef struct lcmconfig_
-{
+typedef struct lcmconfig_ {
     float timeout;
     unsigned int initial_timeout_msec, max_timeout_msec;
     const char *ahrs, *heading, *pitch, *roll;
-    const char *dvl, *xvel, *yvel, *zvel, *beam1, *beam2, *beam3, *beam4, *valid;
+    const char *dvl, *xvel, *yvel, *zvel, *beam1, *beam2, *beam3, *beam4,
+        *valid;
     const char *nav, *lat, *lon;
     const char *depth, *veh_depth, *pressure;
     const char *trn, *mle, *mmse, *var, *reinits, *filter, *updatetime;
     const char *cmd, *reinit, *estimate;
 } LcmConfig;
 
-typedef struct trnconfig_
-{
+typedef struct trnconfig_ {
     int utm_zone;
     float period, coherence;
-    const char *mapn, *cfgn, *partn, *logd;                     // TRN cfg filenames
-    int  maptype, filtertype, weighting, instrument, nbeams;    // TRN options
+    const char *mapn, *cfgn, *partn, *logd;                 // TRN cfg filenames
+    int maptype, filtertype, weighting, instrument, nbeams; // TRN options
     bool allowreinit, lowgrade;
 } TrnConfig;
-
 
 class LcmTrn
 {
   public:
+    LcmTrn(const char *configfilepath);
 
-    LcmTrn(const char* configfilepath);
+    ~LcmTrn();
 
-   ~LcmTrn();
-
-    static char* constructFullName(const char* env_var, const char* base_name);
+    static char *constructFullName(const char *env_var, const char *base_name);
 
     bool good() { return _good; }
-    void run();              // Run while good()
-    void cycle();            // Execute a single listen-update cycle and return
+    void run();   // Run while good()
+    void cycle(); // Execute a single listen-update cycle and return
 
-    LcmConfig* getLcmConfig() { return &_lcmc; }
-    TrnConfig* getTrnConfig() { return &_trnc; }
-
+    LcmConfig *getLcmConfig() { return &_lcmc; }
+    TrnConfig *getTrnConfig() { return &_trnc; }
 
   protected:
-
     void init();
-    void reinit(const char* configfilepath = NULL);
+    void reinit(const char *configfilepath = NULL);
     void loadConfig();
 
     void initTrn();
@@ -184,25 +185,20 @@ class LcmTrn
     void initLcm();
     bool verifyLcmConfig();
     void cleanLcm();
-    int  handleMessages();
-    int  getLcmTimeout(unsigned int initial_timeout, unsigned int max_timeout);
+    int handleMessages();
+    int getLcmTimeout(unsigned int initial_timeout, unsigned int max_timeout);
     void publishEstimates();
 
-    void handleAhrs(const lcm::ReceiveBuffer* rbuf,
-                    const std::string& chan,
-                    const LrauvLcmMessage* msg);
-    void handleDvl (const lcm::ReceiveBuffer* rbuf,
-                    const std::string& chan,
-                    const LrauvLcmMessage* msg);
-    void handleDepth(const lcm::ReceiveBuffer* rbuf,
-                    const std::string& chan,
-                    const LrauvLcmMessage* msg);
-    void handleCmd (const lcm::ReceiveBuffer* rbuf,
-                    const std::string& chan,
-                    const LrauvLcmMessage* msg);
-    void handleNav (const lcm::ReceiveBuffer* rbuf,
-                    const std::string& chan,
-                    const LrauvLcmMessage* msg);
+    void handleAhrs(const lcm::ReceiveBuffer *rbuf, const std::string &chan,
+                    const LrauvLcmMessage *msg);
+    void handleDvl(const lcm::ReceiveBuffer *rbuf, const std::string &chan,
+                   const LrauvLcmMessage *msg);
+    void handleDepth(const lcm::ReceiveBuffer *rbuf, const std::string &chan,
+                     const LrauvLcmMessage *msg);
+    void handleCmd(const lcm::ReceiveBuffer *rbuf, const std::string &chan,
+                   const LrauvLcmMessage *msg);
+    void handleNav(const lcm::ReceiveBuffer *rbuf, const std::string &chan,
+                   const LrauvLcmMessage *msg);
 
     bool time2Update();
     bool updateTrn();
@@ -211,7 +207,7 @@ class LcmTrn
 
     // Config items
     const char *_configfile;
-    Config  *_cfg;
+    Config *_cfg;
 
     int _lcmtimeout;
     LcmConfig _lcmc;
@@ -227,20 +223,19 @@ class LcmTrn
     int _filterstate, _numreinits;
     int64_t _seqNo;
 
-    double  _lastAHRSTimestamp;
-    double  _lastDvlTimestamp;
-    double  _lastNavTimestamp;
-    double  _lastDepthTimestamp;
-    double  _lastCmdTimestamp;
-    double  _lastUpdateTimestamp;
+    double _lastAHRSTimestamp;
+    double _lastDvlTimestamp;
+    double _lastNavTimestamp;
+    double _lastDepthTimestamp;
+    double _lastCmdTimestamp;
+    double _lastUpdateTimestamp;
 
     bool _good;
 
     LcmMessageReader _msg_reader;
     LcmMessageWriter<std::string> _msg_writer;
-
 };
 
-}
+} // namespace lcmTrn
 
 #endif
