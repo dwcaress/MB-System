8435b34ae80b49c8b910b69b182cd3ea99650ead
Tom O'Reilly <oreilly@mbari.org>
2022-05-24 14:01:28 -0700

    boolean options must be specified as option=1 or option=0

--- mbcmake.sh
+++ mbcmake.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Wrapper for MB-System cmake.
 
+# N.B.: Most command-line options to this script *MUST* match spelling of
+# of variables that can be provided on command-line to MB-System/CMakeLists.txt
+
 # Boolean options are specified as single word, which assumes true
 validBoolOptions=(buildGUIs buildOpenCV buildPCL
                   buildTRN buildTNav buildQt buildGSF buildTest)
@@ -13,12 +16,13 @@ validModules=(GMT Proj OpenGL X11 Motif GDAL NetCDF FFTW VTK)
 validDirParams=(otpsDir installDir)
 
 help() {
-    echo -n "$1 : "
-    echo valid boolean options:
+    echo "$1"
+    echo "specify boolean options as option=0 or option=1, where option is one of:"
     for opt in ${validBoolOptions[@]}; do
         echo $opt
     done
 
+    echo
     echo "specify module include path with module_include=<DIR>"
     echo "specify module library with module_lib=<LIB>"
     echo "specify module libraries with module_lib=<LIB;LIB2;LIB3...>"        
@@ -27,12 +31,16 @@ help() {
         echo $package
     done
 
+    echo
     echo "specify directory parameters with parameter=<DIR>"
     echo "where 'parameter' is one of:"
     for dir in ${validDirParams[@]}; do
         echo $dir
     done
-    
+
+    echo
+    echo "-DCMAKE_BUILD_TYPE=Debug/Release Compile/link with/without debug flags"
+    echo "-DBUILD_SHARED_LIBS=ON/OFF Build shared libraries"
     }
 
 # Process commad-line options, convert each to format recognized by cmake
@@ -44,8 +52,8 @@ while (( "$#" )); do
     echo arg: $1
     # Is arg a valid boolean option?
     for opt in ${validBoolOptions[@]}; do
-        if [ "$1" == $opt ]; then
-            options=${options}' '-D${1}=1
+        if [[ "$1" =~ ${opt}=.* ]]; then
+            options=${options} \"-D${1}=1\"
             found=true
             break
         fi
@@ -60,7 +68,7 @@ while (( "$#" )); do
         hdrParam=${module}_include='.+'
         libParam=${module}_lib='.+'
         if [[ "$1" =~ $hdrParam ]] || [[ "$1" =~ $libParam ]]; then
-            options=${options}' "-D'$1\"
+            options=${options} \"-D$1\"
             found=true
             break
         fi
@@ -74,7 +82,7 @@ while (( "$#" )); do
     for param in ${validDirParams[@]}; do
         pattern=${param}'.+'
         if [[ "$1" =~ $pattern ]]; then
-            options=${options}' "-D'$1\"
+            options=${options} \"-D$1\"
             found=true
             break
         fi
@@ -84,30 +92,32 @@ while (( "$#" )); do
         continue
     fi    
 
-    if [ "$1" == "debug" ]; then
-        options=${options}' "-DCMAKE_BUILD_TYPE=Debug"'
+    if [[ "$1" =~ -DCMAKE_BUILD_TYPE=.* ]]; then
+        options=${options} "-DCMAKE_BUILD_TYPE=Debug"
+        echo OK
         shift
         continue
     fi
 
-    if [ "$1" == "shared" ]; then
-        options=${options}' "-DBUILD_SHARED_LIBS=ON"'
+    if [[ "$1" =~ -DBUILD_SHARED_LIBS=.* ]]; then
+        options=${options} "-DBUILD_SHARED_LIBS=ON"
         shift
         continue
     fi    
 
     # If we get here, then unknown option
     error=true
-    echo "unknown/invalid option '$1'"
+    echo unknown/invalid/incomplete option: \'$1\'
 
     shift
 done
-echo options: $options
 
 if [ "$error" == true ]; then
-    help usage:
+    help
     exit 1
 fi
+
+echo options: $options
 
 echo run cmake
 # Go to build and invoke cmake
