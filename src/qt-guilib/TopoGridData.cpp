46fdb045ec825e61009353b807120a6718dcc6b7
Tom O'Reilly <oreilly@mbari.org>
2022-11-10 11:00:27 -0800

    first commit

---
+++ src/qt-guilib/TopoGridData.cpp
@@ -0,0 +1,5 @@
+#include "TopoGridData.h"
+#include <limits>
+
+// Use of very large number here (e.g. std::numeric_limits<int>) causes problems with VTK
+const double mb_system::TopoGridData::NoData = -10000000.;
