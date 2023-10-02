bc8eb84da8a290d45d10e9aceeda4f804c8295d2
Kent Headley <headley@mbari.org>
2023-10-02 09:19:34 -0700

    add plugin build for BUILD_ROVTRN; add plug-common.cpp to initialize pcf_log static initializers

---
+++ src/mbtrnav/opt/rov/plug-common.cpp
@@ -0,0 +1,7 @@
+// This file exists b/c pcf_log.hpp
+// declares static initializers, guarded by INIT_PCF_LOG
+// INIT_PCF_LOG must be defined before first
+// inclusion
+#define INIT_PCF_LOG
+#include "pcf_log.hpp"
+
