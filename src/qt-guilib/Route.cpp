351d5b53835ab07c5b3d1603f799182f247cda61
Tom O'Reilly <oreilly@mbari.org>
2023-10-17 16:53:38 -0700

    refined

--- src/qt-guilib/Route.cpp
+++ src/qt-guilib/Route.cpp
@@ -1,5 +1,7 @@
+#include <string.h>
 #include <iostream>
 #include <fstream>
+
 #include "Route.h"
 
 using namespace mb_system;
@@ -12,14 +14,14 @@ Route::~Route() {
 
   // Free vector elements
   for (int i = 0; i < points_.size(); i++) {
-    RouteWaypoint *point =  points_[i];
+    Route::Waypoint *point =  points_[i];
     delete point;
   }
 
 }
 
 
-bool Route::appendPoint(RouteWaypoint *point) {
+bool Route::appendPoint(Route::Waypoint *point) {
   points_.insert(points_.end(), point);
   return true;
 }
@@ -33,12 +35,80 @@ bool Route::load(std::string filename) {
     return false;
   }
 
+  bool rawRoutefile = true;
+  char routename[4096];     // What is upper limit on path name???
+  int routeColor = 0;
+  int routeSize = 0;
+  int routeEditMode = 0;
+  int nPoint = 0;
+  bool pointOK = false;
+  
   std::string line;
   while (getline(file, line)) {
     std::cout << line << '\n';
+    const char *buffer = line.data();
+
+    // deal with comments
+    if (buffer[0] == '#') {
+      if (rawRoutefile && strncmp(buffer, "## Route File Version", 21) == 0) {
+	rawRoutefile = false;
+      }
+      else if (strncmp(buffer, "## ROUTENAME", 12) == 0) {
+	strcpy(routename, &buffer[13]);
+	if (routename[strlen(routename) - 1] == '\n')
+	  routename[strlen(routename) - 1] = '\0';
+	if (routename[strlen(routename) - 1] == '\r')
+	  routename[strlen(routename) - 1] = '\0';
+      }
+      else if (strncmp(buffer, "## ROUTECOLOR", 13) == 0) {
+	sscanf(buffer, "## ROUTECOLOR %d", &routeColor);
+      }
+      else if (strncmp(buffer, "## ROUTESIZE", 12) == 0) {
+	sscanf(buffer, "## ROUTESIZE %d", &routeSize);
+      }
+      else if (strncmp(buffer, "## ROUTEEDITMODE", 16) == 0) {
+	sscanf(buffer, "## ROUTEEDITMODE %d", &routeEditMode);
+      }
+    }
+
+    // deal with route segment marker
+    else if (buffer[0] == '>') {
+      // if data accumulated, add the route
+      if (nPoint > 0) {
+	std::cout << "TBD: Add the route\n";
+      }
+    }
+
+    else {
+      // read the route waypoint data from the buffer
+      int waypointType;
+      double lon, lat, elev;
+      int nget = sscanf(buffer, "%lf %lf %lf %d", &lon, &lat, &elev,
+			&waypointType);
+      if ((rawRoutefile && nget >= 2) ||
+	  (!rawRoutefile && nget >= 3 &&
+	   waypointType > PointType::Interpolated))
+	pointOK = true;
+      else
+	pointOK = false;
+
+      // add good point to route
+      if (pointOK) {
+	Route::Waypoint *point = new Route::Waypoint();
+	point->easting = lon;
+	point->northing = lat;
+	point->elevation = elev;
+	point->type = (Route::PointType )waypointType;
+	  
+	appendPoint(point);
+	
+	nPoint++;
+      }
+    }
   }
-  
+
   file.close();
   return true;
+  
 
 }
--- src/qt-guilib/Route.h
+++ src/qt-guilib/Route.h
@@ -7,34 +7,6 @@
 
 namespace mb_system {
 
-  /** Route is defined by series of points, each
-       point classified by PointType.
-  */
-  enum PointType {Interpolated=0,
-		  UserSpecified,
-		  Transit,
-		  StartSurvey,
-		  EndSurvey,
-		  StartSurvey2,
-		  EndSurvey2,
-		  StartSurvey3,
-		  EndSurvey3,
-		  StartSurvey4,
-		  EndSurvey4,
-		  StartSurvey5,
-		  EndSurvey5
-  };
-
-
-  /// Route consists of waypoints
-  struct RouteWaypoint {
-    double easting;
-    double northing;
-    double elevation;
-    PointType type;
-  };
-
-
   /***
       Define a route, which consists of individual points. Some of the
       points were specified by the user, others are interpolated between
@@ -50,15 +22,45 @@ namespace mb_system {
     /// Destructor
     ~Route();
 
+    /** Route is defined by series of points, each
+	point classified by PointType.
+    */
+    enum PointType {Interpolated=0,
+		    UserSpecified,
+		    Transit,
+		    StartSurvey,
+		    EndSurvey,
+		    StartSurvey2,
+		    EndSurvey2,
+		    StartSurvey3,
+		    EndSurvey3,
+		    StartSurvey4,
+		    EndSurvey4,
+		    StartSurvey5,
+		    EndSurvey5
+    };
+
+
+
+    
+    /// Route consists of waypoints
+    struct Waypoint {
+      double easting;
+      double northing;
+      double elevation;
+      PointType type;
+    };
+
     /// Append a waypoint; returns true if no error, else false
-    bool appendPoint(RouteWaypoint *point);
+    bool appendPoint(Route::Waypoint *point);
 
     /// Load route from a route file
     bool load(std::string routefileName);
 
 
+
   protected:
-    std::vector<RouteWaypoint *> points_;
+    std::vector<Route::Waypoint *> points_;
 
   };
 
