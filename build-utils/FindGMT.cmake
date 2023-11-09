dbea34630836ff61948f0d2c89051cb2ab0a8ff9
David Caress <caress@mbari.org>
2023-11-09 14:50:16 -0800

    Changes to CMake structure to make it work on MacOs
    Changes to handle finding the GMT package, including the GMT header files, and linking with GMT libraries for libmbaux and the GMT modules within MB-System.

--- build-utils/FindGMT.cmake
+++ build-utils/FindGMT.cmake
@@ -7,9 +7,11 @@
 #
 # This module defines the following CMake variables:
 #
-# GMT_FOUND - True if libgmt is found GMT_LIBRARY - A variable pointing to the
-# GMT library GMT_INCLUDE_DIR - Where to find the headers GMT_INCLUDE_DIRS -
-# Where to find the headers GMT_DEFINITIONS - Extra compiler flags
+# GMT_FOUND - True if libgmt is found 
+# GMT_LIBRARY - A variable pointing to the GMT library 
+# GMT_INCLUDE_DIR - Where to find the headers 
+# GMT_INCLUDE_DIRS - Where to find the headers 
+# GMT_DEFINITIONS - Extra compiler flags
 
 # =============================================================================
 # Inspired by FindGDAL
@@ -24,6 +26,7 @@
 # This makes the presumption that you include gmt.h like
 #
 # include "gmt.h"
+message(STATUS "29 GMT_FOUND: ${GMT_FOUND}  GMT_LIBRARY: ${GMT_LIBRARY}")
 
 if(UNIX AND NOT GMT_FOUND)
   # Use gmt-config to obtain the libraries
@@ -39,6 +42,7 @@ if(UNIX AND NOT GMT_FOUND)
           /opt/csw # Blastwave
           /opt
           /usr/local)
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: GMT_CONFIG: ${GMT_CONFIG}")
 
   if(GMT_CONFIG)
     execute_process(
@@ -58,17 +62,40 @@ if(UNIX AND NOT GMT_FOUND)
     if(GMT_CONFIG_LIBS)
       # Ensure -l is precedeced by whitespace to not match '-l' in
       # '-L/usr/lib/x86_64-linux-gnu/hdf5/serial'
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: GMT_CONFIG_CFLAGS: ${GMT_CONFIG_CFLAGS}")
       string(REGEX MATCHALL "(^| )-l[^ ]+" _gmt_dashl ${GMT_CONFIG_LIBS})
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: _gmt_dashl: ${_gmt_dashl}")
       string(REGEX REPLACE "(^| )-l" "" _gmt_lib "${_gmt_dashl}")
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: _gmt_lib: ${_gmt_lib}")
       string(REGEX MATCHALL "(^| )-L[^ ]+" _gmt_dashL ${GMT_CONFIG_LIBS})
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: _gmt_dashL: ${_gmt_dashL}")
       string(REGEX REPLACE "(^| )-L" "" _gmt_libpath "${_gmt_dashL}")
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: _gmt_libpath: ${_gmt_libpath}")
     endif(GMT_CONFIG_LIBS)
+    message(STATUS "70 GMT_CONFIG_CFLAGS: ${GMT_CONFIG_CFLAGS}")
+    message(STATUS "71 GMT_CONFIG_LIBS: ${GMT_CONFIG_LIBS}")
   endif(GMT_CONFIG)
   if(_gmt_lib)
     list(REMOVE_DUPLICATES _gmt_lib)
-    list(REMOVE_ITEM _gmt_lib gmt)
   endif(_gmt_lib)
 endif(UNIX AND NOT GMT_FOUND)
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: _gmt_libpath:   ${_gmt_libpath}")
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: _gmt_lib:       ${_gmt_lib}")
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: GMT_FOUND: ${GMT_FOUND}  GMT_LIBRARY: ${GMT_LIBRARY}")
+
+# find all libs that gmt-config reports (which is just libgmt, not libpostscriptlight)
+foreach(_extralib ${_gmt_lib})
+  message(STATUS "_extralib: ${_extralib}    _gmt_lib:   ${_gmt_lib} ")
+  find_library(
+    _found_lib_${_extralib}
+    NAMES ${_extralib}
+    PATHS ${_gmt_libpath})
+  list(APPEND GMT_LIBRARY ${_found_lib_${_extralib}})
+  message(STATUS "${CMAKE_CURRENT_LIST_LINE}: GMT_LIBRARY: ${GMT_LIBRARY}")
+endforeach(_extralib)
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: GMT_LIBRARY: ${GMT_LIBRARY}")
+list(REMOVE_DUPLICATES GMT_LIBRARY)
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: GMT_LIBRARY: ${GMT_LIBRARY}")
 
 find_path(
   GMT_INCLUDE_DIR gmt.h
@@ -82,10 +109,28 @@ find_path(
         /opt/csw # Blastwave
         /opt
         /usr/local)
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: GMT_INCLUDE_DIR: ${GMT_INCLUDE_DIR}")
+
+if (NOT GMT_LIBRARY)
+  find_library(
+    GMT_LIBRARY
+    NAMES gmt 
+    HINTS ${_gmt_libpath} ${GMT_DIR} ${GMT_ROOT} $ENV{GMT_DIR} $ENV{GMT_ROOT}
+    PATH_SUFFIXES lib
+    PATHS /sw # Fink
+          /opt/sw # Fink
+          /opt/local # MacPorts
+          /opt/local/lib/gmt6 
+          /opt/local/lib/gmt5
+          /opt/csw # Blastwave
+          /opt
+          /usr/local)
+endif (NOT GMT_LIBRARY)
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: GMT_LIBRARY: ${GMT_LIBRARY}")
 
 find_library(
-  GMT_LIBRARY
-  NAMES gmt
+  PSL_LIBRARY
+  NAMES postscriptlight 
   HINTS ${_gmt_libpath} ${GMT_DIR} ${GMT_ROOT} $ENV{GMT_DIR} $ENV{GMT_ROOT}
   PATH_SUFFIXES lib
   PATHS /sw # Fink
@@ -96,18 +141,10 @@ find_library(
         /opt/csw # Blastwave
         /opt
         /usr/local)
-
-# find all libs that gmt-config reports
-foreach(_extralib ${_gmt_lib})
-  find_library(
-    _found_lib_${_extralib}
-    NAMES ${_extralib}
-    PATHS ${_gmt_libpath})
-  list(APPEND GMT_LIBRARY ${_found_lib_${_extralib}})
-endforeach(_extralib)
+message(STATUS "${CMAKE_CURRENT_LIST_LINE}: PSL_LIBRARY: ${PSL_LIBRARY}")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMT DEFAULT_MSG GMT_LIBRARY GMT_INCLUDE_DIR)
+find_package_handle_standard_args(GMT DEFAULT_MSG GMT_LIBRARY PSL_LIBRARY GMT_INCLUDE_DIR)
 
 if(GMT_FOUND)
   set(GMT_LIBRARIES ${GMT_LIBRARY})
@@ -117,6 +154,13 @@ if(GMT_FOUND)
     add_library(GMT::GMT UNKNOWN IMPORTED)
     set_target_properties(
       GMT::GMT PROPERTIES IMPORTED_LOCATION "${GMT_LIBRARY}"
+                          INTERFACE_INCLUDE_DIRECTORIES "${GMT_INCLUDE_DIR}")
+  endif()
+
+  if(NOT TARGET GMT::PSL)
+    add_library(GMT::PSL UNKNOWN IMPORTED)
+    set_target_properties(
+      GMT::PSL PROPERTIES IMPORTED_LOCATION "${PSL_LIBRARY}"
                           INTERFACE_INCLUDE_DIRECTORIES "${GMT_INCLUDE_DIR}")
   endif()
 endif()
