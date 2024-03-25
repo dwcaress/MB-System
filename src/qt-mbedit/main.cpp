1cdedc510710cedf825f6345b4dad0cebd1f5c5b
Tom O'Reilly <oreilly@mbari.org>
2024-03-25 13:50:32 -0700

    call MainWindows::mbedit_init()

--- src/qt-mbedit/main.cpp
+++ src/qt-mbedit/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    int nInputSpecified = 0;
+    w.mbedit_init(argc, argv, &nInputSpecified);
     w.show();
     return a.exec();
 }
