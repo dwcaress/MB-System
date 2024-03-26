488434a29c91f83d789f9dd1723e26b30ee1e7f8
Tom O'Reilly <oreilly@mbari.org>
2024-03-26 09:00:52 -0700

    nInputSpecified is bool

--- src/qt-mbedit/main.cpp
+++ src/qt-mbedit/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    int nInputSpecified = 0;
+    bool nInputSpecified = false;
     w.mbedit_init(argc, argv, &nInputSpecified);
     w.show();
     return a.exec();
