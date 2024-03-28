365744506416c0c5322152296ec76b7b3846831a
Tom O'Reilly <oreilly@mbari.org>
2024-03-28 10:31:04 -0700

    moved functions defined in mbedit_prog.c to mbedit_prog.h

--- src/mbedit/mbedit.h
+++ src/mbedit/mbedit.h
@@ -32,86 +32,10 @@
  * Date:	October 14, 2009
  */
 
-int mbedit_init(int argc, char **argv, int *startup_file);
-int mbedit_set_graphics(void *xgid, int ncol, unsigned int *pixels);
-int mbedit_set_scaling(int *brdr, int sh_time);
-int mbedit_set_filters(int f_m, int f_m_t, int f_m_x, int f_m_l, int f_w, int f_w_t, int f_b, int f_b_b, int f_b_e, int f_d,
-                       double f_d_b, double f_d_e, int f_a, double f_a_b, double f_a_e);
-int mbedit_get_filters(int *b_m, double *d_m, int *f_m, int *f_m_t, int *f_m_x, int *f_m_l, int *f_w, int *f_w_t, int *f_b,
-                       int *f_b_b, int *f_b_e, int *f_d, double *f_d_b, double *f_d_e, int *f_a, double *f_a_b, double *f_a_e);
-int mbedit_get_defaults(int *plt_size_max, int *plt_size, int *sh_mode, int *sh_flggdsdg, int *sh_flggdprf, int *sh_time, int *buffer_size_max,
-                        int *buffer_size, int *hold_size, int *form, int *plwd, int *exgr, int *xntrvl, int *yntrvl, int *ttime_i,
-                        int *outmode);
-int mbedit_get_viewmode(int *vw_mode);
-int mbedit_set_viewmode(int vw_mode);
-int mbedit_action_open(char *file, int form, int fileid, int numfiles, int savemode, int outmode, int plwd, int exgr, int xntrvl,
-                       int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time, int *buffer_size, int *buffer_size_max,
-                       int *hold_size, int *ndumped, int *nloaded, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_next_buffer(int hold_size, int buffer_size, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size,
-                              int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time, int *ndumped, int *nloaded, int *nbuffer, int *ngood,
-                              int *icurrent, int *nplt, int *quit);
-int mbedit_action_close(int buffer_size, int *ndumped, int *nloaded, int *nbuffer, int *ngood, int *icurrent);
-int mbedit_action_done(int buffer_size, int *ndumped, int *nloaded, int *nbuffer, int *ngood, int *icurrent, int *quit);
-int mbedit_action_quit(int buffer_size, int *ndumped, int *nloaded, int *nbuffer, int *ngood, int *icurrent);
-int mbedit_action_step(int step, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf,
-                       int sh_time, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_plot(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                       int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_mouse_toggle(int x_loc, int y_loc, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode,
-                               int sh_flggdsdg, int sh_flggdprf, int sh_time, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_mouse_pick(int x_loc, int y_loc, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode,
-                             int sh_flggdsdg, int sh_flggdprf, int sh_time, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_mouse_erase(int x_loc, int y_loc, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode,
-                              int sh_flggdsdg, int sh_flggdprf, int sh_time, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_mouse_restore(int x_loc, int y_loc, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode,
-                                int sh_flggdsdg, int sh_flggdprf, int sh_time, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_mouse_grab(int grabmode, int x_loc, int y_loc, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size,
-                             int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_mouse_info(int x_loc, int y_loc, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode,
-                             int sh_flggdsdg, int sh_flggdprf, int sh_time, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_zap_outbounds(int iping, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf,
-                                int sh_time, int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_bad_ping(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                           int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_good_ping(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                            int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_left_ping(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                            int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_right_ping(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                             int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_zero_ping(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                            int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_flag_view(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                            int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_unflag_view(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                              int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_unflag_all(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                             int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_action_filter_all(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time,
-                             int *nbuffer, int *ngood, int *icurrent, int *nplt);
-int mbedit_filter_ping(int iping);
-int mbedit_get_format(char *file, int *form);
-int mbedit_open_file(char *file, int form, bool savemode);
-int mbedit_close_file(void);
-int mbedit_dump_data(int hold_size, int *ndumped, int *nbuffer);
-int mbedit_load_data(int buffer_size, int *nloaded, int *nbuffer, int *ngood, int *icurrent);
-int mbedit_clear_screen(void);
-int mbedit_plot_all(int plwd, int exgr, int xntrvl, int yntrvl, int plt_size, int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time, int *nplt,
-                    bool autoscale);
-int mbedit_plot_beam(int iping, int jbeam);
-int mbedit_plot_ping(int iping);
-int mbedit_plot_ping_label(int iping, bool save);
-int mbedit_plot_info(void);
-int mbedit_unplot_beam(int iping, int jbeam);
-int mbedit_unplot_ping(int iping);
-int mbedit_unplot_info(void);
-int mbedit_action_goto(int ttime_i[7], int hold_size, int buffer_size, int plwd, int exgr, int xntrvl, int yntrvl, int plt_size,
-                       int sh_mode, int sh_flggdsdg, int sh_flggdprf, int sh_time, int *ndumped, int *nloaded, int *nbuffer, int *ngood,
-                       int *icurrent, int *nplt);
-int mbedit_tslabel(int data_id, char *label);
-int mbedit_tsvalue(int iping, int data_id, double *value);
-int mbedit_tsminmax(int iping, int nping, int data_id, double *tsmin, double *tsmax);
-int mbedit_xtrackslope(int iping, double *slope);
+#ifndef MBEDIT_H
+#define MBEDIT_H
+
+#include "mbedit_prog.h"
 
 void do_mbedit_init(int argc, char **argv);
 void do_parse_datalist(char *file, int form);
@@ -191,3 +115,5 @@ void BxUnmanageCB(Widget w, XtPointer client, XtPointer call);
 void BxManageCB(Widget w, XtPointer client, XtPointer call);
 void BxPopupCB(Widget w, XtPointer client, XtPointer call);
 XtPointer BX_CONVERT(Widget w, char *from_string, char *to_type, int to_size, Boolean *success);
+
+#endif
