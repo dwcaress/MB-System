465feeedac74e2dca70531d3a047261b51cd9de7
Tom O'Reilly <oreilly@mbari.org>
2024-04-11 09:06:55 -0700

    added doxygen comments

---
+++ src/qt-guilib/ClickableLabel.h
@@ -0,0 +1,47 @@
+
+#ifndef CLICKABLELABLE_H
+#define CLICKABLELABLE_H
+
+#include <QObject>
+#include <QLabel>
+#include <QMouseEvent>
+#include <Qt>
+
+namespace mb_system {
+
+  /**
+     ClickableLabel is a QLabel widget that emits a signal when mouse button
+     is pressed, released, or while the mouse is dragged in it, with
+     QMouseEvent as signal payload. Typically used to display a QPixmap,
+     which the user interacts with using the mouse.
+
+   */
+  class ClickableLabel : public QLabel {
+
+    Q_OBJECT
+
+  public:
+
+    ClickableLabel(QWidget* parent = Q_NULLPTR,
+		   Qt::WindowFlags f = Qt::WindowFlags());
+    
+  protected:
+
+    /// Emit signal with mouse press event payload 
+    virtual void mousePressEvent(QMouseEvent *event) override;
+
+    /// Emit signal with mouse release event payload     
+    virtual void mouseReleaseEvent(QMouseEvent *event) override;
+
+    /// Emit signal with mouse drag event payload     
+    virtual void mouseMoveEvent(QMouseEvent *event) override;    
+
+
+  signals:
+    void labelMouseEvent(QMouseEvent *event);
+    
+  };
+}
+
+#endif
+
