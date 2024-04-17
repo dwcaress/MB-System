8ff3e1a0237c3d73f06b78a0206613b20d052e40
Tom O'Reilly <oreilly@mbari.org>
2024-04-17 16:49:36 -0700

    added debug

--- src/qt-mbedit-qtquick/PixmapImage.cpp
+++ src/qt-mbedit-qtquick/PixmapImage.cpp
@@ -6,14 +6,15 @@ PixmapImage::PixmapImage(QQuickItem *parent) :
 
 void PixmapImage::setImage(QObject *pixmapContainer)
 {
-    PixmapContainer *pc = qobject_cast<PixmapContainer*>(pixmapContainer);
-    Q_ASSERT(pc);
-    pixmapContainer_.pixmap = pc->pixmap;
-    update();
+  PixmapContainer *pc = qobject_cast<PixmapContainer*>(pixmapContainer);
+  Q_ASSERT(pc);
+  pixmapContainer_.pixmap = pc->pixmap;
+  update();
 }
 
 void PixmapImage::paint(QPainter *painter)
 {
-    painter->drawPixmap(0, 0, width(), height(), pixmapContainer_.pixmap);
+  qDebug() << "*** PixmapImage::paint()";
+  painter->drawPixmap(0, 0, width(), height(), *pixmapContainer_.pixmap);
 }
 
