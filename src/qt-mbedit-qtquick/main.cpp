148935c2d31ce423d7a9f929a4b2d1685f8891a0
Tom O'Reilly <oreilly@mbari.org>
2024-04-19 13:52:49 -0700

    connect QML-emitted signals, in some case "custom" signals (for QButtonGroup, which does not appear to emit expected signals)

--- src/qt-mbedit-qtquick/main.cpp
+++ src/qt-mbedit-qtquick/main.cpp
@@ -2,7 +2,8 @@
 #include <QQmlApplicationEngine>
 #include <QQuickView>
 #include <QQuickItem>
-#include <QQmlContext>
+#include <QAbstractButton>
+#include <QButtonGroup>
 #include "GuiNames.h"
 #include "MainWindow.h"
 #include "PixmapImage.h"
@@ -17,24 +18,7 @@ int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    GuiNames guiNames;
 
-    // Make GUI component names accessible to QML
-    engine.rootContext()->setContextProperty("xTrackSliderObj",
-					     guiNames.xTrackSlider());
-
-    engine.rootContext()->setContextProperty("pingsShownSliderObj",
-					     guiNames.pingsShownSlider());
-
-    engine.rootContext()->setContextProperty("verticalExaggSliderObj",
-					     guiNames.verticalExaggSlider());
-
-    engine.rootContext()->setContextProperty("pingStepSliderObj",
-					     guiNames.pingStepSlider());
-
-    engine.rootContext()->setContextProperty("swathPixmapObj",
-					     guiNames.swathPixmap());    
-    
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
@@ -42,35 +26,97 @@ int main(int argc, char *argv[]) {
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
 
-    qmlRegisterType<PixmapContainer>("bogus", 1, 0, "PixmapContainer");
-    qmlRegisterType<PixmapImage>("bogus", 1, 0, "PixmapImage");    
+    qmlRegisterType<PixmapContainer>("PixmapImage", 1, 0, "PixmapContainer");
+    qmlRegisterType<PixmapImage>("PixmapImage", 1, 0, "PixmapImage");    
     
     engine.load(url);
 
     QObject *rootObject = engine.rootObjects().value(0);
     
-    MainWindow mainWindow(rootObject);
+    MainWindow mainWindow(rootObject, argc, argv);
 
     // MainWindow receives signals from GUI
-    QQuickItem *slider =
-      rootObject->findChild<QQuickItem*>(guiNames.xTrackSlider());
+    /* ***
+    QQuickItem *qqItem =
+      rootObject->findChild<QQuickItem*>(XTRACK_SLIDER_NAME);
 
-    QObject::connect((QObject *)slider, SIGNAL(valueChanged()),
+    QObject::connect((QObject *)qqItem, SIGNAL(valueChanged()),
                      &mainWindow, SLOT(onXtrackSliderChanged()));
 
-    slider =
-      rootObject->findChild<QQuickItem*>(guiNames.verticalExaggSlider());
+		     *** */
 
-    QObject::connect((QObject *)slider, SIGNAL(valueChanged()),
+    // Can we find and connect as just QObject type?
+    QObject *obj2 =
+      rootObject->findChild<QObject*>(XTRACK_SLIDER_NAME);
+
+    QObject::connect((QObject *)obj2, SIGNAL(valueChanged()),
+                     &mainWindow, SLOT(onXtrackSliderChanged()));
+    
+    
+    QQuickItem *qqItem =
+      rootObject->findChild<QQuickItem*>(PINGS_SHOWN_SLIDER_NAME);
+    
+    QObject::connect((QObject *)qqItem, SIGNAL(valueChanged()),
+                     &mainWindow, SLOT(onPingsShownSliderChanged()));
+    
+    qqItem =
+      rootObject->findChild<QQuickItem*>(VERTICAL_EXAGG_SLIDER_NAME);
+    
+    QObject::connect((QObject *)qqItem, SIGNAL(valueChanged()),
                      &mainWindow, SLOT(onVerticalExaggSliderChanged()));
 
-    slider =
-      rootObject->findChild<QQuickItem*>(guiNames.pingStepSlider());
+    qqItem =
+      rootObject->findChild<QQuickItem*>(PING_STEP_SLIDER_NAME);
 
-    QObject::connect((QObject *)slider, SIGNAL(valueChanged()),
-                     &mainWindow, SLOT(onPingStepSliderChanged()));
+    qDebug() << "qqItem object info:";
+    qqItem->dumpObjectInfo();
     
+    QObject::connect((QObject *)qqItem,
+		     SIGNAL(valueChanged()),
+		     &mainWindow, SLOT(onPingStepSliderChanged()));
+
+    qDebug() << "*** TEST connect " << PING_STEP_SLIDER_NAME <<
+      " to baselineOffsetChanged(double)";
+
+    if (!QObject::connect((QObject *)qqItem,
+			  SIGNAL(baselineOffsetChanged(double)),
+			  &mainWindow, SLOT(testSlot(double)))) {
+      qWarning() << "connect() to testSlot() failed";
+    }
+    else {
+      qDebug() << "connect() to testSlot() OK";
+    }
     
+    // NOTE: can find QButtonGroup only if cast to QObject *, for some odd
+    // reason
+    QObject *buttonGroup =
+      rootObject->findChild<QObject*>(EDIT_MODES_NAME);
+
+    if (!buttonGroup) {
+      qWarning() << "Couldn't find QML object named " << EDIT_MODES_NAME;
+    }
+
+    qDebug() << "buttonGroup object info:";
+    buttonGroup->dumpObjectInfo();
+
+    qDebug() << "connect to ButtonGroup clicked signal";
+    if (!QObject::connect((QObject *)buttonGroup,
+			  SIGNAL(clicked()),
+			  &mainWindow, SLOT(onEditModesChanged()))) {
+      qWarning() << "connect() to clicked signal failed";
+    }
+
+    // Connect QML-emitted 'custom' signals
+    QObject::connect(rootObject, SIGNAL(qmlSignal(QString)),
+                     &mainWindow, SLOT(qmlSigSlot(QString)));
+
+    /* ***
+    MainWindow::connectRadioButton(rootObject, &mainWindow, TOGGLE_EDIT_NAME);
+    MainWindow::connectRadioButton(rootObject, &mainWindow, PICK_EDIT_NAME);
+    MainWindow::connectRadioButton(rootObject, &mainWindow, ERASE_EDIT_NAME);
+    MainWindow::connectRadioButton(rootObject, &mainWindow, RESTORE_EDIT_NAME);
+    MainWindow::connectRadioButton(rootObject, &mainWindow, GRAB_EDIT_NAME);    
+    *** */
     return app.exec();
 }
 
