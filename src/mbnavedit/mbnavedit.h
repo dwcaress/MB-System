2f5317debd415e71eebf9c9037c64a08ac44fa71
Tom O'Reilly <oreilly@mbari.org>
2024-06-06 15:46:48 -0700

    includes mbnavedit_prog.h

--- src/mbnavedit/mbnavedit.h
+++ src/mbnavedit/mbnavedit.h
@@ -36,6 +36,7 @@
 #define MBNAVEDIT_MBNAVEDIT_H_
 
 #include "mb_status.h"
+#include "mbnavedit_prog.h"
 
 #ifdef MBNAVEDIT_DECLARE_GLOBALS
 #define MBNAVEDIT_EXTERNAL
@@ -218,63 +219,6 @@ int do_error_dialog(char *s1, char *s2, char *s3);
 void set_label_string(Widget w, String str);
 void set_label_multiline_string(Widget w, String str);
 void get_text_string(Widget w, String str);
-
-int mbnavedit_init_globals(void);
-int mbnavedit_init(int argc, char **argv, int *startup_file);
-int mbnavedit_set_graphics(void *xgid, int ncol, unsigned int *pixels);
-int mbnavedit_action_open(int useprevious);
-int mbnavedit_open_file(int useprevious);
-int mbnavedit_close_file(void);
-int mbnavedit_dump_data(int hold);
-int mbnavedit_load_data(void);
-int mbnavedit_clear_screen(void);
-int mbnavedit_action_next_buffer(int *quit);
-int mbnavedit_action_offset(void);
-int mbnavedit_action_close(void);
-int mbnavedit_action_done(int *quit);
-int mbnavedit_action_quit(void);
-int mbnavedit_action_step(int step);
-int mbnavedit_action_start(void);
-int mbnavedit_action_end(void);
-int mbnavedit_action_mouse_pick(int xx, int yy);
-int mbnavedit_action_mouse_select(int xx, int yy);
-int mbnavedit_action_mouse_deselect(int xx, int yy);
-int mbnavedit_action_mouse_selectall(int xx, int yy);
-int mbnavedit_action_mouse_deselectall(int xx, int yy);
-int mbnavedit_action_deselect_all(int type);
-int mbnavedit_action_set_interval(int xx, int yy, int which);
-int mbnavedit_action_use_dr(void);
-int mbnavedit_action_use_smg(void);
-int mbnavedit_action_use_cmg(void);
-int mbnavedit_action_interpolate(void);
-int mbnavedit_action_interpolaterepeats(void);
-int mbnavedit_action_revert(void);
-int mbnavedit_action_flag(void);
-int mbnavedit_action_unflag(void);
-int mbnavedit_action_fixtime(void);
-int mbnavedit_action_deletebadtime(void);
-int mbnavedit_action_showall(void);
-int mbnavedit_get_smgcmg(int i);
-int mbnavedit_get_model(void);
-int mbnavedit_get_gaussianmean(void);
-int mbnavedit_get_dr(void);
-int mbnavedit_get_inversion(void);
-int mbnavedit_plot_all(void);
-int mbnavedit_plot_tint(int iplot);
-int mbnavedit_plot_lon(int iplot);
-int mbnavedit_plot_lat(int iplot);
-int mbnavedit_plot_speed(int iplot);
-int mbnavedit_plot_heading(int iplot);
-int mbnavedit_plot_draft(int iplot);
-int mbnavedit_plot_roll(int iplot);
-int mbnavedit_plot_pitch(int iplot);
-int mbnavedit_plot_heave(int iplot);
-int mbnavedit_plot_tint_value(int iplot, int iping);
-int mbnavedit_plot_lon_value(int iplot, int iping);
-int mbnavedit_plot_lat_value(int iplot, int iping);
-int mbnavedit_plot_speed_value(int iplot, int iping);
-int mbnavedit_plot_heading_value(int iplot, int iping);
-int mbnavedit_plot_draft_value(int iplot, int iping);
 
 void BxUnmanageCB(Widget w, XtPointer client, XtPointer call);
 void BxManageCB(Widget w, XtPointer client, XtPointer call);
