23909b8e546a86d3dbac8d3f9de75defcf31cfa4
David Caress <caress@mbari.org>
2024-06-09 23:41:46 -0700

    Mbmapscale and mbnavadjustmerge
    Mbmapscale - added program that outputs local scaling between longitude and latitude and easting and northing values.
    
    Mbnavadjustmerge: fixed copying of section triangle files.

--- src/mbio/mb_define.h
+++ src/mbio/mb_define.h
@@ -559,6 +559,7 @@ int mb_buffer_get_kind(int verbose, void *buff_ptr, void *mbio_ptr, int id, int 
 int mb_buffer_get_ptr(int verbose, void *buff_ptr, void *mbio_ptr, int id, void **store_ptr, int *error);
 
 int mb_coor_scale(int verbose, double latitude, double *mtodeglon, double *mtodeglat);
+int mb_alvinxy_scale(int verbose, double latitude, double *mtodeglon, double *mtodeglat);
 int mb_apply_lonflip(int verbose, int lonflip, double *longitude);
 
 int mb_error(int, int, char **);
