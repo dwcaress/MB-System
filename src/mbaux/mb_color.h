73b00855da6c118d84811f8f5c298cc7b02c57ff
Tom O'Reilly <oreilly@mbari.org>
2024-06-12 10:54:37 -0700

    define drawing colors, etc - in windows-system agnostic way

---
+++ src/mbaux/mb_color.h
@@ -0,0 +1,24 @@
+#ifndef MB_COLOR_H
+#define MB_COLOR_H
+/* **
+This header does not contain window-system specific references
+*/
+
+/// Standard mb-system drawing colors
+typedef enum {
+  White = 0,
+  Black,
+  Red,
+  Green,
+  Blue,
+  Orange,
+  Purple,
+  Coral,
+  LightGrey,
+  
+  NDrawingColors
+
+} DrawingColor;
+
+
+#endif
