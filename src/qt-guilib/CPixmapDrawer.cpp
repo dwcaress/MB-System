b7b956444e481a4106ce10a7a4fd853ead2b0a18
Tom O'Reilly <oreilly@mbari.org>
2024-06-19 10:28:38 -0700

    changed class name from PixmapDrawer to CPixmapDrawer

---
+++ src/qt-guilib/CPixmapDrawer.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <QTextStream>
+#include <QtGlobal>
+#include "CPixmapDrawer.h"
+
+
+QPainter *mb_system::CPixmapDrawer::painter_ = nullptr;
+QFontMetrics *mb_system::CPixmapDrawer::fontMetrics_ = nullptr;
+QString mb_system::CPixmapDrawer::textBuf_;
+
+using namespace mb_system;
+
+
+CPixmapDrawer::CPixmapDrawer(QPainter *painter) {
+
+  if (!painter) {
+    qWarning("CPixmapDrawer::CPixmapDrawer(): null painter pointer");
+  }
+  
+  painter_ = painter;
+}
+
+
+void CPixmapDrawer::drawLine(void *dummy,
+			    int x1, int y1, int x2, int y2,
+			    DrawingColor color, int style) {
+
+  setPenColorAndStyle(color, style);
+  
+  painter_->drawLine(x1, y1, x2, y2);
+}
+
+
+
+void CPixmapDrawer::drawRect(void *dummy,
+			  int x, int y, int width, int height,
+			  DrawingColor color, int style) {
+
+  setPenColorAndStyle(color, style);
+
+  painter_->drawRect(x, y, width, height);
+}
+
+
+void CPixmapDrawer::drawString(void *dummy, int x, int y, char *string,
+			    DrawingColor color, int style) {
+
+  QString textBuf;
+  QTextStream(&textBuf) << string;
+  setPenColorAndStyle(color, style);
+  painter_->drawText(x, y, textBuf);
+}
+
+
+void CPixmapDrawer::fillRect(void *dummy,
+			  int x, int y, int width, int height,
+			  DrawingColor color, int style) {
+
+  setPenColorAndStyle(color, style);
+
+  // Set fill color
+  painter_->fillRect(x, y, width, height, colorName(color));
+}
+
+
+void CPixmapDrawer::justifyString(void *dummy, char *string,
+			       int *width, int *ascent, int *descent) {
+
+  if (!fontMetrics_) {
+    fontMetrics_ = new QFontMetrics(painter_->font());
+  }
+  
+  *width = fontMetrics_->width(string);
+  *ascent = fontMetrics_->ascent();
+  *descent = fontMetrics_->descent();
+}
+
+
+const char *CPixmapDrawer::colorName(DrawingColor color) {
+  switch (color) {
+  case WHITE:
+    return "white";
+
+  case BLACK:
+    return "black";
+
+  case RED:
+    return "red";
+
+  case GREEN:
+    return "green";
+    
+  case BLUE:
+    return "blue";
+
+  case ORANGE:
+    return "orange";
+
+  case PURPLE:
+    return "purple";
+    
+  case CORAL:
+    return "coral";
+
+  case LIGHTGREY:
+    return "lightGray";
+
+  default:
+    std::cerr << "colorName(): unknown color!\n";
+    return "black";
+  }  
+
+}
+
+
+void CPixmapDrawer::setPenColorAndStyle(DrawingColor color, int style) {
+
+  if (style == DASH_LINE) {
+    painter_->setPen(Qt::DashLine);
+  }
+  else {
+    painter_->setPen(Qt::SolidLine);    
+  }
+  painter_->setPen(colorName(color));
+  
+}
---
+++ src/qt-guilib/CPixmapDrawer.h
@@ -0,0 +1,62 @@
+#ifndef CPIXMAPDRAWER_H
+#define CPIXMAPDRAWER_H
+
+#include <QString>
+#include <QColor>
+#include <QPainter>
+#include <QPixmap>
+#include <QFontMetrics>
+#include "mb_color.h"
+
+namespace mb_system {
+
+  ///
+  /// Pointers to static methods of this class can be passed to and invoked
+  /// by C code
+  class CPixmapDrawer {
+
+  public:
+
+    CPixmapDrawer(QPainter *painter);
+    
+    static void drawLine(void *dummy, int x1, int y1, int x2, int y2,
+		       DrawingColor color, int style);
+  
+    static void drawRect(void *dummy, int x, int y, int width, int height,
+		       DrawingColor color, int style);
+  
+    static void fillRect(void *dummy, int x, int y, int width, int height,
+			 DrawingColor color, int style);
+
+    static void drawString(void *dummy, int x, int y, char *string,
+			   DrawingColor color, int style);
+  
+    static void justifyString(void *dummy, char *string, int *width,
+			      int *ascent, int *descent);
+
+    /// Return color name corresponding to input mbedit_color
+    static const char *colorName(DrawingColor color);
+
+    /// Set QPainter static pointer member
+    static void setPainter(QPainter *painter) {
+      painter_ = painter;
+    }
+    
+  protected:
+
+    /// Set QPainter pen color and style
+    static void setPenColorAndStyle(DrawingColor color, int style);
+
+    
+    static QPainter *painter_;
+    static QString textBuf_;
+    static QFontMetrics *fontMetrics_;    
+  };
+
+  
+}
+
+
+
+#endif
+
