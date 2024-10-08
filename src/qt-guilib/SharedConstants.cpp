e6bcb3e86e21aaabc73108df87206fcd405a2ed3
Tom O'Reilly <oreilly@mbari.org>
2024-10-07 17:51:21 -0700

    initial commit

---
+++ src/qt-guilib/SharedConstants.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "SharedConstants.h"
+#include "TopoColorMap.h"
+
+using namespace mb_system;
+
+const QString SharedConstants::testString_ = QString("Hello sailor!");
+
+SharedConstants::SharedConstants() {
+  
+    // Get colormap names to be displayed by QML GUI
+    std::vector<const char *> colorMapNames;
+
+    // Load colormap names into vector
+    TopoColorMap::schemeNames(&colorMapNames);
+
+    std::cout << "ColorMaps:\n";
+    // Copy colormap names to QStringList
+    for (int i = 0; i < colorMapNames.size(); i++) {
+      std::cout << colorMapNames[i] << "\n";
+      // Append name to QStringList
+      colorMapsList_.append(colorMapNames[i]);
+    }
+}
---
+++ src/qt-guilib/SharedConstants.h
@@ -0,0 +1,47 @@
+#ifndef SHAREDCONSTANTS_H
+#define SHAREDCONSTANTS_H
+#include <QObject>
+#include <QStringList>
+
+namespace mb_system {
+/// Constants shared between C++ and QML code
+class SharedConstants : public QObject
+{
+    Q_OBJECT
+
+public:
+
+  SharedConstants();
+  
+  enum class EditState : int {
+			      ViewOnly,
+			      EditRoute,
+			      EditPoints,
+			      EditOverlay
+  };
+
+  Q_ENUM(EditState)
+
+    
+  /// Define read-only QString property called "testString"
+  Q_PROPERTY(QString testString READ getTestString)
+
+  /// Just return a dummy string for now...
+  QString getTestString() const {
+      std::cerr << "**** getTestString()\n";
+      return testString_;
+  }
+
+  Q_PROPERTY(QStringList cmaps MEMBER colorMapsList_)
+  
+protected:
+      
+  static const QString testString_;
+
+  QStringList colorMapsList_;
+  
+  
+};
+
+}
+#endif
