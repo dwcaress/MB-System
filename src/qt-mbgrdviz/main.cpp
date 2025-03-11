e2a79fb2e4d83bcc835bf57d7e992a7e5264cbaf
Tom O'Reilly <oreilly@mbari.org>
2025-03-11 12:32:47 -0700

    specify input file with -I

--- src/qt-mbgrdviz/main.cpp
+++ src/qt-mbgrdviz/main.cpp
@@ -1,6 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
+#include <QtGraphs/QAbstractAxis>    /// TEST
 #include <vtk-9.3/QQuickVTKItem.h>
 #include <vtk-9.3/vtkPolyDataMapper.h>
 #include <vtk-9.3/vtkActor.h>
@@ -8,8 +9,11 @@
 #include <vtk-9.3/vtkConeSource.h>
 #include <vtk-9.3/vtkRenderWindow.h>
 #include "TopoDataItem.h"
+#include "TopoProfileItem.h"
 #include "SharedConstants.h"
+#include "PixmapImage.h"
 
+using namespace std;
 using namespace mb_system;
 
 #define TopoDataItemName "topoDataItem"
@@ -17,9 +21,40 @@ using namespace mb_system;
 int main(int argc, char* argv[])
 {
   char *gridFilename = nullptr;
-  if (argc > 1) {
-    gridFilename = argv[1];
+
+  bool error = false;
+  for (int i = 1; i < argc; i++) {
+    if (!strcmp(argv[i], "-I") && i <= argc-2) {
+      gridFilename = argv[++i];
+    }
+    else if (!strcmp(argv[i], "-testpoints") && i <= argc-2) {
+      // Parse two points from next argument:
+      // beginX,beginY,endX,endY
+      std::stringstream input(argv[++i]);
+      string token;
+      char delim = ',';
+      int nToken = 0;
+      while (getline(input, token, delim)) {
+	cout << token << " ";
+	nToken++;
+      }
+      cout << "\n";
+      if (nToken != 4) {
+	cerr << "Expecting 4 coords\n";
+      }
+      
+    }
+    else {
+      cerr << "Unknown option: " << argv[i] << "\n";
+      error = true;
+    }
   }
+
+  if (error) {
+    cerr << "usage: " << argv[0] << "[-I inputFle][-testpoints x1,y1,x2,y2]\n";
+    exit(1);
+  }
+
 
   // Sets the graphics API to OpenGLRhi and sets up the surface format for
   // intermixed VTK and QtQuick rendering. 
@@ -34,7 +69,14 @@ int main(int argc, char* argv[])
 
   // Register SharedConstants type
   qmlRegisterType<SharedConstants>("SharedConstants", 1, 1, "SharedConstants");
+
+  // Register TopoProfileItem type
+  qmlRegisterType<TopoProfileItem>("TopoProfileItem", 1, 0, "TopoProfileItem");
   
+  // ui-components/TopoProfileWindow.qml instantiates PixmapImage, and C++
+  // will draw to that - so register PixmapImage class with QML
+  qmlRegisterType<mb_system::PixmapImage>("PixmapImage", 1, 0,
+					  "PixmapImage");      
   engine.load(QUrl("qrc:/main.qml"));
  
   QObject* topLevel = engine.rootObjects().value(0);
