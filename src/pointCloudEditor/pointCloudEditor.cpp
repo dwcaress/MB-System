b5966d18e50f8252c936a9d2b117bc0a7edb5b69
Tom O'Reilly <oreilly@mbari.org>
2025-07-28 17:45:16 -0700

    display good quality as green, bad quality as red

--- src/pointCloudEditor/pointCloudEditor.cpp
+++ src/pointCloudEditor/pointCloudEditor.cpp
@@ -44,147 +44,48 @@
 #define GOOD 1
 #define BAD 0
 
-// Define interaction style
-class HighlightInteractorStyle : public vtkInteractorStyleRubberBandPick
-{
-public:
-  static HighlightInteractorStyle* New();
-  vtkTypeMacro(HighlightInteractorStyle, vtkInteractorStyleRubberBandPick);
 
-  HighlightInteractorStyle() : vtkInteractorStyleRubberBandPick()
+class PointCloudEditor;
+
+// Define interaction style
+class PointsSelectInteractorStyle : public vtkInteractorStyleRubberBandPick {
+public:
+  static PointsSelectInteractorStyle* New();
+  vtkTypeMacro(PointsSelectInteractorStyle, vtkInteractorStyleRubberBandPick);
+
+  PointsSelectInteractorStyle() : vtkInteractorStyleRubberBandPick()
   {
     selectedMapper_ = vtkSmartPointer<vtkDataSetMapper>::New();
     selectedActor_ = vtkSmartPointer<vtkActor>::New();
     selectedActor_->SetMapper(selectedMapper_);
   }
 
-  virtual void OnLeftButtonUp() override
-  {
-    // Forward events
-    vtkInteractorStyleRubberBandPick::OnLeftButtonUp();
+  virtual void OnLeftButtonUp() override;
 
-    if (CurrentMode == VTKISRBP_SELECT)
-    {
-      vtkNew<vtkNamedColors> colors;
-
-      vtkPlanes* frustum =
-          static_cast<vtkAreaPicker*>(GetInteractor()->GetPicker())
-              ->GetFrustum();
-
-      vtkNew<vtkExtractPolyDataGeometry> extractor;
-      extractor->SetInputData(polyData_);
-
-      // Extract cells that lie within the user-specified frustrum
-      extractor->SetImplicitFunction(frustum);
-      extractor->ExtractInsideOn();  // T.O'R.
-      // Extract the cells
-      extractor->Update();
-
-      vtkPolyData *extractedData = extractor->GetOutput();
-      
-      std::cout << "Extracted "
-                << extractedData->GetNumberOfCells()
-                << " cells." << std::endl;
-
-      // Set mapper input to extracted cells
-      // (Color is not controlled by scalar)
-      selectedMapper_->SetInputData(extractedData);
-      selectedMapper_->ScalarVisibilityOff();
-
-      selectedActor_->GetProperty()->SetColor(
-				      colors->GetColor3d("Red").GetData());
-      
-      selectedActor_->GetProperty()->SetPointSize(5);
-      // selectedActor_->GetProperty()->SetRepresentationToWireframe();
-      // selectedActor_->GetProperty()->SetRepresentationToSurface();
-      selectedActor_->GetProperty()->SetRepresentationToPoints();
-
-      GetInteractor()
-          ->GetRenderWindow()
-          ->GetRenderers()
-          ->GetFirstRenderer()
-          ->AddActor(selectedActor_);
-      
-      GetInteractor()->GetRenderWindow()->Render();
-
-      // Highlight the selected area
-      HighlightProp(nullptr);
-
-      // Get the points
-      vtkPoints *points = extractedData->GetPoints();
-      std::cout << "Got " << points->GetNumberOfPoints() << " points\n";
-      for (int i = 0; i < points->GetNumberOfPoints(); i++) {
-	double *point = points->GetPoint(i);
-	std::cout << "pt " << i << ": "
-		  << point[0] << ", " << point[1] << ", " << point[2] << "\n";
-      }
-
-
-      // Get subsetted original point IDs
-      vtkIdTypeArray *filteredPointIds =
-	vtkIdTypeArray::SafeDownCast(extractedData->GetPointData()->
-				     GetArray(ORIGINAL_IDS));
-
-      if (filteredPointIds) {
-	std::cout << "Got original point IDs\n";
-	vtkPoints *points = polyData_->GetPoints();
-	vtkIntArray* quality =
-	  vtkIntArray::SafeDownCast(polyData_->GetPointData()->
-				    GetArray(DATA_QUALITY_NAME));
-
-	if (!quality) {
-	  std::cerr << "Couldn't find " << DATA_QUALITY_NAME << "!!\n";
-	}
-	else {
-	  std::cerr << "FOUND " << DATA_QUALITY_NAME << "!!\n";	  
-	}
-	
-	for (int i = 0; i < extractedData->GetNumberOfPoints(); i++) {
-	  vtkIdType pointId = filteredPointIds->GetValue(i);
-	  std::cout << "subset point " << i << " -> original point " <<
-	    pointId << "\n";
-	  double xyz[3];
-	  points->GetPoint(pointId, xyz);
-	  std::cout << "x: " << xyz[0] << ", y: " << xyz[1] <<
-	    ", z: " << xyz[2] << "\n";
-
-	  // Set selected point data quality to BAD
-	  if (quality) {
-	    quality->SetValue(pointId, BAD);
-	  }
-
-	}
-      }
-      else {
-	std::cout << "Couldn't get original point Ids\n";
-      }
-    }
-  }
-
-  void setPolyData(vtkSmartPointer<vtkPolyData> polyData)
-  {
-    polyData_ = polyData;
+  void setEditor(PointCloudEditor *editor) {
+    editor_ = editor;
   }
 
 private:
-  vtkSmartPointer<vtkPolyData> polyData_;
-  vtkSmartPointer<vtkActor> selectedActor_;
-  vtkSmartPointer<vtkDataSetMapper> selectedMapper_;  
-};
   
-vtkStandardNewMacro(HighlightInteractorStyle);
+  PointCloudEditor *editor_;
+  vtkSmartPointer<vtkActor> selectedActor_;
+  vtkSmartPointer<vtkDataSetMapper> selectedMapper_;
 
-vtkSmartPointer<vtkPolyData> readPolyData(const char* fileName);
+  
+  /// vtkSmartPointer<vtkPolyData> readPolyData(const char* fileName);
+
+};
+
+vtkStandardNewMacro(PointsSelectInteractorStyle);
 
 
 class PointCloudEditor {
 
 public:
 
-  PointCloudEditor(vtkPolyData *polyData) {
+  PointCloudEditor(void) {
 
-    polyData_ = polyData;
-    
     // Build color lookup table
     lut_->SetNumberOfTableValues(2);
     lut_->SetRange(0, 1);
@@ -192,39 +93,26 @@ public:
     lut_->SetTableValue(1, 0.0, 1.0, 0.0, 1.0);  
     lut_->Build();
 
-    // Associate id's with original polyData
-    idFilter_->SetInputData(polyData_);
-
-    // Specify name by which to retrieve id's
-    idFilter_->SetCellIdsArrayName(ORIGINAL_IDS);
-    idFilter_->SetPointIdsArrayName(ORIGINAL_IDS);
-
-    idFilter_->Update();    
-    surfaceFilter_->SetInputConnection(idFilter_->GetOutputPort());
-    surfaceFilter_->Update();
-    polyData_ = surfaceFilter_->GetOutput();
-    
-    quality_->SetName(DATA_QUALITY_NAME);
-    quality_->SetNumberOfTuples(polyData_->GetNumberOfPoints());
-
-    // First assume all points are good
-    for (int i = 0; i < polyData_->GetNumberOfPoints(); i++) {
-      quality_->SetValue(i, GOOD);
-    }
-
-    // Associate quality array with original poly data
-    polyData_->GetPointData()->AddArray(quality_);
-    
+    style_->setEditor(this);
   }
 
+  /// Get point cloud vtkPolyData
+  vtkPolyData *polyData() {
+    return polyData_;
+  }
+
+  
   /// Visualize the point cloud
   void visualize(void) {
 
     mapper_->SetInputData(polyData_);
-    mapper_->ScalarVisibilityOff();
+    // mapper_->ScalarVisibilityOff();
 
     // Configure mapper to use LUT stuff
     mapper_->SetLookupTable(lut_);
+
+    polyData_->GetPointData()->SetActiveScalars(DATA_QUALITY_NAME);
+    
     mapper_->SetScalarModeToUsePointData();
     mapper_->SetColorModeToMapScalars();
     mapper_->SetScalarRange(0, 1);
@@ -232,9 +120,9 @@ public:
     actor_->GetProperty()->SetPointSize(5);
 
     /* ***
-    actor_->GetProperty()->
-      SetDiffuseColor(colors_->GetColor3d("Peacock").GetData());
-      *** */
+       actor_->GetProperty()->
+       SetDiffuseColor(colors_->GetColor3d("Peacock").GetData());
+       *** */
     
     renderer_->UseHiddenLineRemovalOn();
 
@@ -250,13 +138,63 @@ public:
 
     renderWindow_->Render();
 
-    style_->setPolyData(polyData_);
+    /// style_->setPolyData(polyData_);
     renderWindowInteractor_->SetInteractorStyle(style_);
 
     renderWindowInteractor_->Start();
   }
-
   
+  
+  bool readPolyData(const char* fileName) {
+    
+    std::string extension =
+      vtksys::SystemTools::GetFilenameLastExtension(std::string(fileName));
+
+    if (extension == ".grd" || extension == ".mb88") {
+      std::cerr << "read mb-system grid\n";
+      vtkSmartPointer<mb_system::TopoDataReader> reader =
+	vtkSmartPointer<mb_system::TopoDataReader>::New();
+      reader->SetFileName(fileName);
+      reader->Update();
+      polyData_ = reader->GetOutput();
+    }
+    else {
+      std::cerr << "unknown input file format: " << fileName << "\n";
+      std::cerr << "read sphere source\n";
+      vtkNew<vtkSphereSource> source;
+      source->SetPhiResolution(21);
+      source->SetThetaResolution(40);
+      source->Update();
+      polyData_ = source->GetOutput();
+    }
+    
+    // Associate id's with original polyData
+    idFilter_->SetInputData(polyData_);
+
+    // Specify name by which to retrieve id's
+    idFilter_->SetCellIdsArrayName(ORIGINAL_IDS);
+    idFilter_->SetPointIdsArrayName(ORIGINAL_IDS);
+
+    idFilter_->Update();    
+    surfaceFilter_->SetInputConnection(idFilter_->GetOutputPort());
+    surfaceFilter_->Update();
+    polyData_ = surfaceFilter_->GetOutput();
+    
+    quality_->SetName(DATA_QUALITY_NAME);
+    quality_->SetNumberOfTuples(polyData_->GetNumberOfPoints());
+    
+    // First assume all points are good
+    for (int i = 0; i < polyData_->GetNumberOfPoints(); i++) {
+      quality_->SetValue(i, GOOD);
+    }
+
+    // Associate quality array with original poly data
+    polyData_->GetPointData()->AddArray(quality_);    
+
+    return true;
+  }
+  
+
 protected:
   vtkNew<vtkAreaPicker> areaPicker_;
   vtkNew<vtkRenderWindow> renderWindow_;
@@ -269,15 +207,119 @@ protected:
   vtkNew<vtkRenderer> renderer_;
   vtkNew<vtkRenderWindowInteractor> renderWindowInteractor_;
 
-  vtkNew<HighlightInteractorStyle> style_;
+  vtkNew<PointsSelectInteractorStyle> style_;
 
   vtkSmartPointer<vtkPolyData> polyData_;
   
-  // Add data quality array to input vtkPolyData
+  // data quality array for input vtkPolyData
   vtkSmartPointer<vtkIntArray> quality_ =
     vtkSmartPointer<vtkIntArray>::New();
-
 };
+
+
+void PointsSelectInteractorStyle::OnLeftButtonUp() {
+  // Forward events
+  vtkInteractorStyleRubberBandPick::OnLeftButtonUp();
+  if (CurrentMode == VTKISRBP_SELECT) {
+    vtkNew<vtkNamedColors> colors;
+
+    vtkPlanes* frustum =
+      static_cast<vtkAreaPicker*>(GetInteractor()->GetPicker())
+      ->GetFrustum();
+
+    vtkNew<vtkExtractPolyDataGeometry> extractor;
+    extractor->SetInputData(editor_->polyData());
+
+    // Extract cells that lie within the user-specified frustrum
+    extractor->SetImplicitFunction(frustum);
+    extractor->ExtractInsideOn();  // T.O'R.
+    // Extract the cells
+    extractor->Update();
+
+    vtkPolyData *extractedData = extractor->GetOutput();
+      
+    std::cerr << "Extracted "
+	      << extractedData->GetNumberOfCells()
+	      << " cells." << std::endl;
+
+    // Set mapper input to extracted cells
+    // (Color is not controlled by scalar)
+    selectedMapper_->SetInputData(extractedData);
+    selectedMapper_->ScalarVisibilityOff();
+
+    selectedActor_->GetProperty()->SetColor(
+					    colors->GetColor3d("Red").GetData());
+      
+    selectedActor_->GetProperty()->SetPointSize(5);
+    // selectedActor_->GetProperty()->SetRepresentationToWireframe();
+    // selectedActor_->GetProperty()->SetRepresentationToSurface();
+    selectedActor_->GetProperty()->SetRepresentationToPoints();
+
+    GetInteractor()
+      ->GetRenderWindow()
+      ->GetRenderers()
+      ->GetFirstRenderer()
+      ->AddActor(selectedActor_);
+      
+    GetInteractor()->GetRenderWindow()->Render();
+
+    // Highlight the selected area
+    HighlightProp(nullptr);
+
+    // Get the points
+    vtkPoints *points = extractedData->GetPoints();
+    std::cerr << "Got " << points->GetNumberOfPoints() << " points\n";
+    for (int i = 0; i < points->GetNumberOfPoints(); i++) {
+      double *point = points->GetPoint(i);
+      std::cerr << "pt " << i << ": "
+		<< point[0] << ", " << point[1] << ", " << point[2] << "\n";
+    }
+
+
+    // Get subsetted original point IDs
+    vtkIdTypeArray *filteredPointIds =
+      vtkIdTypeArray::SafeDownCast(extractedData->GetPointData()->
+				   GetArray(ORIGINAL_IDS));
+
+    if (filteredPointIds) {
+      std::cerr << "Got original point IDs\n";
+      vtkPoints *points = editor_->polyData()->GetPoints();
+
+      vtkIntArray* quality =
+	vtkIntArray::SafeDownCast(editor_->polyData()->GetPointData()->
+				  GetArray(DATA_QUALITY_NAME));
+      if (!quality) {
+	std::cerr << "Couldn't find " << DATA_QUALITY_NAME << "!!\n";
+      }
+      else {
+	std::cerr << "FOUND " << DATA_QUALITY_NAME << "!!\n";	  
+      }
+	
+      for (int i = 0; i < extractedData->GetNumberOfPoints(); i++) {
+	vtkIdType pointId = filteredPointIds->GetValue(i);
+	std::cerr << "subset point " << i << " -> original point " <<
+	  pointId << "\n";
+	double xyz[3];
+	points->GetPoint(pointId, xyz);
+	std::cerr << "x: " << xyz[0] << ", y: " << xyz[1] <<
+	  ", z: " << xyz[2] << "\n";
+
+	// Set selected point data quality to BAD
+	if (quality) {
+	  std::cerr << "set value to BAD\n";
+	  quality->SetValue(pointId, BAD);
+	}
+
+      }
+    }
+    else {
+      std::cerr << "Couldn't get original point Ids\n";
+    }
+  }
+
+  std::cerr << "Visualize data\n";
+  editor_->visualize();
+}
 
 
 int main(int argc, char* argv[])
@@ -286,42 +328,17 @@ int main(int argc, char* argv[])
     std::cerr << "usage: " << argv[0] << " <swath-or-gridFile>\n";
     return 1;
   }
-  
-  // Get polyData from specified source
-  auto polyData = readPolyData(argv[1]);
 
-  auto editor = new PointCloudEditor(polyData);
+  PointCloudEditor *editor = new PointCloudEditor();
+
+  if (!editor->readPolyData(argv[1])) {
+    std::cerr << "Couldn't process " << argv[1] << "\n";
+    return 1;
+  }
+
   editor->visualize();
-
 
   return 0;
 }
 
-
-vtkSmartPointer<vtkPolyData> readPolyData(const char* fileName)
-{
-  vtkSmartPointer<vtkPolyData> polyData;
-  std::string extension =
-      vtksys::SystemTools::GetFilenameLastExtension(std::string(fileName));
-
-  if (extension == ".grd" || extension == ".mb88") {
-    std::cout << "read mb-system grid\n";
-    vtkSmartPointer<mb_system::TopoDataReader> reader =
-      vtkSmartPointer<mb_system::TopoDataReader>::New();
-    reader->SetFileName(fileName);
-    reader->Update();
-    polyData = reader->GetOutput();
-  }
-  else
-  {
-    std::cout << "unknown input file format: " << fileName << "\n";
-    std::cout << "read sphere source\n";
-    vtkNew<vtkSphereSource> source;
-    source->SetPhiResolution(21);
-    source->SetThetaResolution(40);
-    source->Update();
-    polyData = source->GetOutput();
-  }
-  return polyData;
-}
 
