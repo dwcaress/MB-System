66e213efbef424f44318cc155643e6804e2cb2a7
Mike McCann <MBARIMike@gmail.com>
2026-01-06 11:11:43 -0800

    Add --verbose to list files modified.

--- src/mbgrd2gltf/clang-format.sh
+++ src/mbgrd2gltf/clang-format.sh
@@ -7,6 +7,6 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 # Formatting only top-level .cpp and .h files
-clang-format -i -style=file ./*.cpp ./*.h
+clang-format -i --verbose  -style=file ./*.cpp ./*.h
 
 echo "Formatted top-level mbgrd2gltf .cpp and .h files."
