17aa6897bd407593671cecb1083fdae55305219e
Tom O'Reilly <tomoreilly@Mac.wifi.mbari.org>
2026-04-06 15:11:07 -0700

    Improved QProperty definitions

--- src/qt-mbnavedit/GuiNames.h
+++ src/qt-mbnavedit/GuiNames.h
@@ -4,42 +4,10 @@
 #include <QObject>
 
 
-/// Define names of C++-connected QML items, used when exchanging information
-/// between QML GUI and C++ code.
-/// N.B.: Take care that QML code uses the literal strings defined here
-/// objectName
-
-/// Name of PixmapImage C++ object instantiated by QML, which holds swath
-/// graphs; Backend C++ code draws into this object
-#define SWATH_PIXMAP_NAME "swathPixmapObj"
-
-/// Names of user-selected checkboxes that enable drawing of specific plots;
-/// passed by QML as arg to Backend C++ callbacks
-#define TIMEINT_PLOTNAME "timeInt"
-#define TIMEINT_ORIG_PLOTNAME "timeIntOrig"
-#define LAT_PLOTNAME "lat"
-#define LAT_ORIG_PLOTNAME "latOrig"
-#define LON_PLOTNAME "lon"
-#define LON_ORIG_PLOTNAME "lonOrig"
-#define SPEED_PLOTNAME "speed"
-#define SPEED_ORIG_PLOTNAME "speedOrig"
-#define SPEED_MADEGOOD_PLOTNAME "speedMadeGood"
-#define HEADING_PLOTNAME "heading"
-#define HEADING_ORIG_PLOTNAME "headingOrig"
-#define HEADING_MADEGOOD_PLOTNAME "headingMadeGood"
-#define SENSORDEPTH_PLOTNAME "sensorDepth"
-#define SENSORDEPTH_ORIG_PLOTNAME "sensorDepthOrig"
-#define ATTITUDE_PLOTNAME "attitude"
-
-/// Names of edit modes selected by user; passed by QML as arg to
-/// Backend C++ callbacks
-#define PICK_MODENAME "pickMode"
-#define SELECT_MODENAME "selectMode"
-#define DESELECT_MODENAME "deselectMode"
-#define SELECT_ALL_MODENAME "selectAllMode"
-#define DESELECT_ALL_MODENAME "deselectAllMode"
-#define DEFINE_INTERVAL_MODENAME "defineIntervalMode"
-
+/**
+   GuiNames defines QString properties used for objectName in QML GUI,
+   defined to match the string literals referenced by Backend C++ methods.
+*/
 class GuiNames : public QObject {
 
   Q_OBJECT
@@ -47,79 +15,94 @@ class GuiNames : public QObject {
  public:
   GuiNames() {}
   
-  Q_PROPERTY(QString swathPixmap READ swathPixmap CONSTANT)
-  QString swathPixmap() const {return QStringLiteral(SWATH_PIXMAP_NAME);}
-
-  Q_PROPERTY(QString timeIntPlot READ timeIntPlot CONSTANT)
-  QString timeIntPlot() const {return QStringLiteral(TIMEINT_PLOTNAME);}  
-
+  static inline const char *swathPixmapObjStr = "swathPixmapObj";
   Q_PROPERTY(QString swathPixmapObj READ swathPixmapObj CONSTANT)
-  QString swathPixmapObj() const {return QStringLiteral(SWATH_PIXMAP_NAME);}
+  QString swathPixmapObj() const {return QString::fromLatin1(swathPixmapObjStr);}
 
+  static inline const char *timeIntStr = "timeInt";
   Q_PROPERTY(QString timeInt READ timeInt CONSTANT)
-  QString timeInt() const {return QStringLiteral(TIMEINT_PLOTNAME);}
+  QString timeInt() const {return QString::fromLatin1(timeIntStr);}
 
+  static inline const char *timeIntOrigStr = "timeIntOrig";
   Q_PROPERTY(QString timeIntOrig READ timeIntOrig CONSTANT)
-  QString timeIntOrig() const {return QStringLiteral(TIMEINT_ORIG_PLOTNAME);}
+  QString timeIntOrig() const {return QString::fromLatin1(timeIntOrigStr);}
 
+  static inline const char *latStr = "lat";
   Q_PROPERTY(QString lat READ lat CONSTANT)
-  QString lat() const {return QStringLiteral(LAT_PLOTNAME);}
+  QString lat() const {return QString::fromLatin1(latStr);}
 
+  static inline const char *latOrigStr = "latOrig";
   Q_PROPERTY(QString latOrig READ latOrig CONSTANT)
-  QString latOrig() const {return QStringLiteral(LAT_ORIG_PLOTNAME);}
+  QString latOrig() const {return QString::fromLatin1(latOrigStr);}
 
+  static inline const char *lonStr = "lon";
   Q_PROPERTY(QString lon READ lon CONSTANT)
-  QString lon() const {return QStringLiteral(LON_PLOTNAME);}
+  QString lon() const {return QString::fromLatin1(lonStr);}
 
+  static inline const char *lonOrigStr = "lonOrig";
   Q_PROPERTY(QString lonOrig READ lonOrig CONSTANT)
-  QString lonOrig() const {return QStringLiteral(LON_ORIG_PLOTNAME);}
+  QString lonOrig() const {return QString::fromLatin1(lonOrigStr);}
 
+  static inline const char *speedStr = "speed";
   Q_PROPERTY(QString speed READ speed CONSTANT)
-  QString speed() const {return QStringLiteral(SPEED_PLOTNAME);}
+  QString speed() const {return QString::fromLatin1(speedStr);}
 
+  static inline const char *speedOrigStr = "speedOrig";
   Q_PROPERTY(QString speedOrig READ speedOrig CONSTANT)
-  QString speedOrig() const {return QStringLiteral(SPEED_ORIG_PLOTNAME);}
+  QString speedOrig() const {return QString::fromLatin1(speedOrigStr);}
 
+  static inline const char *speedMadeGoodStr = "speedMadeGood";
   Q_PROPERTY(QString speedMadeGood READ speedMadeGood CONSTANT)
-  QString speedMadeGood() const {return QStringLiteral(SPEED_MADEGOOD_PLOTNAME);}
+  QString speedMadeGood() const {return QString::fromLatin1(speedMadeGoodStr);}
 
+  static inline const char *headingStr = "heading";
   Q_PROPERTY(QString heading READ heading CONSTANT)
-  QString heading() const {return QStringLiteral(HEADING_PLOTNAME);}
+  QString heading() const {return QString::fromLatin1(headingStr);}
 
+  static inline const char *headingOrigStr = "headingOrig";
   Q_PROPERTY(QString headingOrig READ headingOrig CONSTANT)
-  QString headingOrig() const {return QStringLiteral(HEADING_ORIG_PLOTNAME);}
+  QString headingOrig() const {return QString::fromLatin1(headingOrigStr);}
 
+  static inline const char *headingMadeGoodStr = "headingMadeGood";
   Q_PROPERTY(QString headingMadeGood READ headingMadeGood CONSTANT)
-  QString headingMadeGood() const {return QStringLiteral(HEADING_MADEGOOD_PLOTNAME);}
+  QString headingMadeGood() const {return QString::fromLatin1(headingMadeGoodStr);}
 
+  static inline const char *sensorDepthStr = "sensorDepth";
   Q_PROPERTY(QString sensorDepth READ sensorDepth CONSTANT)
-  QString sensorDepth() const {return QStringLiteral(SENSORDEPTH_PLOTNAME);}
+  QString sensorDepth() const {return QString::fromLatin1(sensorDepthStr);}
 
+  static inline const char *sensorDepthOrigStr = "sensorDepthOrig";
   Q_PROPERTY(QString sensorDepthOrig READ sensorDepthOrig CONSTANT)
-  QString sensorDepthOrig() const {return QStringLiteral(SENSORDEPTH_ORIG_PLOTNAME);}
+  QString sensorDepthOrig() const {return QString::fromLatin1(sensorDepthOrigStr);}
 
+  static inline const char *attitudeStr = "attitude";
   Q_PROPERTY(QString attitude READ attitude CONSTANT)
-  QString attitude() const {return QStringLiteral(ATTITUDE_PLOTNAME);}
+  QString attitude() const {return QString::fromLatin1(attitudeStr);}
 
+  static inline const char *pickModeStr = "pickMode";
   Q_PROPERTY(QString pickMode READ pickMode CONSTANT)
-  QString pickMode() const {return QStringLiteral(PICK_MODENAME);}
+  QString pickMode() const {return QString::fromLatin1(pickModeStr);}
 
+  static inline const char *selectModeStr = "selectMode";
   Q_PROPERTY(QString selectMode READ selectMode CONSTANT)
-  QString selectMode() const {return QStringLiteral(SELECT_MODENAME);}
+  QString selectMode() const {return QString::fromLatin1(selectModeStr);}
 
+  static inline const char *deselectModeStr = "deselectMode";
   Q_PROPERTY(QString deselectMode READ deselectMode CONSTANT)
-  QString deselectMode() const {return QStringLiteral(DESELECT_MODENAME);}
+  QString deselectMode() const {return QString::fromLatin1(deselectModeStr);}
 
+  static inline const char *selectAllModeStr = "selectAllMode";
   Q_PROPERTY(QString selectAllMode READ selectAllMode CONSTANT)
-  QString selectAllMode() const {return QStringLiteral(SELECT_ALL_MODENAME);}
+  QString selectAllMode() const {return QString::fromLatin1(selectAllModeStr);}
 
+  static inline const char *deselectAllModeStr = "deselectAllMode";
   Q_PROPERTY(QString deselectAllMode READ deselectAllMode CONSTANT)
-  QString deselectAllMode()
-    const {return QStringLiteral(DESELECT_ALL_MODENAME);}
+  QString deselectAllMode() const {return QString::fromLatin1(deselectAllModeStr);}
 
+  static inline const char *defineIntervalModeStr = "defineIntervalMode";
   Q_PROPERTY(QString defineIntervalMode READ defineIntervalMode CONSTANT)
-  QString defineIntervalMode()
-    const {return QStringLiteral(DEFINE_INTERVAL_MODENAME);}
+  QString defineIntervalMode() const {return QString::fromLatin1(defineIntervalModeStr);}
+
   
 };
 
