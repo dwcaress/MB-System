57ea5d2f6bb8ba40717cbf82a5e4025e452bbed8
Tom O'Reilly <tomoreilly@Mac.wifi.mbari.org>
2026-04-06 14:21:12 -0700

    Improved QProperty definitions

--- src/qt-mbedit/GuiNames.h
+++ src/qt-mbedit/GuiNames.h
@@ -3,131 +3,130 @@
 
 #include <QObject>
 
-/// define names of C++-connected QML items, used when exchanging information
-/// between QML GUI and C++ code.
 
-/// Name of PixmapImage instantiated by QML, which holds swath graphs
-#define SWATH_PIXMAP_NAME "swathPixmapObj"
-
-/// Names of ancillary data options created by QML.
-#define NONE_ANCILLDATA "none"
-#define TIME_ANCILLDATA "time"
-#define INTERVAL_ANCILLDATA "interval"
-#define LATITUDE_ANCILLDATA "latitude"
-#define LONGITUDE_ANCILLDATA "longitude"
-#define HEADING_ANCILLDATA "heading"
-#define SPEED_ANCILLDATA "speed"
-#define DEPTH_ANCILLDATA "depth"
-#define ALTITUDE_ANCILLDATA "altitude"
-#define SENSORDEPTH_ANCILLDATA "sensorDepth"
-#define ROLL_ANCILLDATA "roll"
-#define PITCH_ANCILLDATA "pitch"
-#define HEAVE_ANCILLDATA "heave"
-
-/// Names of slice options created by QML
-#define ALONGTRACK_SLICE "alongTrack"
-#define WATERFALL_SLICE "waterfall"
-#define CROSSTRACK_SLICE "crossTrack"
-
-/// Names of color-coding options
-#define BOTTOM_DETECT_COLOR "bottomDetect"
-#define PULSE_SOURCE_COLOR "pulseSource"
-#define FLAG_STATE_COLOR "flagState"
-
-/// Names of edit modes
-#define TOGGLE_EDIT_MODE "toggleEdit"
-#define PICK_EDIT_MODE "pickEdit"
-#define ERASE_EDIT_MODE "eraseEdit"
-#define RESTORE_EDIT_MODE "restoreEdit"
-#define GRAB_EDIT_MODE "grabEdit"
-#define INFO_EDIT_MODE "infoEdit"
-
+/**
+   GuiNames defines QString properties used for objectName in QML GUI,
+   defined to match the string literals referenced by Backend C++ methods.
+*/
 class GuiNames : public QObject {
+
   Q_OBJECT
 
 public:
   
   GuiNames() {
   }
-  
-  
+
+  /// Name of PixmapImage instantiated by QML, which holds swath graphs
+  static inline const char *swathPixmapObjStr = "swathPixmapObj";
   Q_PROPERTY(QString swathPixmapObj READ swathPixmapObj CONSTANT)
-  QString swathPixmapObj() const {return QStringLiteral(SWATH_PIXMAP_NAME);}
+  QString swathPixmapObj() const {return QString::fromLatin1(swathPixmapObjStr);}
 
+  /// Names of ancillary data options created by QML.
+  static inline const char *noneStr = "none";
   Q_PROPERTY(QString none READ none CONSTANT)
-  QString none() const {return QStringLiteral(NONE_ANCILLDATA);}
+  QString none() const {return QString::fromLatin1(noneStr);}
 
+  static inline const char *timeStr = "time";
   Q_PROPERTY(QString time READ time CONSTANT)
-  QString time() const {return QStringLiteral(TIME_ANCILLDATA);}
+  QString time() const {return QString::fromLatin1(timeStr);}
 
+  static inline const char *intervalStr = "interval";
   Q_PROPERTY(QString interval READ interval CONSTANT)
-  QString interval() const {return QStringLiteral(INTERVAL_ANCILLDATA);}
+  QString interval() const {return QString::fromLatin1(intervalStr);}
 
+  static inline const char *latitudeStr = "latitude";
   Q_PROPERTY(QString latitude READ latitude CONSTANT)
-  QString latitude() const {return QStringLiteral(LATITUDE_ANCILLDATA);}
+  QString latitude() const {return QString::fromLatin1(latitudeStr);}
 
+  static inline const char *longitudeStr = "longitude";
   Q_PROPERTY(QString longitude READ longitude CONSTANT)
-  QString longitude() const {return QStringLiteral(LONGITUDE_ANCILLDATA);}
+  QString longitude() const {return QString::fromLatin1(longitudeStr);}
 
+  static inline const char *headingStr = "heading";
   Q_PROPERTY(QString heading READ heading CONSTANT)
-  QString heading() const {return QStringLiteral(HEADING_ANCILLDATA);}
+  QString heading() const {return QString::fromLatin1(headingStr);}
 
+  static inline const char *speedStr = "speed";
   Q_PROPERTY(QString speed READ speed CONSTANT)
-  QString speed() const {return QStringLiteral(SPEED_ANCILLDATA);}
+  QString speed() const {return QString::fromLatin1(speedStr);}
 
+  static inline const char *depthStr = "depth";
   Q_PROPERTY(QString depth READ depth CONSTANT)
-  QString depth() const {return QStringLiteral(DEPTH_ANCILLDATA);}
+  QString depth() const {return QString::fromLatin1(depthStr);}
 
+  static inline const char *altitudeStr = "altitude";
   Q_PROPERTY(QString altitude READ altitude CONSTANT)
-  QString altitude() const {return QStringLiteral(ALTITUDE_ANCILLDATA);}
+  QString altitude() const {return QString::fromLatin1(altitudeStr);}
 
+  static inline const char *sensorDepthStr = "sensorDepth";
   Q_PROPERTY(QString sensorDepth READ sensorDepth CONSTANT)
-  QString sensorDepth() const {return QStringLiteral(SENSORDEPTH_ANCILLDATA);}
+  QString sensorDepth() const {return QString::fromLatin1(sensorDepthStr);}
 
+  static inline const char *rollStr = "roll";
   Q_PROPERTY(QString roll READ roll CONSTANT)
-  QString roll() const {return QStringLiteral(ROLL_ANCILLDATA);}
+  QString roll() const {return QString::fromLatin1(rollStr);}
 
+  static inline const char *pitchStr = "pitch";
   Q_PROPERTY(QString pitch READ pitch CONSTANT)
-  QString pitch() const {return QStringLiteral(PITCH_ANCILLDATA);}
+  QString pitch() const {return QString::fromLatin1(pitchStr);}
 
+  static inline const char *heaveStr = "heave";
   Q_PROPERTY(QString heave READ heave CONSTANT)
-  QString heave() const {return QStringLiteral(HEAVE_ANCILLDATA);}
+  QString heave() const {return QString::fromLatin1(heaveStr);}
 
+  /// Names of slice options created by QML
+  static inline const char *alongTrackStr = "alongTrack";
   Q_PROPERTY(QString alongTrack READ alongTrack CONSTANT)
-  QString alongTrack() const {return QStringLiteral(ALONGTRACK_SLICE);}
+  QString alongTrack() const {return QString::fromLatin1(alongTrackStr);}
 
+  static inline const char *waterfallStr = "waterfall";
   Q_PROPERTY(QString waterfall READ waterfall CONSTANT)
-  QString waterfall() const {return QStringLiteral(WATERFALL_SLICE);}
+  QString waterfall() const {return QString::fromLatin1(waterfallStr);}
 
+  static inline const char *crossTrackStr = "crossTrack";
   Q_PROPERTY(QString crossTrack READ crossTrack CONSTANT)
-  QString crossTrack() const {return QStringLiteral(CROSSTRACK_SLICE);}
+  QString crossTrack() const {return QString::fromLatin1(crossTrackStr);}
 
+  /// Names of color-coding options
+  static inline const char *bottomDetectStr = "bottomDetect";
   Q_PROPERTY(QString bottomDetect READ bottomDetect CONSTANT)
-  QString bottomDetect() const {return QStringLiteral(BOTTOM_DETECT_COLOR);}
+  QString bottomDetect() const {return QString::fromLatin1(bottomDetectStr);}
 
+  static inline const char *pulseSourceStr = "pulseSource";
   Q_PROPERTY(QString pulseSource READ pulseSource CONSTANT)
-  QString pulseSource() const {return QStringLiteral(PULSE_SOURCE_COLOR);}
+  QString pulseSource() const {return QString::fromLatin1(pulseSourceStr);}
 
+  static inline const char *flagStateStr = "flagState";
   Q_PROPERTY(QString flagState READ flagState CONSTANT)
-  QString flagState() const {return QStringLiteral(FLAG_STATE_COLOR);}
+  QString flagState() const {return QString::fromLatin1(flagStateStr);}
 
+  /// Names of edit modes
+  static inline const char *toggleEditStr = "toggleEdit";
   Q_PROPERTY(QString toggleEdit READ toggleEdit CONSTANT)
-  QString toggleEdit() const {return QStringLiteral(TOGGLE_EDIT_MODE);}
+  QString toggleEdit() const {return QString::fromLatin1(toggleEditStr);}
 
+  static inline const char *pickEditStr = "pickEdit";
   Q_PROPERTY(QString pickEdit READ pickEdit CONSTANT)
-  QString pickEdit() const {return QStringLiteral(PICK_EDIT_MODE);}
+  QString pickEdit() const {return QString::fromLatin1(pickEditStr);}
 
+  static inline const char *eraseEditStr = "eraseEdit";
   Q_PROPERTY(QString eraseEdit READ eraseEdit CONSTANT)
-  QString eraseEdit() const {return QStringLiteral(ERASE_EDIT_MODE);}
+  QString eraseEdit() const {return QString::fromLatin1(eraseEditStr);}
 
+  static inline const char *restoreEditStr = "restoreEdit";
   Q_PROPERTY(QString restoreEdit READ restoreEdit CONSTANT)
-  QString restoreEdit() const {return QStringLiteral(RESTORE_EDIT_MODE);}
+  QString restoreEdit() const {return QString::fromLatin1(restoreEditStr);}
 
+  static inline const char *grabEditStr = "grabEdit";
   Q_PROPERTY(QString grabEdit READ grabEdit CONSTANT)
-  QString grabEdit() const {return QStringLiteral(GRAB_EDIT_MODE);}
+  QString grabEdit() const {return QString::fromLatin1(grabEditStr);}
 
+  static inline const char *infoEditStr = "infoEdit";
   Q_PROPERTY(QString infoEdit READ infoEdit CONSTANT)
-  QString infoEdit() const {return QStringLiteral(INFO_EDIT_MODE);}
+  QString infoEdit() const {return QString::fromLatin1(infoEditStr);}
+
+
 };
 
 #endif
